16e8ec0774cfe4e87695689fa84d52fe3cd56280
liaugust <ivan.liaugust@gmail.com>
2025-09-28 02:17:00 +0300

    chore: split page by separate components

--- packages/landing/components/ui/button.tsx
+++ packages/landing/components/ui/button.tsx
@@ -1,12 +1,14 @@
 import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
-import type { ButtonHTMLAttributes } from 'react';
+import { type VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
 import { forwardRef } from 'react';
 
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
-const buttonVariants = cva(
+import type { ButtonHTMLAttributes } from 'react';
+
+
+export const buttonVariants = cva(
     'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
     {
         variants: {
@@ -36,10 +38,8 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, Va
     readonly asChild?: boolean;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, ...props }, ref) => {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
 
     return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
 });
-
-export { Button, buttonVariants };
