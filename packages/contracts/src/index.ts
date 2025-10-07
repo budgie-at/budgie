84bf3e3a870cad43ef927c3c9f058fe7fe0c3b91
liaugust <ivan.liaugust@gmail.com>
2025-10-08 01:11:39 +0300

    feat: add zod to contracts

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,5 +1,5 @@
-export type * from './generic';
+export * from './generic';
 export * from './account';
 export * from './category';
-export type * from './tag';
+export * from './tag';
 export * from './transaction';
