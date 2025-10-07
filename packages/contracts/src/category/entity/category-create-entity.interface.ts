44b1c86143079a95ee408c81ff5733286fb857fb
liaugust <ivan.liaugust@gmail.com>
2025-10-08 01:11:39 +0300

    feat: add zod to contracts

--- packages/contracts/src/category/entity/category-create-entity.interface.ts
+++ packages/contracts/src/category/entity/category-create-entity.interface.ts
@@ -1,5 +1,4 @@
 import type { CategoryCreateEntitySchema } from '@/category';
 import type { z } from 'zod';
 
-
 export interface CategoryCreateEntityInterface extends z.infer<typeof CategoryCreateEntitySchema> {}
--- packages/contracts/src/tag/entity/tag-create-entity.interface.ts
+++ packages/contracts/src/tag/entity/tag-create-entity.interface.ts
@@ -1,5 +1,4 @@
 import type { TagCreateEntitySchema } from '@/tag';
 import type { z } from 'zod';
 
-
 export interface TagCreateEntityInterface extends z.infer<typeof TagCreateEntitySchema> {}
