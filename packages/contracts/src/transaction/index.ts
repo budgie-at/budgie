b00cd31138e5692908d3e5afa31e1aca29e1d61a
liaugust <ivan.liaugust@gmail.com>
2025-10-07 20:41:10 +0300

    feat: add contracts package

---
+++ packages/contracts/src/transaction/index.ts
@@ -0,0 +1,5 @@
+export { TransactionAssociationEnum } from './enum/transaction-association.enum';
+export { TransactionTypeEnum } from './enum/transaction-type.enum';
+
+export type { TransactionEntityInterface } from './entity/transaction-entity.interface';
+export type { TransactionCreateEntityInterface } from './entity/transaction-create-entity.interface';
