ce7fa644381eac65dca1089df537e105aa2f3afe
liaugust <ivan.liaugust@gmail.com>
2025-10-08 19:11:58 +0300

    feat: add describe to entity fields

--- packages/contracts/src/tag/schema/tag-entity.schema.ts
+++ packages/contracts/src/tag/schema/tag-entity.schema.ts
@@ -1,13 +1,13 @@
-import { z } from 'zod';
+import { array, string } from 'zod';
 
 import { BaseEntitySchema } from '../../generic/schema/base-entity.schema';
 import { TransactionEntitySchema } from '../../transaction/schema/transaction-entity.schema';
 import { TagAssociationEnum } from '../enum/tag-association.enum';
 
 export const TagEntitySchema = BaseEntitySchema.extend({
-    title: z.string(),
+    title: string().describe('Title of the tag.'),
 
     get [TagAssociationEnum.TRANSACTIONS]() {
-        return z.array(TransactionEntitySchema);
+        return array(TransactionEntitySchema).describe('Transactions associated with the tag.');
     }
 });
