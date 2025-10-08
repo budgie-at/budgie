bd8b8b4d06d6ba8200fa36ce59072f1f5e4d5a87
liaugust <ivan.liaugust@gmail.com>
2025-10-08 19:36:35 +0300

    feat: add enums

--- packages/contracts/src/generic/schema/base-entity.schema.ts
+++ packages/contracts/src/generic/schema/base-entity.schema.ts
@@ -1,7 +1,7 @@
 import { date, number, object } from 'zod';
 
 export const BaseEntitySchema = object({
-    id: number().describe('Unique identifier of the entity.'),
+    id: number().positive().describe('Unique identifier of the entity.'),
     createdAt: date().describe('Date of creation of the entity.'),
     updatedAt: date().describe('Date of last update of the entity.'),
     deletedAt: date().nullable().describe('Date of deletion (archiving) of the entity.')
