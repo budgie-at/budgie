85158913cab471aec15b94cf0a88b8e82d5a2633
liaugust <ivan.liaugust@gmail.com>
2025-10-10 00:21:39 +0300

    feat: fix review comments

--- packages/contracts/src/transaction/schema/transaction-entity.schema.ts
+++ packages/contracts/src/transaction/schema/transaction-entity.schema.ts
@@ -14,18 +14,18 @@ export const TransactionEntitySchema = BaseEntitySchema.extend({
     type: TransactionTypeEnumSchema,
     title: string().max(TRANSACTION_TITLE_MAX_LENGTH).describe('Title of the transaction.'),
     amount: number().describe('Amount of the transaction.'),
-    accountId: number().positive().describe('Id of the account associated with the transaction.'),
-    counterpartyAccountId: number().positive().optional().describe('Id of the counterparty account.'),
+    sourceAccountId: number().positive().describe('Id of the source account associated with the transaction.'),
+    destinationAccountId: number().positive().optional().describe('Id of the destination account.'),
     comment: string().max(TRANSACTION_COMMENT_MAX_LENGTH).optional().describe('Comment of the transaction.'),
     operatedAt: date()
         .default(() => new Date())
         .describe('Date of the transaction. Default is current date. Can be changed later.'),
 
-    get [TransactionAssociationEnum.ACCOUNT]() {
-        return AccountEntitySchema.describe('Account associated with the transaction.');
+    get [TransactionAssociationEnum.SOURCE_ACCOUNT]() {
+        return AccountEntitySchema.describe('Source account associated with the transaction.');
     },
-    get [TransactionAssociationEnum.COUNTERPARTY_ACCOUNT]() {
-        return AccountEntitySchema.nullable().describe('Counterparty account of the transaction.');
+    get [TransactionAssociationEnum.DESTINATION_ACCOUNT]() {
+        return AccountEntitySchema.nullable().describe('Destination account of the transaction.');
     },
     get [TransactionAssociationEnum.CATEGORY]() {
         return CategoryEntitySchema.describe('Category of the transaction.');
