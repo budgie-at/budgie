9a3e8c1cf8a85c8d0cce78200b83d0a3d61b02f2
liaugust <ivan.liaugust@gmail.com>
2025-10-09 22:07:54 +0300

    feat: fix review comments

--- packages/contracts/src/account/schema/account-type-enum.schema.ts
+++
@@ -1,5 +0,0 @@
-import { enum as zodEnum } from 'zod';
-
-import { AccountTypeEnum } from '../enum/account-type.enum';
-
-export const AccountTypeEnumSchema = zodEnum(AccountTypeEnum).describe('Type of the account.');
