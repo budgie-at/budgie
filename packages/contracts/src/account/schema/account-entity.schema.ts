e656afa42368d94f5fb468a99848eae586dccb73
liaugust <ivan.liaugust@gmail.com>
2025-10-11 00:28:12 +0300

    feat: resolve deadcode issues

--- packages/contracts/src/account/schema/account-entity.schema.ts
+++ packages/contracts/src/account/schema/account-entity.schema.ts
@@ -1,7 +1,7 @@
 import { array, number, string, enum as zodEnum } from 'zod';
 
+import { CurrencyEnum } from '../../generic/enum/currency.enum';
 import { BaseEntitySchema } from '../../generic/schema/base-entity.schema';
-import { CurrencyEnumSchema } from '../../generic/schema/currency-enum.schema';
 import { TransactionEntitySchema } from '../../transaction/schema/transaction-entity.schema';
 import { ACCOUNT_TITLE_MAX_LENGTH } from '../constant/account-title-max-length.constant';
 import { AccountAssociationEnum } from '../enum/account-association.enum';
@@ -9,7 +9,7 @@ import { AccountTypeEnum } from '../enum/account-type.enum';
 
 export const AccountEntitySchema = BaseEntitySchema.extend({
     type: zodEnum(AccountTypeEnum).describe('Type of the account.'),
-    currency: CurrencyEnumSchema.describe('Currency of the account.'),
+    currency: zodEnum(CurrencyEnum).describe('Currency of the account.'),
     balance: number().positive().describe('Current balance of the account.'),
     title: string().max(ACCOUNT_TITLE_MAX_LENGTH).describe('Name of the account.'),
 
