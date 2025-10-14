7d209e5690297bba12c4701ed03d165eb385d59d
liaugust <ivan.liaugust@gmail.com>
2025-10-14 22:53:55 +0300

    feat: add different types of transactions

--- packages/contracts/src/transaction/schema/sell-asset/sell-asset-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/sell-asset/sell-asset-transaction-create-entity.schema.ts
@@ -4,10 +4,12 @@ import { AssetTransactionLineCreateEntitySchema } from '../../../transaction-lin
 import { MoneyTransactionLineCreateEntitySchema } from '../../../transaction-line/schema/money/money-transaction-line-create-entity.schema';
 
 import { SellAssetTransactionEntitySchema } from './sell-asset-transaction-entity.schema';
+import { BaseTransactionCreateEntityFieldsSchema } from '../base/base-transaction-create-entity-fields.schema';
 
 export const SellAssetTransactionCreateEntitySchema = SellAssetTransactionEntitySchema.pick({
     type: true
 }).extend({
+    ...BaseTransactionCreateEntityFieldsSchema.shape,
     lines: array(union([AssetTransactionLineCreateEntitySchema, MoneyTransactionLineCreateEntitySchema]))
         .min(2)
         .describe('Lines associated with the sell asset transaction.')
--- packages/contracts/src/transaction/schema/transfer-asset/transfer-asset-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/transfer-asset/transfer-asset-transaction-create-entity.schema.ts
@@ -3,9 +3,11 @@ import { array } from 'zod';
 import { AssetTransactionLineCreateEntitySchema } from '../../../transaction-line/schema/asset/asset-transaction-line-create-entity.schema';
 
 import { TransferAssetTransactionEntitySchema } from './transfer-asset-transaction-entity.schema';
+import { BaseTransactionCreateEntityFieldsSchema } from '../base/base-transaction-create-entity-fields.schema';
 
 export const TransferAssetTransactionCreateEntitySchema = TransferAssetTransactionEntitySchema.pick({
     type: true
 }).extend({
+    ...BaseTransactionCreateEntityFieldsSchema.shape,
     lines: array(AssetTransactionLineCreateEntitySchema).length(2).describe('Lines associated with the asset-transfer transaction.')
 });
