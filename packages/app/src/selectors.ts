11f7b9ca69ecd22b9eaf8e8f0b9d4b6dfdd02208
liaugust <ivan.liaugust@gmail.com>
2025-10-19 21:03:10 +0300

    chore: remove useless code from the app

--- packages/app/src/selectors.ts
+++
@@ -1,3 +0,0 @@
-export * from './game/components/field-cell/field-cell.selectors';
-export * from './game/components/available-values-item/available-value-item.selectors';
-export * from './screens/components/game-screen/game-screen.selectors';
