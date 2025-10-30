1a144fce2027c3cc57735ed48d240cda8de9dcbb
liaugust <ivan.liaugust@gmail.com>
2025-10-30 20:43:15 +0200

    feat: add max-length

--- packages/contracts/src/transaction/schema/transaction-entity.schema.ts
+++ packages/contracts/src/transaction/schema/transaction-entity.schema.ts
@@ -2,13 +2,14 @@ import { createSelectSchema } from 'drizzle-zod';
 import { enum as zodEnum } from 'zod';
 
 import { BaseEntityFields } from '../../generic/constant/base-entity-fields.constant';
+import { TRANSACTION_TITLE_MAX_LENGTH } from '../constant/transaction-title-max-length.constant';
 import { TransactionTypeEnum } from '../enum/transaction-type.enum';
 import { TransactionEntityTable } from '../table/transaction-entity.table';
 
 export const TransactionEntitySchema = createSelectSchema(TransactionEntityTable, {
     ...BaseEntityFields,
     type: zodEnum(TransactionTypeEnum).describe('The transaction type.'),
-    title: schema => schema.describe('The transaction title.'),
+    title: schema => schema.max(TRANSACTION_TITLE_MAX_LENGTH).describe('The transaction title.'),
     comment: schema => schema.describe('The transaction comment.'),
     operatedAt: schema => schema.describe('The transaction operated at.'),
     accountId: schema => schema.describe('The id of the account transaction belongs to.'),
