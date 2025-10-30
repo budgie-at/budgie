9e913a4961aa29f01a0330dbf78e05a8a75931f5
liaugust <ivan.liaugust@gmail.com>
2025-10-30 20:41:14 +0200

    feat: add describe for columns

--- packages/contracts/src/category/schema/category-entity.schema.ts
+++ packages/contracts/src/category/schema/category-entity.schema.ts
@@ -1,5 +1,10 @@
 import { createSelectSchema } from 'drizzle-zod';
 
+import { BaseEntityFields } from '../../generic/constant/base-entity-fields.constant';
 import { CategoryEntityTable } from '../table/category-entity.table';
 
-export const CategoryEntitySchema = createSelectSchema(CategoryEntityTable);
+export const CategoryEntitySchema = createSelectSchema(CategoryEntityTable, {
+    ...BaseEntityFields,
+    title: schema => schema.describe('The category title.'),
+    icon: schema => schema.describe('The category icon.')
+});
