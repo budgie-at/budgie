5f272c530fd9e51c0a1a89ed996ed5229ad04463
liaugust <ivan.liaugust@gmail.com>
2025-10-30 20:43:15 +0200

    feat: add max-length

--- packages/contracts/src/tag/schema/tag-entity.schema.ts
+++ packages/contracts/src/tag/schema/tag-entity.schema.ts
@@ -1,9 +1,10 @@
 import { createSelectSchema } from 'drizzle-zod';
 
 import { BaseEntityFields } from '../../generic/constant/base-entity-fields.constant';
+import { TAG_TITLE_MAX_LENGTH } from '../constant/tag-title-max-length.constant';
 import { TagEntityTable } from '../table/tag-entity.table';
 
 export const TagEntitySchema = createSelectSchema(TagEntityTable, {
     ...BaseEntityFields,
-    title: schema => schema.describe('The tag title.')
+    title: schema => schema.max(TAG_TITLE_MAX_LENGTH).describe('The tag title.')
 });
