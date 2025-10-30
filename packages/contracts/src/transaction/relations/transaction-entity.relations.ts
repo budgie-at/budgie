bf20924bf3b9bdba6a4df7030c9c3d6518dad2a6
liaugust <ivan.liaugust@gmail.com>
2025-10-30 12:50:43 +0200

    feat: integrate drizzle to contracts

---
+++ packages/contracts/src/transaction/relations/transaction-entity.relations.ts
@@ -0,0 +1,19 @@
+import { relations } from 'drizzle-orm';
+
+import { AccountEntityTable } from '../../account/table/account-entity.table';
+import { CategoryEntityTable } from '../../category/table/category-entity.table';
+import { TransactionToTagEntityTable } from '../../transaction-to-tag/table/transaction-to-tag-entity.table';
+import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
+import { TransactionEntityTable } from '../table/transaction-entity.table';
+
+export const TransactionEntityRelations = relations(TransactionEntityTable, ({ one, many }) => ({
+    [TransactionAssociationEnum.SOURCE_ACCOUNT]: one(AccountEntityTable, {
+        fields: [TransactionEntityTable.accountId],
+        references: [AccountEntityTable.id]
+    }),
+    [TransactionAssociationEnum.CATEGORY]: one(CategoryEntityTable, {
+        fields: [TransactionEntityTable.categoryId],
+        references: [CategoryEntityTable.id]
+    }),
+    [TransactionAssociationEnum.TAGS]: many(TransactionToTagEntityTable)
+}));
