6ffbd4da85e14dd38da41d5e22a5da9c387dbb72
liaugust <ivan.liaugust@gmail.com>
2025-10-31 16:02:25 +0200

    feat: integrate drizzle db to the app

--- packages/contracts/src/generic/util/with-base-entity-table-columns.util.ts
+++ packages/contracts/src/generic/util/with-base-entity-table-columns.util.ts
@@ -1,11 +1,13 @@
 import { int } from 'drizzle-orm/sqlite-core';
 
+import { CURRENT_TIMESTAMP } from '../constant/current-timestamp.constant';
+
 import type { SQLiteColumnBuilderBase } from 'drizzle-orm/sqlite-core/columns/common';
 
 const baseFields = {
     id: int({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-    createdAt: int({ mode: 'timestamp' }).notNull(),
-    updatedAt: int({ mode: 'timestamp' }).notNull(),
+    createdAt: int({ mode: 'timestamp' }).notNull().default(CURRENT_TIMESTAMP),
+    updatedAt: int({ mode: 'timestamp' }).notNull().default(CURRENT_TIMESTAMP),
     deletedAt: int({ mode: 'timestamp' })
 } as const;
 
