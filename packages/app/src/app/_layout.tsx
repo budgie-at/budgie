5d9cdcde83f41b2038c2f8fd42c84f49c9aec6dd
liaugust <ivan.liaugust@gmail.com>
2025-11-01 01:57:32 +0200

    feat: add drizzle studio

--- packages/app/src/app/_layout.tsx
+++ packages/app/src/app/_layout.tsx
@@ -5,6 +5,7 @@ import { I18nProvider } from '@lingui/react';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { SQLiteProvider } from 'expo-sqlite';
 import { useEffect } from 'react';
 import { enableFreeze, enableScreens } from 'react-native-screens';
 import { Provider } from 'react-redux';
@@ -14,6 +15,7 @@ import migrations from '../../drizzle/migrations';
 import { appRootPersistor, appRootStore } from '../@generic/app-root.store';
 import { i18nGetOSLocale } from '../@generic/utils/i18n.util';
 import '../global.css';
+import { DB_NAME } from '../drizzle/constant/db-name.constant';
 import { db } from '../drizzle/db/db';
 import { ThemeProvider } from '../theme/context/theme.context';
 
@@ -46,7 +48,10 @@ export default function RootLayout() {
             <PersistGate loading={null} persistor={appRootPersistor}>
                 <I18nProvider i18n={i18n}>
                     <ThemeProvider>
-                        <Stack screenOptions={stackOptions} />
+                        <SQLiteProvider databaseName={DB_NAME} options={{ enableChangeListener: true }}>
+                            {/* eslint-disable-next-line react/jsx-max-depth */}
+                            <Stack screenOptions={stackOptions} />
+                        </SQLiteProvider>
                     </ThemeProvider>
                 </I18nProvider>
             </PersistGate>
