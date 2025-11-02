dc61e5b4610559f8f24a7318f1dcb9c4ac5b32e1
liaugust <ivan.liaugust@gmail.com>
2025-11-02 22:55:57 +0200

    fix: change account create mutation example
    # Conflicts:
    #	packages/app/src/@account/mutation/create-account.mutation.ts
    #	packages/app/src/app/(tabs)/index.tsx

--- packages/contracts/src/account/entity/crypto/crypto-account-entity.interface.ts
+++ packages/contracts/src/account/entity/crypto/crypto-account-entity.interface.ts
@@ -1,5 +1,4 @@
 import type { CryptoAccountEntitySchema } from '../../schema/crypto/crypto-account-entity.schema';
 import type { infer } from 'zod';
 
-export interface CryptoAccountEntityInterface extends infer<typeof CryptoAccountEntitySchema> {
-}
+export interface CryptoAccountEntityInterface extends infer<typeof CryptoAccountEntitySchema> {}
