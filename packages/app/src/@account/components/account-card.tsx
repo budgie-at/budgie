bab6fbdaa6e86cd95bdd9fc2a1603dde737ceddc
liaugust <ivan.liaugust@gmail.com>
2025-11-03 16:12:33 +0200

    feat: add icon support for chip

--- packages/app/src/@account/components/account-card.tsx
+++ packages/app/src/@account/components/account-card.tsx
@@ -1,10 +1,11 @@
 import { Card } from '../../@generic/components/card/card';
 import { Chip } from '../../@generic/components/chip/chip';
+import { HouseIcon } from 'lucide-react-native';
 
 export const AccountCard = () => (
-    <Card className="flex-row flex-wrap">
-        <Chip isSelected label="Chip asdasd1" />
-        <Chip label="Chip 2 asdasd asdasda asdasd" />
+    <Card className="flex-row flex-wrap gap-2">
+        <Chip isSelected label="Chip asdasd1" icon={HouseIcon} />
+        <Chip label="Chip 2 asdasd asdasda asdasd" icon={HouseIcon} />
         <Chip label="Chip 3" />
     </Card>
 );
--- packages/app/src/@generic/components/chip/chip.tsx
+++ packages/app/src/@generic/components/chip/chip.tsx
@@ -1,16 +1,22 @@
 import { cva } from 'class-variance-authority';
 import { Text, TouchableOpacity } from 'react-native';
 
+import { isDefined } from '@rnw-community/shared';
+
 import { cn } from '../../utils/cn.util';
+import { Icon } from '../icon/icon';
+
+import type { LucideIcon } from 'lucide-react-native';
 
 interface ChipPropsInterface {
     readonly onPress?: () => void;
     readonly isSelected?: boolean;
     readonly className?: string;
+    readonly icon?: LucideIcon;
     readonly label: string;
 }
 
-const chipVariants = cva('max-w-50 rounded-7xl py-3 px-3xl border', {
+const chipVariants = cva('max-w-50 rounded-5xl py-3 px-3xl gap-x-2 border items-center flex-row', {
     variants: {
         isSelected: {
             true: 'bg-secondary-reverse-background/5 border-secondary-reverse-corner/20',
@@ -19,8 +25,10 @@ const chipVariants = cva('max-w-50 rounded-7xl py-3 px-3xl border', {
     }
 });
 
-export const Chip = ({ label, onPress, className, isSelected = false }: ChipPropsInterface) => (
+export const Chip = ({ label, onPress, className, isSelected = false, icon }: ChipPropsInterface) => (
     <TouchableOpacity className={cn(chipVariants({ isSelected }), className)} disabled={isSelected} onPress={onPress}>
+        {isDefined(icon) && <Icon className={isSelected ? 'text-primary' : 'text-secondary-foreground'} icon={icon} size={16} />}
+
         <Text className={isSelected ? 'text-primary' : 'text-secondary-foreground'} ellipsizeMode="tail" numberOfLines={1}>
             {label}
         </Text>
