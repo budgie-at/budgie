970faff1c5c506425c06b8c24e27e61854a6e2bf
liaugust <ivan.liaugust@gmail.com>
2025-11-03 15:25:34 +0200

    feat: add shared circle-icon component

--- packages/app/src/ai-chat/components/ai-chat-button/ai-chat-button.tsx
+++ packages/app/src/ai-chat/components/ai-chat-button/ai-chat-button.tsx
@@ -1,19 +1,22 @@
 import { useLingui } from '@lingui/react/macro';
 import { Mic } from 'lucide-react-native';
+import { useContext } from 'react';
 import { Pressable, Text, View } from 'react-native';
 
 import { Icon } from '../../../@generic/components/icon/icon';
+import { ThemeContext } from '../../../theme/context/theme.context';
 
 export const AiChatButton = () => {
     const { t } = useLingui();
+    const { toggleColorSchema } = useContext(ThemeContext);
 
     return (
         <View>
-            <Pressable className="bg-bg-primary-reverse p-[24] rounded-full mb-[6] -translate-y-[15]">
-                <Icon className="text-text-primary-reverse" icon={Mic} size={16} />
+            <Pressable className="bg-primary p-7xl rounded-full mb-sm -translate-y-4" onPress={toggleColorSchema}>
+                <Icon className="text-primary-reverse" icon={Mic} size={16} />
             </Pressable>
 
-            <Text className="text-text-primary text-center">{t`AI`}</Text>
+            <Text className="text-primary text-center">{t`AI`}</Text>
         </View>
     );
 };
--- packages/app/src/app/(tabs)/index.tsx
+++ packages/app/src/app/(tabs)/index.tsx
@@ -2,15 +2,20 @@ import { AccountTypeEnum, CurrencyEnum } from '@budgie/contracts';
 import { Trans } from '@lingui/react';
 import { useLingui } from '@lingui/react/macro';
 import { useDrizzleStudio } from 'expo-drizzle-studio-plugin';
+import { ImpactFeedbackStyle } from 'expo-haptics';
 import { useSQLiteContext } from 'expo-sqlite';
 import { Pressable, Text } from 'react-native';
 
+import { AccountCard } from '../../@account/components/account-card';
 import { createAccountMutation } from '../../@account/mutation/create-account.mutation';
 import { useGetAccountsQuery } from '../../@account/query/use-get-accounts.query';
+import { Card } from '../../@generic/components/card/card';
 import { Page } from '../../@generic/components/page/page';
+import { useVibration } from '../../@generic/hooks/use-vibration.hook';
 
 export default function HomePage() {
     const { data } = useGetAccountsQuery();
+    const [, hapticImpact] = useVibration();
     const { t } = useLingui();
 
     const db = useSQLiteContext();
@@ -23,17 +28,25 @@ export default function HomePage() {
             currency: CurrencyEnum.UAH,
             type: AccountTypeEnum.CASH
         });
+        hapticImpact(ImpactFeedbackStyle.Light);
     };
 
     return (
         <Page>
-            <Text className="text-text-primary">{t`Home Screen`}</Text>
-            <Pressable onPress={() => void onAdd()}>
-                <Text>{t`Create Account`}</Text>
-            </Pressable>
+            <Text className="text-primary">{t`Home Screen`}</Text>
+
+            <Card asChild>
+                <Pressable onPress={() => void onAdd()}>
+                    <Text className="text-primary">{t`Create Account`}</Text>
+                </Pressable>
+            </Card>
 
             {data.map(account => (
-                <Text key={account.id}>
+                <AccountCard key={account.id} />
+            ))}
+
+            {data.map(account => (
+                <Text className="text-primary" key={account.id}>
                     <Trans
                         id="greeting"
                         message="ID: {accountId}. Created at: {createdAt}"
