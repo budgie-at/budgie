9d8a0a1db84ebea5f4ac76206c139db34f6a060d
liaugust <ivan.liaugust@gmail.com>
2025-11-03 21:00:52 +0200

    fix: change bottom-tabs safe-area edges

--- packages/app/src/@generic/components/bottom-tabs/bottom-tabs.tsx
+++ packages/app/src/@generic/components/bottom-tabs/bottom-tabs.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import type { PropsWithChildren } from 'react';
 
 export const BottomTabs = ({ children }: PropsWithChildren) => (
-    <SafeAreaView>
+    <SafeAreaView edges={['bottom']}>
         <View className="flex-row justify-evenly border-t border-t-secondary-corner items-baseline">{children}</View>
     </SafeAreaView>
 );
