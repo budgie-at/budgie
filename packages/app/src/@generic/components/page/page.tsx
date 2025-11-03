073aafaa47b902f57ec775157208f56fdac7de6a
liaugust <ivan.liaugust@gmail.com>
2025-11-03 21:00:52 +0200

    fix: change bottom-tabs safe-area edges

--- packages/app/src/@generic/components/page/page.tsx
+++ packages/app/src/@generic/components/page/page.tsx
@@ -1,8 +1,12 @@
 import { styled } from 'nativewind';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { cn } from '../../utils/cn.util';
+
 import type { ComponentProps } from 'react';
 
 const Wrapper = styled(SafeAreaView);
 
-export const Page = (props: ComponentProps<typeof SafeAreaView>) => <Wrapper {...props} className="bg-primary-reverse flex-1 pl-5xl pr-5xl" />;
+export const Page = ({ className, ...rest }: ComponentProps<typeof SafeAreaView>) => (
+    <Wrapper {...rest} className={cn('bg-primary-reverse pl-5xl pr-5xl flex-1', className)} />
+);
