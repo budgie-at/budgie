cb1c5bf55b55cb089d2c6351f308b091eabf9986
liaugust <ivan.liaugust@gmail.com>
2025-11-03 21:10:24 +0200

    fix: replace SafeAreaView with View in page component

--- packages/app/src/@generic/components/page/page.tsx
+++ packages/app/src/@generic/components/page/page.tsx
@@ -1,12 +1,12 @@
 import { styled } from 'nativewind';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View } from 'react-native';
 
 import { cn } from '../../utils/cn.util';
 
 import type { ComponentProps } from 'react';
 
-const Wrapper = styled(SafeAreaView);
+const Wrapper = styled(View);
 
-export const Page = ({ className, ...rest }: ComponentProps<typeof SafeAreaView>) => (
+export const Page = ({ className, ...rest }: ComponentProps<typeof View>) => (
     <Wrapper {...rest} className={cn('bg-primary-reverse pl-5xl pr-5xl flex-1', className)} />
 );
