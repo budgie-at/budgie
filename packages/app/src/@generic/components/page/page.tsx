3e9a1cfc30eb35d2b5d026e15c82b66ab265c990
liaugust <ivan.liaugust@gmail.com>
2025-11-04 23:23:08 +0200

    fix: change page component

--- packages/app/src/@generic/components/page/page.tsx
+++ packages/app/src/@generic/components/page/page.tsx
@@ -1,12 +1,9 @@
-import { styled } from 'nativewind';
 import { View } from 'react-native';
 
 import { cn } from '../../utils/cn.util';
 
 import type { ComponentProps } from 'react';
 
-const Wrapper = styled(View);
-
 export const Page = ({ className, ...rest }: ComponentProps<typeof View>) => (
-    <Wrapper {...rest} className={cn('bg-primary-reverse pl-5xl pr-5xl flex-1', className)} />
+    <View {...rest} className={cn('bg-primary-reverse pl-5xl pr-5xl flex-1', className)} />
 );
