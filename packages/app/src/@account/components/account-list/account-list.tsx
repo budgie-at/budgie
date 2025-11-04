8b2548ec406ff1af1e1a338e8a662d9e813d2d8b
liaugust <ivan.liaugust@gmail.com>
2025-11-04 22:50:03 +0200

    fix: add temp default icon for accounts

--- packages/app/src/@account/components/account-list/account-list.tsx
+++ packages/app/src/@account/components/account-list/account-list.tsx
@@ -12,6 +12,7 @@ export const AccountList = ({ accounts }: AccountListPropsInterface) => (
     <View className="flex-row flex-wrap -mx-1.5 gap-y-3">
         {accounts.map(account => (
             <View className="w-1/2 px-1.5" key={account.id}>
+                {/* eslint-disable-next-line lingui/no-unlocalized-strings */}
                 <AccountCard balance={account.balance} currency={account.currency} icon="Home" title={account.title} />
             </View>
         ))}
