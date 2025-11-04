8bfb598fadd3608813ab6588d2da442d212e49dc
liaugust <ivan.liaugust@gmail.com>
2025-11-04 23:23:08 +0200

    fix: change page component

--- packages/app/src/@generic/components/card/card.tsx
+++ packages/app/src/@generic/components/card/card.tsx
@@ -1,17 +1,19 @@
-import * as Slot from '@rn-primitives/slot';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
+
+import { isDefined } from '@rnw-community/shared';
 
 import { cn } from '../../utils/cn.util';
 
+import type { OnEventFn } from '@rnw-community/shared';
 import type { PropsWithChildren } from 'react';
 
 interface Props {
-    readonly asChild?: boolean;
+    readonly onPress?: OnEventFn;
     readonly className?: string;
 }
 
-export const Card = ({ asChild, className, ...rest }: PropsWithChildren<Props>) => {
-    const Component = asChild === true ? Slot.View : View;
+export const Card = ({ className, onPress, ...rest }: PropsWithChildren<Props>) => {
+    const Component = isDefined(onPress) ? Pressable : View;
 
-    return <Component className={cn(`p-5xl border rounded-5xl border-corner`, className)} {...rest} />;
+    return <Component className={cn(`p-5xl border rounded-5xl border-corner`, className)} onPress={onPress} {...rest} />;
 };
