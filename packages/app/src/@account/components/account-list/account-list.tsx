ea6f8df0f26a25f446f0aa9917fe43febccba119
liaugust <ivan.liaugust@gmail.com>
2025-11-05 19:15:00 +0200

    fix: resolve conflicts

--- packages/app/src/@account/components/account-list/account-list.tsx
+++ packages/app/src/@account/components/account-list/account-list.tsx
@@ -12,7 +12,7 @@ export const AccountList = ({ accounts }: Props) => (
     <View className="flex-row flex-wrap -mx-1.5 gap-y-3">
         {accounts.map(account => (
             <View className="w-1/2 px-1.5" key={account.id}>
-                {/* eslint-disable-next-line lingui/no-unlocalized-strings */}
+                { }
                 <AccountCard balance={account.balance} currency={account.currency} icon="Home" title={account.title} />
             </View>
         ))}
