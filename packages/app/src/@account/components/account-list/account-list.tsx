d4862536c644e91015617b032fc1c7e571e57ce2
liaugust <ivan.liaugust@gmail.com>
2025-11-05 19:33:37 +0200

    fix: disable lint for providers

--- packages/app/src/@account/components/account-list/account-list.tsx
+++ packages/app/src/@account/components/account-list/account-list.tsx
@@ -12,7 +12,6 @@ export const AccountList = ({ accounts }: Props) => (
     <View className="flex-row flex-wrap -mx-1.5 gap-y-3">
         {accounts.map(account => (
             <View className="w-1/2 px-1.5" key={account.id}>
-                { }
                 <AccountCard balance={account.balance} currency={account.currency} icon="Home" title={account.title} />
             </View>
         ))}
