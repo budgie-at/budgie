631d8c75107ecf5f9bddce5cdc2c0a35cf902792
liaugust <ivan.liaugust@gmail.com>
2025-11-05 21:15:22 +0200

    feat: update migrations

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -4,13 +4,15 @@ import journal from './meta/_journal.json';
 import m0000 from './0000_massive_electro.sql';
 import m0001 from './0001_flawless_kylun.sql';
 import m0002 from './0002_skinny_winter_soldier.sql';
+import m0003 from './0003_narrow_demogoblin.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
 m0001,
-m0002
+m0002,
+m0003
     }
   }
   
