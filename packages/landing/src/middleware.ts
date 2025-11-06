ebf16414e76d2ddc55f90d759d3877a62f69584a
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-06 19:53:42 +0100

    fix(landing): i18n middleware

--- packages/landing/src/middleware.ts
+++ packages/landing/src/middleware.ts
@@ -34,5 +34,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!_next/static|_next/image|favicon.ico|.well-known|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
+    matcher: ['/((?!_next/static|_next/image|favicon.ico|.well-known|[^.]*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
 };
