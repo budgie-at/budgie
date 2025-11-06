6344ad97b1ddb98e13e8e574806dccd4e9b7a6ae
liaugust <ivan.liaugust@gmail.com>
2025-11-06 21:52:35 +0200

    feat: update migration

--- packages/contracts/src/account/table/account-entity.table.ts
+++ packages/contracts/src/account/table/account-entity.table.ts
@@ -1,6 +1,7 @@
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 import { CurrencyEnum } from '../../generic/enum/currency.enum';
+import { UserIconNameEnum } from '../../generic/enum/user-icon-name.enum';
 import { convertEnumToDrizzleEnum } from '../../generic/util/convert-enum-to-drizzle-enum.util';
 import { withBaseEntityTableColumns } from '../../generic/util/with-base-entity-table-columns.util';
 import { AccountTypeEnum } from '../enum/account-type.enum';
@@ -8,7 +9,9 @@ import { AccountTypeEnum } from '../enum/account-type.enum';
 export const AccountEntityTable = sqliteTable(
     'accounts',
     withBaseEntityTableColumns({
-        icon: text().notNull(),
+        icon: text({ enum: convertEnumToDrizzleEnum(UserIconNameEnum) })
+            .$type<UserIconNameEnum>()
+            .notNull(),
         order: int({ mode: 'number' }).default(0).notNull(),
         balance: int({ mode: 'number' }).default(0).notNull(),
         title: text().default('').notNull(),
