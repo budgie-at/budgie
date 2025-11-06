e34b60059971e487079437961fbf58a666304d09
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-06 08:13:51 +0100

    ci: add github support for [WIP]

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes']
+    extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+    rules: {
+        'header-pattern': [2, 'always', /^(?:\[(WIP|wip)\]\s*)?(.*)$/]
+    }
 };
