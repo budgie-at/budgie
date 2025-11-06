99587d84a61b524ca34014c4b4f330081f7453bb
liaugust <ivan.liaugust@gmail.com>
2025-11-06 21:43:57 +0200

    feat: add "min" for category and tag titles

--- packages/contracts/src/category/schema/category-entity.schema.ts
+++ packages/contracts/src/category/schema/category-entity.schema.ts
@@ -2,11 +2,12 @@ import { createSelectSchema } from 'drizzle-zod';
 
 import { BaseEntityFields } from '../../generic/constant/base-entity-fields.constant';
 import { CATEGORY_TITLE_MAX_LENGTH } from '../constant/category-title-max-length.constant';
+import { CATEGORY_TITLE_MIN_LENGTH } from '../constant/category-title-min-length.constant';
 import { CategoryEntityTable } from '../table/category-entity.table';
 
 export const CategoryEntitySchema = createSelectSchema(CategoryEntityTable, {
     ...BaseEntityFields,
-    title: schema => schema.max(CATEGORY_TITLE_MAX_LENGTH).describe('The category title.'),
+    title: schema => schema.min(CATEGORY_TITLE_MIN_LENGTH).max(CATEGORY_TITLE_MAX_LENGTH).describe('The category title.'),
     icon: schema => schema.describe('The category icon.'),
     parentId: schema => schema.positive().nullable().describe('The id of the parent category.'),
 });
