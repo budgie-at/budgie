50a5416bc3b9f170e4c23a0c0928298ccb2b32b5
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-06 19:09:44 +0100

    feat(landing): blog

--- packages/landing/src/mdx-components.tsx
+++ packages/landing/src/mdx-components.tsx
@@ -1,16 +1,13 @@
 import type { MDXComponents } from 'mdx/types';
 
 const ownComponents: MDXComponents = {
-    h1: ({ children }) => <h1 className="text-4xl md:text-5xl font-bold tracking-tight mb-6 mt-12">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-3xl md:text-4xl font-bold tracking-tight mb-4 mt-10">{children}</h2>,
-    h3: ({ children }) => <h3 className="text-2xl md:text-3xl font-bold tracking-tight mb-3 mt-8">{children}</h3>,
-    p: ({ children }) => <p className="text-lg font-bold mb-2 mt-4">{children}</p>,
-    li: ({ children }) => <li className="text-base mb-2 ml-6">{children}</li>,
-    a: ({ children, ...props }) => (
-        <a className="text-primary hover:underline" {...props}>
-            {children}
-        </a>
-    )
+    h1: props => <h1 className="text-4xl md:text-5xl font-bold tracking-tight mb-6 mt-12" {...props}></h1>,
+    h2: props => <h2 className="text-3xl md:text-4xl font-bold tracking-tight mb-4 mt-10" {...props}></h2>,
+    h3: props => <h3 className="text-2xl md:text-3xl font-bold tracking-tight mb-3 mt-8" {...props}></h3>,
+    p: props => <p className="text-lg font-bold mb-2 mt-4" {...props}></p>,
+    li: props => <li className="text-base mb-2 ml-6" {...props}></li>,
+    ol: props => <ol className="text-base mb-2 ml-6" {...props}></ol>,
+    a: props => <a className="text-primary hover:underline" {...props}></a>
 } satisfies MDXComponents;
 
 // eslint-disable-next-line func-style
