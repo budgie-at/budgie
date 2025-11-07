ad1de99b82716cbf6bebe09d1e2fa3944a771240
liaugust <ivan.liaugust@gmail.com>
2025-11-07 02:30:01 +0200

    fix: remove unused file

--- packages/contracts/src/transaction/schema/buy-asset/buy-asset-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/buy-asset/buy-asset-transaction-create-entity.schema.ts
@@ -1,5 +1,6 @@
-import { BuyAssetTransactionEntitySchema } from './buy-asset-transaction-entity.schema';
 import { convertToCreateEntitySchema } from '../../../generic/util/convert-to-create-entity-schema.util';
+
+import { BuyAssetTransactionEntitySchema } from './buy-asset-transaction-entity.schema';
 
 export const BuyAssetTransactionCreateEntitySchema = convertToCreateEntitySchema(BuyAssetTransactionEntitySchema).partial({
     comment: true,
--- packages/contracts/src/transaction/schema/income/income-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/income/income-transaction-create-entity.schema.ts
@@ -1,5 +1,6 @@
-import { IncomeTransactionEntitySchema } from './income-transaction-entity.schema';
 import { convertToCreateEntitySchema } from '../../../generic/util/convert-to-create-entity-schema.util';
+
+import { IncomeTransactionEntitySchema } from './income-transaction-entity.schema';
 
 export const IncomeTransactionCreateEntitySchema = convertToCreateEntitySchema(IncomeTransactionEntitySchema).partial({
     comment: true,
