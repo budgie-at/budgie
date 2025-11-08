32075cff188214b8677636880346b98a9e04e67f
liaugust <ivan.liaugust@gmail.com>
2025-11-08 19:52:22 +0200

    feat: add sub-account relation

--- packages/contracts/src/account/enum/account-association.enum.ts
+++ packages/contracts/src/account/enum/account-association.enum.ts
@@ -1,5 +1,7 @@
 export enum AccountAssociationEnum {
     TRANSACTIONS = 'transactions',
+    SUB_ACCOUNTS = 'subAccounts',
     INSTRUMENT = 'instrument',
     BALANCES = 'balances',
+    PARENT = 'parent',
 }
