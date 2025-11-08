4aaba7e6c2426daf8bfab3ef77e2cfd1e7f27ae7
liaugust <ivan.liaugust@gmail.com>
2025-11-08 19:33:10 +0200

    feat: add refine

--- packages/contracts/src/transaction/schema/buy-asset-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/buy-asset-transaction-create-entity.schema.ts
@@ -2,43 +2,18 @@ import { array } from 'zod';
 
 import { TransactionEntryCreateEntitySchema } from '../../transaction-entry/schema/transaction-entry-create-entity.schema';
 import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
+import { transferTransactionRefine } from '../refines/transfer-transaction.refine';
 
 import { ExpenseTransactionCreateEntitySchema } from './expense-transaction-create-entity.schema';
 
 export const BuyAssetTransactionCreateEntitySchema = ExpenseTransactionCreateEntitySchema.omit({
     [TransactionAssociationEnum.ENTRIES]: true
 })
-    .extend({
-        [TransactionAssociationEnum.ENTRIES]: array(TransactionEntryCreateEntitySchema).length(2)
-    })
+    .extend({ [TransactionAssociationEnum.ENTRIES]: array(TransactionEntryCreateEntitySchema).length(2) })
     .superRefine(({ entries, exchangeRate }, context) => {
-        const [entryA, entryB] = entries;
-
-        if (entryA.accountId === entryB.accountId) {
-            context.addIssue({
-                code: 'custom',
-                path: ['entries'],
-                message: 'buy asset transaction entries must have different accounts'
-            });
-
-            return;
-        }
-
-        if (entryA.instrumentId === entryB.instrumentId) {
-            context.addIssue({
-                code: 'custom',
-                path: ['entries'],
-                message: 'buy asset transaction entries must have different instruments'
-            });
-
-            return;
-        }
-
-        if (exchangeRate === 1) {
-            context.addIssue({
-                code: 'custom',
-                path: ['exchangeRate'],
-                message: 'buy asset transaction exchange rate must be > 1'
-            });
-        }
+        transferTransactionRefine(entries, exchangeRate, context, {
+            sameAccount: false,
+            sameInstrument: false,
+            stableExchangeRate: false
+        });
     });
