1c189ca3412313054297f9e28b53695813f9c9f5
liaugust <ivan.liaugust@gmail.com>
2025-11-08 19:33:10 +0200

    feat: add refine

--- packages/contracts/src/transaction/schema/transfer-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/transfer-transaction-create-entity.schema.ts
@@ -1,8 +1,19 @@
+import { array } from 'zod';
+
 import { convertToCreateEntitySchema } from '../../generic/util/convert-to-create-entity-schema.util';
+import { TransactionEntryCreateEntitySchema } from '../../transaction-entry/schema/transaction-entry-create-entity.schema';
+import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
+import { transferTransactionRefine } from '../refines/transfer-transaction.refine';
 
 import { TransferTransactionEntitySchema } from './transfer-transaction-entity.schema';
 
-export const TransferTransactionCreateEntitySchema = convertToCreateEntitySchema(TransferTransactionEntitySchema).partial({
-    comment: true,
-    operatedAt: true
-});
+export const TransferTransactionCreateEntitySchema = convertToCreateEntitySchema(TransferTransactionEntitySchema)
+    .partial({ comment: true, operatedAt: true })
+    .extend({ [TransactionAssociationEnum.ENTRIES]: array(TransactionEntryCreateEntitySchema).min(1) })
+    .superRefine(({ entries, exchangeRate }, context) => {
+        transferTransactionRefine(entries, exchangeRate, context, {
+            sameAccount: false,
+            sameInstrument: true,
+            stableExchangeRate: true
+        });
+    });
