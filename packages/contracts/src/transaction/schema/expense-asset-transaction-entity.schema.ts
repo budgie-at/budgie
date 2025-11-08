5b424b59606d7fc0f6b41fe804bbdb6b9ca81ea9
liaugust <ivan.liaugust@gmail.com>
2025-11-08 19:33:10 +0200

    feat: add refine

--- packages/contracts/src/transaction/schema/expense-asset-transaction-entity.schema.ts
+++
@@ -1,9 +0,0 @@
-import { literal } from 'zod';
-
-import { TransactionTypeEnum } from '../enum/transaction-type.enum';
-
-import { TransactionEntitySchema } from './transaction-entity.schema';
-
-export const ExpenseAssetTransactionEntitySchema = TransactionEntitySchema.omit({ type: true }).extend({
-    type: literal(TransactionTypeEnum.EXPENSE)
-});
