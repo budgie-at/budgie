b89931138d04cc7260e8cbd02a696f18a5909115
liaugust <ivan.liaugust@gmail.com>
2025-11-08 19:47:15 +0200

    feat: add refine

--- packages/contracts/src/transaction/schema/buy-asset-transaction-create-entity.schema.ts
+++ packages/contracts/src/transaction/schema/buy-asset-transaction-create-entity.schema.ts
@@ -1,19 +1,13 @@
-import { array } from 'zod';
-
-import { TransactionEntryCreateEntitySchema } from '../../transaction-entry/schema/transaction-entry-create-entity.schema';
-import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
 import { transferTransactionRefine } from '../refines/transfer-transaction.refine';
 
-import { ExpenseTransactionCreateEntitySchema } from './expense-transaction-create-entity.schema';
+import { BaseTransferTransactionCreateEntitySchema } from './base-transfer-transaction-create-entity.schema';
 
-export const BuyAssetTransactionCreateEntitySchema = ExpenseTransactionCreateEntitySchema.omit({
-    [TransactionAssociationEnum.ENTRIES]: true
-})
-    .extend({ [TransactionAssociationEnum.ENTRIES]: array(TransactionEntryCreateEntitySchema).length(2) })
-    .superRefine(({ entries, exchangeRate }, context) => {
+export const BuyAssetTransactionCreateEntitySchema = BaseTransferTransactionCreateEntitySchema.superRefine(
+    ({ entries, exchangeRate }, context) => {
         transferTransactionRefine(entries, exchangeRate, context, {
             sameAccount: false,
             sameInstrument: false,
             stableExchangeRate: false
         });
-    });
+    }
+);
