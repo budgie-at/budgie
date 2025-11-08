0498edd8b1944ae31b285571b9602a2e82e1c4ba
liaugust <ivan.liaugust@gmail.com>
2025-11-08 22:52:18 +0200

    feat: add settings screen with theme switch

--- packages/app/src/app/(tabs)/settings.tsx
+++ packages/app/src/app/(tabs)/settings.tsx
@@ -1,14 +1,53 @@
-import { useLingui } from '@lingui/react/macro';
-import { Text } from 'react-native';
+import { Trans, useLingui } from '@lingui/react/macro';
+import { ScrollView, Text, View } from 'react-native';
 
 import { Page } from '../../@generic/components/page/page';
+import { SettingsTextItem } from '../../@settings/components/settings-text-item/settings-text-item';
+import { ThemeSwitch } from '../../@settings/components/theme-switch/theme-switch';
 
 export default function SettingsPage() {
     const { t } = useLingui();
 
     return (
         <Page>
-            <Text className="text-primary">{t`Settings Page`}</Text>
+            <ScrollView>
+                <Text className="text-primary text-[36px]">{t`Settings`}</Text>
+
+                <View className={'pt-[20px] gap-y-[24px]'}>
+                    <View className={'gap-y-[10px]'}>
+                        <Text className={'text-[12px] uppercase text-secondary-foreground'}>
+                            <Trans>Privacy</Trans>
+                        </Text>
+
+                        <SettingsTextItem
+                            variant={'positive'}
+                            title={t`100% Offline & Private`}
+                            description={t`All your financial data is stored locally on your device. No cloud sync, no tracking, no data sharing.`}
+                            icon="Shield"
+                        />
+                    </View>
+
+                    <View className={'gap-y-[10px]'}>
+                        <Text className={'text-[12px] uppercase text-secondary-foreground'}>
+                            <Trans>General</Trans>
+                        </Text>
+
+                        <ThemeSwitch />
+                    </View>
+                    <View className={'gap-y-[10px]'}>
+                        <Text className={'text-[12px] uppercase text-secondary-foreground'}>
+                            <Trans>About</Trans>
+                        </Text>
+
+                        <SettingsTextItem
+                            variant={'ghost'}
+                            title={t`BudgetAI`}
+                            description={t`AI-powered budgeting app with complete privacy. All data processing happens locally on your device. \nVersion 1.0.0`}
+                            icon="Database"
+                        />
+                    </View>
+                </View>
+            </ScrollView>
         </Page>
     );
 }
