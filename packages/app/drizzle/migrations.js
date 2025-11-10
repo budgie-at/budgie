1173c1c807d7e532ceb5b6b011a1e826b7e47c4f
liaugust <ivan.liaugust@gmail.com>
2025-11-10 22:43:52 +0200

    feat: update migrations

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_absurd_paper_doll.sql';
 import m0001 from './0001_category-parent.sql';
+import m0002 from './0002_transactions.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
--- packages/contracts/src/test-utils/convert-to-from-micro.util.ts
+++ packages/contracts/src/test-utils/convert-to-from-micro.util.ts
@@ -1,9 +1,8 @@
 import { PRECISION } from '../transaction/constant/precision.constant';
 
-// convert TO micro-units -> FROM micro-units with same fixed-point rule as schema
 export const convertToFromMicro = (toAmountMicro: number, exchangeRate: number): number => {
-    const rateScaledInteger = Math.round(exchangeRate * PRECISION);
+    const rateScaled = Math.round(exchangeRate * PRECISION);
 
-    return Math.round((toAmountMicro * rateScaledInteger) / PRECISION);
+    return Math.round((toAmountMicro * rateScaled) / PRECISION);
 };
 
