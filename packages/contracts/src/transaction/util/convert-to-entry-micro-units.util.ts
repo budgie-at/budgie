9c907785f10de31dca23b787945413fbc8a25a46
liaugust <ivan.liaugust@gmail.com>
2025-11-10 22:32:12 +0200

    fix: remove useless file

--- packages/contracts/src/transaction/util/convert-to-entry-micro-units.util.ts
+++
@@ -1,4 +0,0 @@
-import { PRECISION } from '../constant/precision.constant';
-
-export const convertToEntryMicroUnits = (amountMicroUnits: number, rateScaled: number): number =>
-    Math.round((amountMicroUnits * rateScaled) / PRECISION);
