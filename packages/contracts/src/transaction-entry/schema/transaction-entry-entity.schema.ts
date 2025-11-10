2e8c892c187c454278057cc1cfc88201d1b52d67
liaugust <ivan.liaugust@gmail.com>
2025-11-10 15:09:04 +0200

    feat: add tests and refine for asset-related transactions

--- packages/contracts/src/transaction-entry/schema/transaction-entry-entity.schema.ts
+++ packages/contracts/src/transaction-entry/schema/transaction-entry-entity.schema.ts
@@ -14,5 +14,5 @@ export const TransactionEntryEntitySchema = createSelectSchema(TransactionEntryE
     instrumentId: schema => schema.describe('Id of the instrument the entry belongs to'),
     parentAccountId: schema => schema.describe('Id of the parent-account the entry belongs to'),
     type: zodEnum(TransactionEntryTypeEnum).describe('Type of the entry'),
-    amount: schema => schema.describe('Amount of the entry')
+    amount: schema => schema.positive().describe('Amount of the entry')
 });
