d315f758cf4c6e614dca71176b43688221b9370f
liaugust <ivan.liaugust@gmail.com>
2025-11-10 15:51:08 +0200

    feat: add test util to create transaction-entry

--- packages/contracts/src/test-utils/create-transfer-transaction-entry-input.util.ts
+++ packages/contracts/src/test-utils/create-transfer-transaction-entry-input.util.ts
@@ -1,15 +1,15 @@
 import { TransactionEntryCreateEntityInterface } from '../transaction-entry/entity/transaction-entry-create-entity.interface';
 import { TransactionEntryTypeEnum } from '../transaction-entry/enum/transaction-entry-type.enum';
 
-export const createTransactionEntryInput = (
+export const createTransferTransactionEntryInput = (
+    accountId: number,
     type: TransactionEntryTypeEnum,
-    amount: number,
-    categoryId: number,
+    amount: number
 ): TransactionEntryCreateEntityInterface => ({
     type,
     amount,
-    categoryId,
-    accountId: 0,
+    accountId,
+    categoryId: 0,
     instrumentId: 0,
     transactionId: 0,
     parentCategoryId: 0,
