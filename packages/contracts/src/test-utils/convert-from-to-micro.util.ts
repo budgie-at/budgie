62272629447cfb3f46b27ad8acd445a3761f2f36
liaugust <ivan.liaugust@gmail.com>
2025-11-10 22:52:37 +0200

    fix: rename method; remove useless test-case

--- packages/contracts/src/test-utils/convert-from-to-micro.util.ts
+++
@@ -1,7 +0,0 @@
-import { PRECISION } from '../transaction/constant/precision.constant';
-
-export const convertFromToMicro = (fromAmountMicro: number, exchangeRate: number): number => {
-    const rateScaled = Math.round(exchangeRate * PRECISION);
-
-    return Math.round((fromAmountMicro * PRECISION) / rateScaled);
-};
--- packages/contracts/src/test-utils/quote-micro-to-base-micro.util.ts
+++ packages/contracts/src/test-utils/quote-micro-to-base-micro.util.ts
@@ -1,6 +1,6 @@
 import { PRECISION } from '../transaction/constant/precision.constant';
 
-export const convertToFromMicro = (toAmountMicro: number, exchangeRate: number): number => {
+export const quoteMicroToBaseMicro = (toAmountMicro: number, exchangeRate: number): number => {
     const rateScaled = Math.round(exchangeRate * PRECISION);
 
     return Math.round((toAmountMicro * rateScaled) / PRECISION);
