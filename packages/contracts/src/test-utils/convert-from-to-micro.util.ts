5a964f4a8f0af2ec1e52ef9e858e7732b9720cd1
liaugust <ivan.liaugust@gmail.com>
2025-11-10 22:43:52 +0200

    feat: update migrations

--- packages/contracts/src/test-utils/convert-from-to-micro.util.ts
+++ packages/contracts/src/test-utils/convert-from-to-micro.util.ts
@@ -1,12 +1,7 @@
-// SELL conversion helper (matches refine):
-// We balance in the TO currency.
-// FROM micro -> TO micro with inverse of fixed-point rate:
-//   rateScaled = round(exchangeRate * PRECISION)
-//   toMicro    = round((fromMicro * PRECISION) / rateScaled)
 import { PRECISION } from '../transaction/constant/precision.constant';
 
 export const convertFromToMicro = (fromAmountMicro: number, exchangeRate: number): number => {
-    const rateScaledInteger = Math.round(exchangeRate * PRECISION);
+    const rateScaled = Math.round(exchangeRate * PRECISION);
 
-    return Math.round((fromAmountMicro * PRECISION) / rateScaledInteger);
+    return Math.round((fromAmountMicro * PRECISION) / rateScaled);
 };
