3ed4fdb776d18480e35bae0546e1aae2c49211d4
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-10 17:44:43 +0100

    ci: expo setup

--- packages/app/app.config.js
+++ packages/app/app.config.js
@@ -5,7 +5,7 @@ const IS_DEV = APP_VARIANT === 'development';
 const IS_PREVIEW = APP_VARIANT === 'preview';
 
 const getUniqueIdentifier = isAndroid => {
-    const prefix = isAndroid ? 'com.vitaliiyehorov.suuudokuuu' : 'com.vitalyiegorov.suuudokuuu';
+    const prefix = isAndroid ? 'com.vitaliiyehorov.budgie' : 'com.vitalyiegorov.budgie';
 
     if (IS_DEV) {
         return `${prefix}.dev`;
@@ -20,21 +20,21 @@ const getUniqueIdentifier = isAndroid => {
 
 const getAppName = () => {
     if (IS_DEV) {
-        return 'suuudokuuu (Dev)';
+        return 'budgie (Dev)';
     }
 
     if (IS_PREVIEW) {
-        return 'suuudokuuu (Preview)';
+        return 'budgie (Preview)';
     }
 
-    return 'suuudokuuu';
+    return 'budgie';
 };
 
 export default ({ config }) => ({
     ...config,
     name: getAppName(),
-    slug: 'suuudokuuu',
-    scheme: 'suuudokuuu',
+    slug: 'budgie',
+    scheme: 'budgie',
     version: rootPkg.version,
     orientation: 'portrait',
     userInterfaceStyle: 'automatic',
@@ -45,7 +45,7 @@ export default ({ config }) => ({
         config: {
             usesNonExemptEncryption: false
         },
-        associatedDomains: ['applinks:suuudokuuu.com'],
+        associatedDomains: ['applinks:budgie.at'],
         icon: {
             dark: './assets/icons/ios-dark.png',
             light: './assets/icons/ios-light.png',
@@ -66,7 +66,7 @@ export default ({ config }) => ({
                 data: [
                     {
                         scheme: 'https',
-                        host: '*.suuudokuuu.com',
+                        host: '*.budgie.at',
                         pathPrefix: '/'
                     }
                 ],
@@ -80,12 +80,12 @@ export default ({ config }) => ({
     },
     extra: {
         eas: {
-            projectId: '4a70028a-5f9e-4ab6-9389-82d8b8b6c833'
+            projectId: '41569eb3-e5c7-41f2-bea0-200d87a7fc36'
         }
     },
     owner: 'vitalyiegorov',
     updates: {
-        url: 'https://u.expo.dev/4a70028a-5f9e-4ab6-9389-82d8b8b6c833'
+        url: 'https://u.expo.dev/41569eb3-e5c7-41f2-bea0-200d87a7fc36'
     },
     plugins: [
         'expo-sqlite',
@@ -102,7 +102,7 @@ export default ({ config }) => ({
                 }
             }
         ],
-        ['expo-router', { origin: 'https://www.suuudokuuu.com/' }],
+        ['expo-router', { origin: 'https://www.budgie.at/' }],
         [
             'expo-font',
             {
