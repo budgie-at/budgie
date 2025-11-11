72c895b7701464d96cc4ab86ed687f677f14949c
liaugust <ivan.liaugust@gmail.com>
2025-11-11 18:19:14 +0200

    fix: update migrations

--- packages/contracts/src/transaction-to-tag/table/transaction-to-tag-entity.table.ts
+++ packages/contracts/src/transaction-to-tag/table/transaction-to-tag-entity.table.ts
@@ -1,7 +1,7 @@
 import { int, primaryKey, sqliteTable } from 'drizzle-orm/sqlite-core';
 
 export const TransactionToTagEntityTable = sqliteTable(
-    'transactions_to_tags',
+    'transaction_tags',
     {
         transactionId: int('transaction_id', { mode: 'number' }).notNull(),
         tagId: int('tag_id', { mode: 'number' }).notNull()
