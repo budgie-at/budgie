e2c464b7358e9fc82e43bc6aca6f9acd2334c227
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-11 07:46:27 +0100

    ci: fix expo web build

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -4,6 +4,16 @@ const { withNativewind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// HINT: https://docs.expo.dev/versions/latest/sdk/sqlite/#web-setup
+config.resolver.assetExts.push('wasm');
+config.server.enhanceMiddleware = middleware => {
+    return (req, res, next) => {
+        res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+        middleware(req, res, next);
+    };
+};
+
 config.resolver.sourceExts.push('sql');
 
 module.exports = withNativewind(config);
