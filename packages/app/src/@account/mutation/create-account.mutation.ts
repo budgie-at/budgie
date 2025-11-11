1c4ad875ea1335d896d411c3aced2599c56ac002
liaugust <ivan.liaugust@gmail.com>
2025-11-11 13:17:55 +0200

    fix: remove unused

--- packages/app/src/@account/mutation/create-account.mutation.ts
+++
@@ -1,7 +0,0 @@
-import { AccountEntityTable } from '@budgie/contracts';
-
-import { db } from '../../drizzle/db/db';
-
-import type { AccountCreateEntityInterface } from '@budgie/contracts';
-
-export const createAccountMutation = async (input: AccountCreateEntityInterface) => db.insert(AccountEntityTable).values([input]);
