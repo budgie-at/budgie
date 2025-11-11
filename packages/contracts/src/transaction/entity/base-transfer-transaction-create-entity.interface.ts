ca2c29d1990460b45ebc875a391fa69e7f9ec5c0
liaugust <ivan.liaugust@gmail.com>
2025-11-11 13:17:55 +0200

    fix: remove unused

--- packages/contracts/src/transaction/entity/base-transfer-transaction-create-entity.interface.ts
+++
@@ -1,5 +0,0 @@
-import { infer } from 'zod';
-
-import { BaseTransferTransactionCreateEntitySchema } from '../schema/base-transfer-transaction-create-entity.schema';
-
-export interface BaseTransferTransactionCreateEntityInterface extends infer<typeof BaseTransferTransactionCreateEntitySchema> {}
