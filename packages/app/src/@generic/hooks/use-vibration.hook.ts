70d58dcc5f465d46ba33f9a1bb374ae7f1eb6d45
liaugust <ivan.liaugust@gmail.com>
2025-11-14 00:26:49 +0200

    feat: add isVibrationEnabled to the settings table

--- packages/app/src/@generic/hooks/use-vibration.hook.ts
+++ packages/app/src/@generic/hooks/use-vibration.hook.ts
@@ -1,15 +1,22 @@
 import * as Haptics from 'expo-haptics';
 import { ImpactFeedbackStyle, NotificationFeedbackType } from 'expo-haptics/src/Haptics.types';
 
-// TODO add "hasVibration" to the settings table
+import { useGetSettingsQuery } from '../../@settings/query/use-get-settings.query';
 
 export const useVibration = (): [notification: (type: NotificationFeedbackType) => void, impact: (style: ImpactFeedbackStyle) => void] => {
+    const { settings } = useGetSettingsQuery();
+    const { isVibrationEnabled } = settings;
+
     const hapticNotification = (type: NotificationFeedbackType = NotificationFeedbackType.Success) => {
-        void Haptics.notificationAsync(type);
+        if (isVibrationEnabled) {
+            void Haptics.notificationAsync(type);
+        }
     };
 
     const hapticImpact = (style: ImpactFeedbackStyle = ImpactFeedbackStyle.Medium) => {
-        void Haptics.impactAsync(style);
+        if (isVibrationEnabled) {
+            void Haptics.impactAsync(style);
+        }
     };
 
     return [hapticNotification, hapticImpact] as const;
