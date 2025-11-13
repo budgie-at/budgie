24faf2ec72c9ebb87abbea8830c04d78bd8f226e
liaugust <ivan.liaugust@gmail.com>
2025-11-13 22:09:20 +0200

    fix: remove redux

--- packages/app/src/@generic/hooks/use-vibration.hook.ts
+++ packages/app/src/@generic/hooks/use-vibration.hook.ts
@@ -1,21 +1,15 @@
 import * as Haptics from 'expo-haptics';
 import { ImpactFeedbackStyle, NotificationFeedbackType } from 'expo-haptics/src/Haptics.types';
 
-import { useAppSelector } from './use-app-selector.hook';
+// TODO add "hasVibration" to the settings table
 
 export const useVibration = (): [notification: (type: NotificationFeedbackType) => void, impact: (style: ImpactFeedbackStyle) => void] => {
-    const { hasVibration } = useAppSelector(state => state.settings);
-
     const hapticNotification = (type: NotificationFeedbackType = NotificationFeedbackType.Success) => {
-        if (hasVibration) {
-            void Haptics.notificationAsync(type);
-        }
+        void Haptics.notificationAsync(type);
     };
 
     const hapticImpact = (style: ImpactFeedbackStyle = ImpactFeedbackStyle.Medium) => {
-        if (hasVibration) {
-            void Haptics.impactAsync(style);
-        }
+        void Haptics.impactAsync(style);
     };
 
     return [hapticNotification, hapticImpact] as const;
