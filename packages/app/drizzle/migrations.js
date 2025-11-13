2c2382790d062dbf58b0250238beaf9c7db7d971
liaugust <ivan.liaugust@gmail.com>
2025-11-13 20:21:04 +0200

    fix: update migration

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -2,13 +2,11 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_initial.sql';
-import m0001 from './0001_settings.sql';
 
   export default {
     journal,
     migrations: {
-      m0000,
-m0001
+      m0000
     }
   }
   
