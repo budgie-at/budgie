1c1172906f940a821a30871b4108fb5d92bd89f3
liaugust <ivan.liaugust@gmail.com>
2025-11-13 20:21:04 +0200

    fix: update migration

--- packages/app/drizzle/0000_initial.sql
+++ packages/app/drizzle/0000_initial.sql
@@ -58,6 +58,21 @@ CREATE TABLE `instruments` (
 	`symbol` text NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE `settings` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`createdAt` integer DEFAULT (unixepoch()) NOT NULL,
+	`updatedAt` integer DEFAULT (unixepoch()) NOT NULL,
+	`deletedAt` integer,
+	`locale` text NOT NULL,
+	`language` text DEFAULT 'en' NOT NULL,
+	`default_account_id` integer,
+	`default_instrument_id` integer,
+	`theme` text DEFAULT 'SYSTEM' NOT NULL,
+	`hide_cents` integer DEFAULT false NOT NULL,
+	FOREIGN KEY (`default_account_id`) REFERENCES `accounts`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`default_instrument_id`) REFERENCES `instruments`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `tags` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`createdAt` integer DEFAULT (unixepoch()) NOT NULL,
