0efb0b28015144caaf47025d6c45a30de4ca94fe
liaugust <ivan.liaugust@gmail.com>
2025-11-15 14:55:34 +0200

    update migrations

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -1,13 +1,12 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_initial.sql';
-import m0001 from './0001_ambitious_gunslinger.sql';
+import m0000 from './0000_dark_christian_walker.sql';
 
-export default {
+  export default {
     journal,
     migrations: {
-        m0000,
-        m0001
+      m0000
     }
-};
+  }
+  
