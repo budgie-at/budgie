e35df1c988cc9c7a21a970b7cbc75b791e27f6db
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-15 11:14:41 +0100

    fix(app): background task

--- packages/app/src/@exchange-rate/task/exchange-rate-sync.task.ts
+++ packages/app/src/@exchange-rate/task/exchange-rate-sync.task.ts
@@ -1,5 +1,4 @@
- 
-import * as BackgroundFetch from 'expo-background-fetch';
+import * as BackgroundTask from 'expo-background-task';
 import * as TaskManager from 'expo-task-manager';
 
 import { EXCHANGE_RATE_SYNC_TASK } from '../constant/exchange-rate-sync-task.constant';
@@ -8,9 +7,9 @@ import { exchangeRatesService } from '../service/exchange-rates-sync.service';
 TaskManager.defineTask(EXCHANGE_RATE_SYNC_TASK, async () => {
     try {
         await exchangeRatesService.sync();
-
-        return BackgroundFetch.BackgroundFetchResult.NewData;
     } catch {
-        return BackgroundFetch.BackgroundFetchResult.Failed;
+        return BackgroundTask.BackgroundTaskResult.Failed;
     }
+
+    return BackgroundTask.BackgroundTaskResult.Success;
 });
