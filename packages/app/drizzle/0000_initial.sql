aa77b80b1f744760d043ca17b6d864d38a97b6e1
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-15 12:22:04 +0100

    fix(app): background task

--- packages/app/drizzle/0000_initial.sql
+++ packages/app/drizzle/0000_initial.sql
@@ -59,20 +59,20 @@ CREATE TABLE `instruments` (
 );
 --> statement-breakpoint
 CREATE TABLE `settings` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`createdAt` integer DEFAULT (unixepoch()) NOT NULL,
-	`updatedAt` integer DEFAULT (unixepoch()) NOT NULL,
-	`deletedAt` integer,
-	`locale` text NOT NULL,
-	`language` text DEFAULT 'en' NOT NULL,
-	`default_account_id` integer,
-	`default_instrument_id` integer,
-	`theme` text DEFAULT 'SYSTEM' NOT NULL,
-	`show_cents` integer DEFAULT true NOT NULL,
-	`is_vibration_enabled` integer DEFAULT true NOT NULL,
-	FOREIGN KEY (`default_account_id`) REFERENCES `accounts`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`default_instrument_id`) REFERENCES `instruments`(`id`) ON UPDATE no action ON DELETE no action
-);
+    `id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `createdAt` integer DEFAULT (unixepoch()) NOT NULL,
+    `updatedAt` integer DEFAULT (unixepoch()) NOT NULL,
+    `deletedAt` integer,
+    `locale` text NOT NULL,
+    `language` text DEFAULT 'en' NOT NULL,
+    `default_account_id` integer,
+    `default_instrument_id` integer,
+    `theme` text DEFAULT 'SYSTEM' NOT NULL,
+    `show_cents` integer DEFAULT true NOT NULL,
+    `is_vibration_enabled` integer DEFAULT true NOT NULL,
+    FOREIGN KEY (`default_account_id`) REFERENCES `accounts`(`id`) ON UPDATE no action ON DELETE no action,
+    FOREIGN KEY (`default_instrument_id`) REFERENCES `instruments`(`id`) ON UPDATE no action ON DELETE no action
+    );
 --> statement-breakpoint
 CREATE TABLE `tags` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
