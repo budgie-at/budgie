d7c642ba954d9b6c1bcad6401a9dfc6bc2dbefb7
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-11-15 12:16:35 +0100

    fix(app): background task

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -3,11 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_initial.sql';
 import m0001 from './0001_seed_instruments.sql';
+import m0002 from './0002_amazing_ulik.sql';
 
-export default {
+  export default {
     journal,
     migrations: {
-        m0000,
-        m0001
+      m0000,
+m0001,
+m0002
     }
-};
+  }
+  
