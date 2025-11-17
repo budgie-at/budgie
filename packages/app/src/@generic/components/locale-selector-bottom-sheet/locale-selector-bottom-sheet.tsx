529a3368350dd9cd3f720ab5fcaa77333de125c5
liaugust <ivan.liaugust@gmail.com>
2025-11-17 03:14:39 +0200

    feat: add locale setting

---
+++ packages/app/src/@generic/components/locale-selector-bottom-sheet/locale-selector-bottom-sheet.tsx
@@ -0,0 +1,66 @@
+import { useLingui } from '@lingui/react/macro';
+import { RefObject, useState } from 'react';
+
+import { LOCALES, LocaleInfoWithDetailsInterface } from '../../constant/locales.constant';
+import { BottomSheetInterface } from '../../interface/bottom-sheet.interface';
+import { SearchableListBottomSheet } from '../bottom-sheet-searchable-list/bottom-sheet-searchable-list';
+import { LocaleSelectorCard } from '../locale-selector-card/locale-selector-card';
+
+interface Props {
+    readonly locale: string;
+    readonly onSelect: (locale: string) => void;
+    readonly ref: RefObject<BottomSheetInterface | null>;
+}
+
+const snapPoints = ['70%'];
+
+export const LocaleSelectorBottomSheet = ({ ref, locale, onSelect }: Props) => {
+    const [search, setSearch] = useState('');
+    const { t, i18n } = useLingui();
+
+    const filteredLocales = LOCALES.filter(
+        ({ name, languageTag }) =>
+            i18n.t(name).toLowerCase().includes(search.toLowerCase()) || languageTag.toLowerCase().includes(search.toLowerCase())
+    );
+
+    const handleSelect = (locale: string) => {
+        onSelect(locale);
+        ref.current?.close();
+    };
+
+    const keyExtractor = (item: LocaleInfoWithDetailsInterface) => item.languageTag;
+
+    const renderItem = ({ item }: { item: LocaleInfoWithDetailsInterface }) => (
+        <LocaleSelectorCard
+            key={item.languageTag}
+            isSelected={item.languageTag === locale}
+            code={item.languageTag}
+            emoji={item.emoji}
+            name={i18n.t(item.name)}
+            onSelect={handleSelect}
+        />
+    );
+
+    const flatListProps = {
+        className: 'pt-3 px-xl',
+        contentContainerClassName: 'gap-y-lg'
+    };
+
+    return (
+        <SearchableListBottomSheet
+            ref={ref}
+            snapPoints={snapPoints}
+            title={t`Select Locale`}
+            description={t`Choose your preferred locale for date and number formatting`}
+            onSearchChange={setSearch}
+            searchPlaceholder={t`Search locales...`}
+            search={search}
+            keyExtractor={keyExtractor}
+            renderItem={renderItem}
+            emptyDescription={t`Try a different search term`}
+            emptyTitle={t`No locales found`}
+            data={filteredLocales}
+            flatListProps={flatListProps}
+        />
+    );
+};
