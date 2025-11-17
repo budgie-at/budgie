339b3de98640fa82424ad5ca7113ba4f8cf36ec2
liaugust <ivan.liaugust@gmail.com>
2025-11-17 04:23:31 +0200

    feat: add cents setting

--- packages/app/src/settings/components/theme-switch/theme-switch.tsx
+++ packages/app/src/settings/components/theme-switch/theme-switch.tsx
@@ -1,8 +1,8 @@
 import { useLingui } from '@lingui/react/macro';
 import { useContext } from 'react';
-import { Switch } from 'react-native';
 
 import { CircleIcon } from '../../../@generic/components/circle-icon/circle-icon';
+import { ThemedSwitch } from '../../../@generic/components/themed-switch/themed-switch';
 import { ICONS } from '../../../@generic/constant/icons.constant';
 import { ThemeContext } from '../../../theme/context/theme.context';
 import { SettingsCard } from '../settings-card/settings-card';
@@ -11,28 +11,11 @@ export const ThemeSwitch = () => {
     const { toggleColorSchema, isDarkColorSchema } = useContext(ThemeContext);
     const { t } = useLingui();
 
-    const thumbColor = isDarkColorSchema ? '#000000' : '#ffffff';
-    const iosBackgroundColor = isDarkColorSchema ? '#ffffff' : '#000000';
-
-    const trackColor = {
-        false: isDarkColorSchema ? '#000000' : '#ffffff',
-        true: isDarkColorSchema ? '#ffffff' : '#000000'
-    };
-
     return (
         <SettingsCard
             title={t`Dark Mode`}
             description={t`Switch between light and dark themes`}
-            right={
-                <Switch
-                    className="my-auto"
-                    onChange={toggleColorSchema}
-                    value={isDarkColorSchema}
-                    trackColor={trackColor}
-                    thumbColor={thumbColor}
-                    ios_backgroundColor={iosBackgroundColor}
-                />
-            }
+            right={<ThemedSwitch className="my-auto" onChange={toggleColorSchema} value={isDarkColorSchema} />}
             left={<CircleIcon size="1_5xl" icon={ICONS.Moon} variant="ghost" border={false} />}
         />
     );
