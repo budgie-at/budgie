bd24c68aad108233b24b79becc28065fa1cd6355
liaugust <ivan.liaugust@gmail.com>
2025-11-17 02:48:03 +0200

    feat: add bottom-sheet searchable list

--- packages/contracts/src/transaction/schema/transfer-transaction-create-entity-schema-invalid.spec.ts
+++ packages/contracts/src/transaction/schema/transfer-transaction-create-entity-schema-invalid.spec.ts
@@ -3,10 +3,10 @@ import { prettifyError } from 'zod';
 
 import { isDefined } from '@rnw-community/shared';
 
+import { PRECISION } from '../../generic/constant/precision.constant';
 import { createTransferTransactionEntryInput } from '../../test-utils/create-transfer-transaction-entry-input.util';
 import { createTransferTransactionInput } from '../../test-utils/create-transfer-transaction-input.util';
 import { TransactionEntryTypeEnum } from '../../transaction-entry/enum/transaction-entry-type.enum';
-import { PRECISION } from '../../generic/constant/precision.constant';
 import { TOLERANCE_MICRO } from '../constant/tolerance-micro.constant';
 import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
 
