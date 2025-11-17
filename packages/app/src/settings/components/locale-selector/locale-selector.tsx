3715193177cc01964d0a1d3f1b8903721927045e
liaugust <ivan.liaugust@gmail.com>
2025-11-17 11:18:20 +0200

    fix: resolve cpd

--- packages/app/src/settings/components/locale-selector/locale-selector.tsx
+++ packages/app/src/settings/components/locale-selector/locale-selector.tsx
@@ -1,18 +1,14 @@
 import { useLingui } from '@lingui/react/macro';
 import { useRef } from 'react';
-import { View } from 'react-native';
 
 import { isDefined } from '@rnw-community/shared';
 
-import { CircleIcon } from '../../../@generic/components/circle-icon/circle-icon';
-import { Icon } from '../../../@generic/components/icon/icon';
-import { ICONS } from '../../../@generic/constant/icons.constant';
 import { BottomSheetInterface } from '../../../@generic/interface/bottom-sheet.interface';
 import { LocaleSelectorBottomSheet } from '../../../i18n/components/locale-selector-bottom-sheet/locale-selector-bottom-sheet';
 import { LOCALES } from '../../../i18n/constant/locales.constant';
 import { useSettingsContext } from '../../context/settings.context';
 import { updateSettingsMutation } from '../../mutation/update-settings.mutation';
-import { SettingsCard } from '../settings-card/settings-card';
+import { GenericSelectorCard } from '../generic-selector-card/generic-selector-card';
 
 export const LocaleSelector = () => {
     const ref = useRef<BottomSheetInterface | null>(null);
@@ -23,6 +19,7 @@ export const LocaleSelector = () => {
 
     const updateDefaultInstrument = async (locale: string) => {
         await updateSettingsMutation({ locale });
+        i18n.activate(locale);
     };
 
     const handleOpen = () => void ref.current?.open();
@@ -33,16 +30,12 @@ export const LocaleSelector = () => {
 
     return (
         <>
-            <SettingsCard
-                right={
-                    <View className="ml-auto">
-                        <Icon className="text-primary" icon={ICONS.ChevronRight} />
-                    </View>
-                }
-                left={<CircleIcon icon={ICONS.MapPinIcon} variant="warning" size="1_5xl" border={false} />}
-                onPress={handleOpen}
+            <GenericSelectorCard
+                icon='MapPinIcon'
                 title={t`Locale`}
-                description={i18n.t(selectedLocale.name)}
+                onPress={handleOpen}
+                iconVariant='warning'
+                description={`${selectedLocale.emoji} ${i18n.t(selectedLocale.name)}`}
             />
 
             <LocaleSelectorBottomSheet locale={settings.locale} onSelect={updateDefaultInstrument} ref={ref} />
