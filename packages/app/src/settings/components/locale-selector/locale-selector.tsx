672dda18cacbb83e2633ce6b45409c8941fa4b84
liaugust <ivan.liaugust@gmail.com>
2025-11-17 03:14:39 +0200

    feat: add locale setting

---
+++ packages/app/src/settings/components/locale-selector/locale-selector.tsx
@@ -0,0 +1,51 @@
+import { useLingui } from '@lingui/react/macro';
+import { useRef } from 'react';
+import { View } from 'react-native';
+
+import { isDefined } from '@rnw-community/shared';
+
+import { CircleIcon } from '../../../@generic/components/circle-icon/circle-icon';
+import { Icon } from '../../../@generic/components/icon/icon';
+import { LocaleSelectorBottomSheet } from '../../../@generic/components/locale-selector-bottom-sheet/locale-selector-bottom-sheet';
+import { ICONS } from '../../../@generic/constant/icons.constant';
+import { LOCALES } from '../../../@generic/constant/locales.constant';
+import { BottomSheetInterface } from '../../../@generic/interface/bottom-sheet.interface';
+import { useSettingsContext } from '../../context/settings.context';
+import { updateSettingsMutation } from '../../mutation/update-settings.mutation';
+import { SettingsCard } from '../settings-card/settings-card';
+
+export const LocaleSelector = () => {
+    const ref = useRef<BottomSheetInterface | null>(null);
+    const { settings } = useSettingsContext();
+    const { i18n, t } = useLingui();
+
+    const selectedLocale = LOCALES.find(({ languageTag }) => languageTag === settings.locale);
+
+    const updateDefaultInstrument = async (locale: string) => {
+        await updateSettingsMutation({ locale });
+    };
+
+    const handleOpen = () => void ref.current?.open();
+
+    if (!isDefined(selectedLocale)) {
+        return null;
+    }
+
+    return (
+        <>
+            <SettingsCard
+                right={
+                    <View className="ml-auto">
+                        <Icon className="text-primary" icon={ICONS.ChevronRight} />
+                    </View>
+                }
+                left={<CircleIcon icon={ICONS.MapPinIcon} variant="warning" size="1_5xl" border={false} />}
+                onPress={handleOpen}
+                title={t`Locale`}
+                description={i18n.t(selectedLocale.name)}
+            />
+
+            <LocaleSelectorBottomSheet locale={settings.locale} onSelect={updateDefaultInstrument} ref={ref} />
+        </>
+    );
+};
