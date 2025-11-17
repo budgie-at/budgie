3f39630f37d5b79a20724b52c861189a6576db19
liaugust <ivan.liaugust@gmail.com>
2025-11-17 03:14:39 +0200

    feat: add locale setting

--- packages/app/src/app/(tabs)/settings.tsx
+++ packages/app/src/app/(tabs)/settings.tsx
@@ -1,10 +1,13 @@
 import { useLingui } from '@lingui/react/macro';
 import { ScrollView, Text, View } from 'react-native';
 
+import { CircleIcon } from '../../@generic/components/circle-icon/circle-icon';
 import { Page } from '../../@generic/components/page/page';
 import { Separator } from '../../@generic/components/separator/separator';
+import { ICONS } from '../../@generic/constant/icons.constant';
+import { LocaleSelector } from '../../settings/components/locale-selector/locale-selector';
+import { SettingsCard } from '../../settings/components/settings-card/settings-card';
 import { SettingsGroup } from '../../settings/components/settings-group/settings-group';
-import { SettingsTextItem } from '../../settings/components/settings-text-item/settings-text-item';
 import { ThemeSwitch } from '../../settings/components/theme-switch/theme-switch';
 
 export default function SettingsPage() {
@@ -19,24 +22,25 @@ export default function SettingsPage() {
 
                 <View className={'pt-5xl gap-y-7xl'}>
                     <SettingsGroup title={t`Privacy`}>
-                        <SettingsTextItem
-                            variant={'positive'}
+                        <SettingsCard
+                            className="items-baseline"
                             title={t`100% Offline & Private`}
                             description={t`All your financial data is stored locally on your device. No cloud sync, no tracking, no data sharing.`}
-                            icon="Shield"
+                            left={<CircleIcon size="1_5xl" icon={ICONS.Shield} variant="positive" border={false} />}
                         />
                     </SettingsGroup>
 
                     <SettingsGroup title={t`General`}>
                         <ThemeSwitch />
+                        <LocaleSelector />
                     </SettingsGroup>
 
                     <SettingsGroup title={t`About`}>
-                        <SettingsTextItem
-                            variant={'ghost'}
+                        <SettingsCard
                             title={t`BudgetAI`}
+                            className="items-baseline"
                             description={t`AI-powered budgeting app with complete privacy. All data processing happens locally on your device. \nVersion 1.0.0`}
-                            icon="Database"
+                            left={<CircleIcon size="1_5xl" icon={ICONS.Database} variant="ghost" border={false} />}
                         />
                     </SettingsGroup>
                 </View>
