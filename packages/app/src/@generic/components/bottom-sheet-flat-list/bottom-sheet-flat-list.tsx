c5f57f11ba17821974b8814a109108b4d9a8b293
liaugust <ivan.liaugust@gmail.com>
2025-11-17 02:48:03 +0200

    feat: add bottom-sheet searchable list

---
+++ packages/app/src/@generic/components/bottom-sheet-flat-list/bottom-sheet-flat-list.tsx
@@ -0,0 +1,15 @@
+import { BottomSheetFlatList as GorhomBottomSheetFlatList } from '@gorhom/bottom-sheet';
+import { styled } from 'nativewind';
+import { ComponentProps } from 'react';
+
+interface Props extends ComponentProps<typeof GorhomBottomSheetFlatList> {
+    readonly className?: string;
+    readonly contentContainerClassName?: string;
+}
+
+const Wrapper = styled(GorhomBottomSheetFlatList, {
+    className: 'style',
+    contentContainerClassName: 'contentContainerStyle'
+});
+
+export const BottomSheetFlatList = ({ children, ...rest }: Props) => <Wrapper {...rest}>{children}</Wrapper>;
--- packages/contracts/src/transaction/schema/transfer-transaction-create-entity-schema-valid.spec.ts
+++ packages/contracts/src/transaction/schema/transfer-transaction-create-entity-schema-valid.spec.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from '@jest/globals';
 
+import { PRECISION } from '../../generic/constant/precision.constant';
 import { createTransferTransactionEntryInput } from '../../test-utils/create-transfer-transaction-entry-input.util';
 import { createTransferTransactionInput } from '../../test-utils/create-transfer-transaction-input.util';
 import { TransactionEntryTypeEnum } from '../../transaction-entry/enum/transaction-entry-type.enum';
-import { PRECISION } from '../../generic/constant/precision.constant';
 import { TOLERANCE_MICRO } from '../constant/tolerance-micro.constant';
 import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
 
