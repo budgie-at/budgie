65c52af6abde3475caa08108d9c09ee428cd9e7c
liaugust <ivan.liaugust@gmail.com>
2025-11-17 02:42:10 +0200

    fix: update create-transaction bottom-sheet

--- packages/app/src/transaction/components/create-transaction-bottom-sheet/create-transaction-bottom-sheet.tsx
+++ packages/app/src/transaction/components/create-transaction-bottom-sheet/create-transaction-bottom-sheet.tsx
@@ -3,6 +3,7 @@ import { Trans, useLingui } from '@lingui/react/macro';
 import { Text, View } from 'react-native';
 
 import { BottomSheet } from '../../../@generic/components/bottom-sheet/bottom-sheet';
+import { BottomSheetView } from '../../../@generic/components/bottom-sheet-view/bottom-sheet-view';
 import { BottomSheetInterface } from '../../../@generic/interface/bottom-sheet.interface';
 import { CreateTransactionCard } from '../create-transaction-card/create-transaction-card';
 
@@ -17,41 +18,43 @@ export const CreateTransactionBottomSheet = ({ ref }: Props) => {
 
     return (
         <BottomSheet ref={ref}>
-            <View className="gap-y-1 mb-10">
-                <Text className="text-center text-xl text-primary font-semibold">
-                    <Trans>New Transaction</Trans>
-                </Text>
-                <Text className="text-center text-sm text-secondary-foreground">
-                    <Trans>Choose a type to get started</Trans>
-                </Text>
-            </View>
+            <BottomSheetView>
+                <View className="gap-y-1 mb-10">
+                    <Text className="text-center text-xl text-primary font-semibold">
+                        <Trans>New Transaction</Trans>
+                    </Text>
+                    <Text className="text-center text-sm text-secondary-foreground">
+                        <Trans>Choose a type to get started</Trans>
+                    </Text>
+                </View>
 
-            <View className="gap-y-3.5">
-                <CreateTransactionCard
-                    description={t`Money you spend`}
-                    icon="TrendingDown"
-                    title={t`Expense`}
-                    type={TransactionTypeEnum.EXPENSE}
-                />
-                <CreateTransactionCard
-                    description={t`Money you earn`}
-                    icon="TrendingUp"
-                    title={t`Income`}
-                    type={TransactionTypeEnum.INCOME}
-                />
-                <CreateTransactionCard
-                    description={t`Move between accounts`}
-                    icon="ArrowRightLeft"
-                    title={t`Transfer`}
-                    type={TransactionTypeEnum.TRANSFER}
-                />
-                <CreateTransactionCard
-                    description={t`Loans & credit cards`}
-                    icon="CreditCard"
-                    title={t`Debt`}
-                    type={TransactionTypeEnum.DEBT}
-                />
-            </View>
+                <View className="gap-y-3.5">
+                    <CreateTransactionCard
+                        description={t`Money you spend`}
+                        icon="TrendingDown"
+                        title={t`Expense`}
+                        type={TransactionTypeEnum.EXPENSE}
+                    />
+                    <CreateTransactionCard
+                        description={t`Money you earn`}
+                        icon="TrendingUp"
+                        title={t`Income`}
+                        type={TransactionTypeEnum.INCOME}
+                    />
+                    <CreateTransactionCard
+                        description={t`Move between accounts`}
+                        icon="ArrowRightLeft"
+                        title={t`Transfer`}
+                        type={TransactionTypeEnum.TRANSFER}
+                    />
+                    <CreateTransactionCard
+                        description={t`Loans & credit cards`}
+                        icon="CreditCard"
+                        title={t`Debt`}
+                        type={TransactionTypeEnum.DEBT}
+                    />
+                </View>
+            </BottomSheetView>
         </BottomSheet>
     );
 };
