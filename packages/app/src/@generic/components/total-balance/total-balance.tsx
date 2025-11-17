15be7f218fcf013f1458637564d43012bc206e84
liaugust <ivan.liaugust@gmail.com>
2025-11-17 10:34:56 +0200

    fix: resolve review comment

--- packages/app/src/@generic/components/total-balance/total-balance.tsx
+++ packages/app/src/@generic/components/total-balance/total-balance.tsx
@@ -1,13 +1,14 @@
-import { PRECISION } from '@budgie/contracts';
 import { Trans } from '@lingui/react/macro';
 import { Text, View } from 'react-native';
 
 import { useSettingsContext } from '../../../settings/context/settings.context';
 import { FormattedMoney } from '../formatted-money/formatted-money';
 
-const MOCK_BALANCE = (1_123_213.12)
+interface Props {
+    readonly balance: number;
+}
 
-export const TotalBalance = () => {
+export const TotalBalance = ({ balance }: Props) => {
     const { defaultCurrency, decimalPlaces } = useSettingsContext();
 
     return (
@@ -17,7 +18,7 @@ export const TotalBalance = () => {
             </Text>
 
             <FormattedMoney decimalPlaces={decimalPlaces} fontSize={60} minFontSize={24} maxFontSize={60} currency={defaultCurrency}>
-                {MOCK_BALANCE * PRECISION}
+                {balance}
             </FormattedMoney>
         </View>
     );
--- packages/app/src/@generic/hooks/use-format-money.hook.ts
+++ packages/app/src/@generic/hooks/use-format-money.hook.ts
@@ -6,10 +6,18 @@ import { convertFromMicroUnits } from '../utils/convert-from-micro-units.util';
 
 import { useLocaleInfo } from './use-locale-info.hook';
 
-export const useFormatMoney = (decimalPlaces: number) => {
+export const useFormatMoney = (decimalPlaces: number, currency: CurrencyEnum) => {
     const { languageCode, regionCode } = useLocaleInfo();
 
-    return (rawAmount: number, currency: CurrencyEnum) => {
+    const intl = new Intl.NumberFormat(`${languageCode}-${regionCode}`, {
+        currency,
+        style: 'currency',
+        useGrouping: true,
+        minimumFractionDigits: decimalPlaces,
+        maximumFractionDigits: decimalPlaces
+    });
+
+    return (rawAmount: number) => {
         const amount = convertFromMicroUnits(rawAmount).toString();
 
         if (!isNotEmptyString(amount)) {
@@ -20,14 +28,6 @@ export const useFormatMoney = (decimalPlaces: number) => {
         if (Number.isNaN(num)) {
             return '';
         }
-
-        const intl = new Intl.NumberFormat(`${languageCode}-${regionCode}`, {
-            currency,
-            style: 'currency',
-            useGrouping: true,
-            minimumFractionDigits: decimalPlaces,
-            maximumFractionDigits: decimalPlaces
-        });
 
         return intl.format(num);
     };
