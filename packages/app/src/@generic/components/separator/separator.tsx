e27f7a475e4926896fb2f53a0f7eb905e2e03bf0
liaugust <ivan.liaugust@gmail.com>
2025-11-17 04:01:30 +0200

    feat: add language setting

--- packages/app/src/@generic/components/separator/separator.tsx
+++ packages/app/src/@generic/components/separator/separator.tsx
@@ -4,11 +4,15 @@ import { styled } from 'nativewind';
 import { cn } from '../../utils/cn.util';
 
 interface Props {
-    className?: string;
+    readonly className?: string;
 }
 
 const Gradient = styled(LinearGradient);
 
 const colors = ['rgba(255,255,255,0.40)', 'rgba(0,0,0,0)'] as const;
+const start = { x: 0, y: 0 } as const;
+const end = { x: 1, y: 0 } as const;
 
-export const Separator = ({ className }: Props) => <Gradient className={cn('h-[2px] w-[100px]', className)} colors={colors} />;
+export const Separator = ({ className }: Props) => (
+    <Gradient className={cn('h-[2px] w-[100px]', className)} colors={colors} start={start} end={end} />
+);
