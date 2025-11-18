0efae429703f3cb6072e14c187c8c2ea80e75d53
liaugust <ivan.liaugust@gmail.com>
2025-11-18 21:23:32 +0200

    feat: add categories screen

--- packages/contracts/src/category/entity/category-update-entity.interface.ts
+++ packages/contracts/src/category/entity/category-update-entity.interface.ts
@@ -1,4 +1,4 @@
-import type { CategoryCreateEntitySchema } from '../schema/category-create-entity.schema';
+import type { CategoryUpdateEntitySchema } from '../schema/category-update-entity.schema';
 import type { infer } from 'zod';
 
-export interface CategoryCreateEntityInterface extends infer<typeof CategoryCreateEntitySchema> {}
+export interface CategoryUpdateEntityInterface extends infer<typeof CategoryUpdateEntitySchema> {}
