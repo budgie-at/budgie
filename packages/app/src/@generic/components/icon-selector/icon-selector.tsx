6620dbbaf36eb780d7162937d958da9444fea5c1
liaugust <ivan.liaugust@gmail.com>
2025-11-18 21:41:50 +0200

    wip

--- packages/app/src/@generic/components/icon-selector/icon-selector.tsx
+++ packages/app/src/@generic/components/icon-selector/icon-selector.tsx
@@ -6,9 +6,10 @@ import { BottomSheetInterface } from '../../interface/bottom-sheet.interface';
 import { IconSelectorBottomSheet } from '../icon-selector-bottom-sheet/icon-selector-bottom-sheet';
 import { IconSelectorCardBig } from '../icon-selector-card-big/icon-selector-card-big';
 import { IconSelectorCardSmall } from '../icon-selector-card-small/icon-selector-card-small';
+import { IconSelectorCardMedium } from '../icon-selector-card-medium/icon-selector-card-medium';
 
 interface Props {
-    readonly size: 'sm' | 'lg';
+    readonly size: 'sm' | 'md' | 'lg';
     readonly icon: UserIconNameEnum;
     readonly onSelect: (icon: UserIconNameEnum) => void;
 }
@@ -24,6 +25,7 @@ export const IconSelector = ({ onSelect, icon, size = 'sm' }: Props) => {
     return (
         <View>
             {size === 'sm' ? <IconSelectorCardSmall icon={icon} onPress={openIconSelector} /> : null}
+            {size === 'md' ? <IconSelectorCardMedium icon={icon} onPress={openIconSelector} /> : null}
             {size === 'lg' ? <IconSelectorCardBig icon={icon} onPress={openIconSelector} /> : null}
 
             <IconSelectorBottomSheet variant="default" onSelect={onSelect} ref={ref} selectedIcon={icon} />
