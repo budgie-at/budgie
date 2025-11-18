ff9a11dd7b17958cfecfd0dafd55e0ffa8e482c1
liaugust <ivan.liaugust@gmail.com>
2025-11-18 20:08:01 +0200

    fix: rename total-balance to net worth

--- packages/app/src/account/query/use-net-worth.query.ts
+++ packages/app/src/account/query/use-net-worth.query.ts
@@ -3,9 +3,9 @@ import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 import { accountRepository } from '../../@generic/drizzle/db/db';
 import { useSettingsContext } from '../../settings/context/settings.context';
 
-export const useAccountTotalBalanceQuery = () => {
+export const useNetWorthQuery = () => {
     const { defaultInstrument } = useSettingsContext();
-    const { data } = useLiveQuery(accountRepository.getTotalBalance(defaultInstrument.id));
+    const { data } = useLiveQuery(accountRepository.getNetWorth(defaultInstrument.id));
     const { total } = data.at(0) ?? { total: 0 };
 
     return total;
--- packages/contracts/src/account/repository/account.repository.ts
+++ packages/contracts/src/account/repository/account.repository.ts
@@ -54,7 +54,7 @@ export class AccountRepository {
         });
     }
 
-    getTotalBalance(defaultInstrumentId: number) {
+    getNetWorth(defaultInstrumentId: number) {
         return this.db
             .select({
                 total: sql<number>`
