5b0394957a5c33568ccd273cc0b37b32401e0a87
liaugust <ivan.liaugust@gmail.com>
2025-11-19 15:40:46 +0200

    feat: add categories screen

--- packages/app/src/@generic/components/bottom-sheet-header/bottom-sheet-header.tsx
+++ packages/app/src/@generic/components/bottom-sheet-header/bottom-sheet-header.tsx
@@ -1,12 +1,17 @@
 import { cva } from 'class-variance-authority';
 import { Text, View } from 'react-native';
 
+import { isNotEmptyString } from '@rnw-community/shared';
+
+import { ICONS, IconName } from '../../constant/icons.constant';
 import { BottomSheetHeaderAlign } from '../../type/bottom-sheet-header-align.type';
 import { cn } from '../../utils/cn.util';
+import { Icon } from '../icon/icon';
 
 interface Props {
     readonly size: 'sm' | 'md' | 'lg';
     readonly title: string;
+    readonly icon?: IconName;
     readonly className?: string;
     readonly description: string;
     readonly titleClassName?: string;
@@ -52,10 +57,11 @@ const descriptionVariants = cva('text-center text-secondary-foreground', {
 });
 
 export const BottomSheetHeader = (props: Props) => {
-    const { size = 'md', align = 'center', title, description, className, titleClassName, descriptionClassName } = props;
+    const { size = 'md', align = 'center', icon, title, description, className, titleClassName, descriptionClassName } = props;
 
     return (
         <View className={cn(headerVariant({ align }), className)}>
+            {isNotEmptyString(icon) ? <Icon icon={ICONS[icon]} /> : null}
             <Text className={cn(titleVariants({ size, align }), titleClassName)}>{title}</Text>
             <Text className={cn(descriptionVariants({ size, align }), descriptionClassName)}>{description}</Text>
         </View>
---
+++ packages/app/src/category/components/category-form-icon-field/category-form-icon-field.tsx
@@ -0,0 +1,22 @@
+import { CategoryCreateEntityInterface } from '@budgie/contracts';
+import { useLingui } from '@lingui/react/macro';
+import { Control, Controller, UseControllerReturn } from 'react-hook-form';
+
+import { FormItem } from '../../../@generic/components/form-item/form-item';
+import { IconSelector } from '../../../@generic/components/icon-selector/icon-selector';
+
+interface Props {
+    readonly control: Control<CategoryCreateEntityInterface>;
+}
+
+export const CategoryFormIconField = ({ control }: Props) => {
+    const { t } = useLingui();
+
+    const render = ({ field: { value, onChange }, fieldState: { error } }: UseControllerReturn<CategoryCreateEntityInterface, 'icon'>) => (
+        <FormItem label={t`Icon`} error={error?.message}>
+            <IconSelector size="md" icon={value} onSelect={onChange} />
+        </FormItem>
+    );
+
+    return <Controller name="icon" control={control} render={render} />;
+};
