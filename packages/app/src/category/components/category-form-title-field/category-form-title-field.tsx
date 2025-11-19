d11188e51e349754ee2aaddcaa324e25f079e157
liaugust <ivan.liaugust@gmail.com>
2025-11-19 19:12:12 +0200

    fix: update translations

--- packages/app/src/category/components/category-form-title-field/category-form-title-field.tsx
+++ packages/app/src/category/components/category-form-title-field/category-form-title-field.tsx
@@ -16,19 +16,24 @@ export const CategoryFormTitleField = ({ control }: Props) => {
     const render = ({
         field: { value, onChange },
         fieldState: { error, invalid }
-    }: UseControllerReturn<CategoryCreateEntityInterface, 'title'>) => (
-        <FormItem label={t`Category Name`} error={error?.message}>
-            <Shake isEnabled={invalid}>
-                <BottomSheetTextInput
-                    value={value}
-                    onChangeText={onChange}
-                    placeholder={t`Category name`}
-                    maxLength={CATEGORY_TITLE_MAX_LENGTH}
-                    className="text-md text-primary placeholder:text-secondary-foreground h-[56px] px-5xl bg-secondary-background rounded-5xl border border-secondary-corner"
-                />
-            </Shake>
-        </FormItem>
-    );
+    }: UseControllerReturn<CategoryCreateEntityInterface, 'title'>) => {
+        const variant = invalid ? 'destructive' : 'default'
+
+        return (
+            <FormItem label={t`Category Name`} error={error?.message}>
+                <Shake isEnabled={invalid}>
+                    <BottomSheetTextInput
+                        value={value}
+                        variant={variant}
+                        onChangeText={onChange}
+                        placeholder={t`Category name`}
+                        maxLength={CATEGORY_TITLE_MAX_LENGTH}
+                        className="text-md text-primary placeholder:text-secondary-foreground h-[56px] px-5xl bg-secondary-background rounded-5xl border border-secondary-corner"
+                    />
+                </Shake>
+            </FormItem>
+        )
+    };
 
     return <Controller name="title" control={control} render={render} />;
 };
