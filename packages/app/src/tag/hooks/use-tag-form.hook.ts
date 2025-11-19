025d451fda05fbe337f43f057c0026febe7bced0
liaugust <ivan.liaugust@gmail.com>
2025-11-19 21:41:40 +0200

    feat: add tags screen

--- packages/app/src/tag/hooks/use-tag-form.hook.ts
+++ packages/app/src/tag/hooks/use-tag-form.hook.ts
@@ -1,23 +1,30 @@
 import { TagCreateEntityInterface, TagCreateEntitySchema } from '@budgie/contracts';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 
 const DEFAULT_VALUES: TagCreateEntityInterface = { title: '' };
 
 export const useTagForm = (defaultValues: TagCreateEntityInterface | null) => {
-    const form = useForm({
+    const { reset, control, ...rest } = useForm({
         resolver: zodResolver(TagCreateEntitySchema),
         defaultValues: defaultValues ?? DEFAULT_VALUES,
         mode: 'onSubmit'
     });
 
     const title = useWatch({
-        control: form.control,
-        name: 'title'
+        name: 'title',
+        control
     });
 
+    useEffect(() => {
+        reset(defaultValues ?? DEFAULT_VALUES);
+    }, [defaultValues, reset]);
+
     return {
-        ...form,
+        ...rest,
+        control,
+        reset,
         title
     };
 };
