07b07845f9c1c732c5bdd6752e40eda4a872d513
liaugust <ivan.liaugust@gmail.com>
2025-11-19 19:29:08 +0200

    fix: resolve review comments

--- packages/app/src/@generic/components/animated-flat-list/animated-flat-list.tsx
+++ packages/app/src/@generic/components/animated-flat-list/animated-flat-list.tsx
@@ -1,31 +1,20 @@
 import { ComponentProps, ReactNode } from 'react';
-import { FlatList, ViewStyle } from 'react-native';
+import { FlatList } from 'react-native';
 import Animated, { FadeIn, FadeOut, LinearTransition } from 'react-native-reanimated';
 
 import type { ListRenderItem } from '@react-native/virtualized-lists';
 
-interface AnimatedFlatListProps<T> {
+interface Props<T> extends Pick<ComponentProps<typeof FlatList>, 'className' | 'contentContainerStyle' | 'ListFooterComponent'> {
     readonly data: readonly T[];
     readonly keyExtractor: (item: T, index: number) => string;
     readonly renderItem: (item: T, index: number) => ReactNode;
-    readonly listFooterComponent?: ComponentProps<typeof FlatList>['ListFooterComponent'];
-
-    readonly className?: string;
-    readonly contentContainerStyle?: ViewStyle;
 }
 
 const STIFFNESS = 220;
 const DURATION = 180;
 const DAMPING = 22;
 
-export const AnimatedFlatList = <T,>({
-    data,
-    keyExtractor,
-    renderItem,
-    className,
-    listFooterComponent,
-    contentContainerStyle
-}: AnimatedFlatListProps<T>) => {
+export const AnimatedFlatList = <T,>({ data, keyExtractor, renderItem, ...rest }: Props<T>) => {
     const internalRenderItem: ListRenderItem<T> = ({ item, index }) => (
         <Animated.View
             entering={FadeIn.duration(DURATION)}
@@ -37,14 +26,6 @@ export const AnimatedFlatList = <T,>({
     );
 
     return (
-        <FlatList
-            data={data}
-            className={className}
-            keyExtractor={keyExtractor}
-            renderItem={internalRenderItem}
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={contentContainerStyle}
-            ListFooterComponent={listFooterComponent}
-        />
+        <FlatList data={data} keyExtractor={keyExtractor} renderItem={internalRenderItem} showsVerticalScrollIndicator={false} {...rest} />
     );
 };
