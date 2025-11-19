0ca32053ad32a093a710927aca8d37cc60223994
liaugust <ivan.liaugust@gmail.com>
2025-11-19 21:41:40 +0200

    feat: add tags screen

--- packages/app/src/category/components/category-form-bottom-sheet/category-form-bottom-sheet.tsx
+++ packages/app/src/category/components/category-form-bottom-sheet/category-form-bottom-sheet.tsx
@@ -1,19 +1,12 @@
 import { CategoryCreateEntityInterface } from '@budgie/contracts';
-import { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 import { useLingui } from '@lingui/react/macro';
-import { ComponentProps, RefObject } from 'react';
-import { View } from 'react-native';
+import { RefObject, useCallback } from 'react';
 import Toast from 'react-native-toast-message';
 
-import { BottomSheet } from '../../../@generic/components/bottom-sheet/bottom-sheet';
-import { BottomSheetFooter } from '../../../@generic/components/bottom-sheet-footer/bottom-sheet-footer';
-import { BottomSheetHeader } from '../../../@generic/components/bottom-sheet-header/bottom-sheet-header';
-import { Icon } from '../../../@generic/components/icon/icon';
-import { ICONS } from '../../../@generic/constant/icons.constant';
+import { FormBottomSheet } from '../../../@generic/components/form-bottom-sheet/form-bottom-sheet';
 import { categoryRepository } from '../../../@generic/drizzle/db/db';
 import { BottomSheetInterface } from '../../../@generic/interface/bottom-sheet.interface';
 import { useCategoryForm } from '../../hooks/use-category-form.hook';
-import { CategoryBottomSheetFooter } from '../category-bottom-sheet-footer/category-bottom-sheet-footer';
 import { CategoryFormIconField } from '../category-form-icon-field/category-form-icon-field';
 import { CategoryFormTitleField } from '../category-form-title-field/category-form-title-field';
 import { CategoryPreview } from '../category-preview/category-preview';
@@ -24,57 +17,49 @@ interface Props {
 }
 
 export const CategoryFormBottomSheet = ({ ref, category }: Props) => {
+    const { handleSubmit, reset, control, title, icon } = useCategoryForm(category);
     const { t } = useLingui();
 
-    const { control, handleSubmit, reset, icon, title } = useCategoryForm(category);
-
-    const handleCancel = () => void ref.current?.close();
-
-    const createCategory = async (values: CategoryCreateEntityInterface) => {
-        try {
-            await categoryRepository.create(values);
-            reset();
-            ref.current?.close();
-        } catch {
-            Toast.show({
-                type: 'error',
-                text1: t`Could not create category`,
-                text2: t`Please try again later`
-            });
-        }
+    const handleCancel = () => {
+        void ref.current?.close();
+        reset();
     };
 
-    const renderFooter = (props: ComponentProps<typeof BottomSheetFooter>) => (
-        <BottomSheetFooter {...props}>
-            <CategoryBottomSheetFooter onCancel={handleCancel} onSubmit={handleSubmit(createCategory)} />
-        </BottomSheetFooter>
+    const createCategory = useCallback(
+        async (values: CategoryCreateEntityInterface) => {
+            try {
+                await categoryRepository.create(values);
+                reset();
+                ref.current?.close();
+            } catch {
+                Toast.show({
+                    type: 'error',
+                    text1: t`Could not create category`,
+                    text2: t`Please try again later`
+                });
+            }
+        },
+        [ref, reset, t]
     );
 
+    const onSubmit = useCallback(() => {
+        void handleSubmit(createCategory)();
+    }, [handleSubmit, createCategory]);
+
     return (
-        <BottomSheet footerComponent={renderFooter} onDismiss={reset} ref={ref}>
-            <BottomSheetScrollView showsVerticalScrollIndicator={false}>
-                <View className="px-5xl pb-[100px]">
-                    <View className="bg-secondary-background p-xl rounded-3xl mx-auto mb-3xl border border-secondary-corner">
-                        <Icon icon={ICONS.FolderOpen} className="text-primary" size={28} />
-                    </View>
+        <FormBottomSheet
+            onDismiss={handleCancel}
+            onCancel={handleCancel}
+            onSubmit={onSubmit}
+            icon="Folder"
+            title={t`Create Category`}
+            description={t`Add a new category to organize your transactions`}
+            ref={ref}
+        >
+            <CategoryFormTitleField control={control} />
+            <CategoryFormIconField control={control} />
 
-                    <View className="mb-10">
-                        <BottomSheetHeader
-                            size="lg"
-                            align="center"
-                            title={t`Create Category`}
-                            description={t`Add a new category to organize your transactions`}
-                        />
-                    </View>
-
-                    <View className="gap-y-3xl">
-                        <CategoryFormTitleField control={control} />
-                        <CategoryFormIconField control={control} />
-
-                        <CategoryPreview icon={icon} title={title} />
-                    </View>
-                </View>
-            </BottomSheetScrollView>
-        </BottomSheet>
+            <CategoryPreview icon={icon} title={title} />
+        </FormBottomSheet>
     );
 };
