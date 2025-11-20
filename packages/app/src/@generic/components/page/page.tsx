24ec40698682ae7002809e03409c8ee3cbdbaa5a
liaugust <ivan.liaugust@gmail.com>
2025-11-20 23:27:04 +0200

    feat: add archived accounts screen

--- packages/app/src/@generic/components/page/page.tsx
+++ packages/app/src/@generic/components/page/page.tsx
@@ -1,5 +1,5 @@
 import { styled } from 'nativewind';
-import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { cn } from '../../utils/cn.util';
@@ -17,16 +17,10 @@ interface Props extends ComponentProps<typeof View> {
 const Wrapper = styled(SafeAreaView);
 const DEFAULT_SAFE_EDGES: Edges = ['top'];
 
-export const Page = ({ className, header, footer, children, safeEdges = DEFAULT_SAFE_EDGES, contentClassName, ...rest }: Props) => {
-    const dismissKeyboard = () => void Keyboard.dismiss();
-
-    return (
-        <Wrapper {...rest} className={cn('bg-primary-reverse flex-1', className)} edges={safeEdges}>
-            {header}
-            <TouchableWithoutFeedback onPress={dismissKeyboard}>
-                <View className={cn('bg-primary-reverse px-5xl flex-1', contentClassName)}>{children}</View>
-            </TouchableWithoutFeedback>
-            {footer}
-        </Wrapper>
-    );
-};
+export const Page = ({ className, header, footer, children, safeEdges = DEFAULT_SAFE_EDGES, contentClassName, ...rest }: Props) => (
+    <Wrapper {...rest} className={cn('bg-primary-reverse flex-1', className)} edges={safeEdges}>
+        {header}
+        <View className={cn('bg-primary-reverse px-5xl flex-1', contentClassName)}>{children}</View>
+        {footer}
+    </Wrapper>
+);
