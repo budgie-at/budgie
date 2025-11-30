4bdbfa7732cd9eade78ebee8f9cf88544ee61b8c
liaugust <ivan.liaugust@gmail.com>
2025-12-01 00:39:00 +0200

    fix: apply patch for react-native-css

--- packages/app/src/@generic/components/bottom-tabs/bottom-tabs.tsx
+++ packages/app/src/@generic/components/bottom-tabs/bottom-tabs.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from 'react';
 import { View, ViewProps } from 'react-native';
 
-import { isDefined } from '@rnw-community/shared';
-
-/** @deprecated https://github.com/nativewind/nativewind/issues/1647 */
-const useFixNativewindInitialRenderBug = (style: ViewProps['style']) => {
-    const [tempStyle, setTempStyle] = useState(style);
-
-    useEffect(() => {
-        // eslint-disable-next-line no-undefined,react-hooks/set-state-in-effect
-        setTempStyle(undefined);
-    }, []);
-
-    return isDefined(tempStyle) && { style: tempStyle };
-};
-
-export const BottomTabs = ({ children, style, ...props }: ViewProps) => (
-    <View
-        {...props}
-        {...useFixNativewindInitialRenderBug(style)}
-        className="bg-primary-reverse flex-row justify-between border-t border-t-secondary-corner items-baseline pb-2 pl-4 pr-4"
-    >
+export const BottomTabs = ({ children, ...props }: ViewProps) => (
+    <View {...props} className="bg-primary-reverse border-t border-t-secondary-corner px-5xl pt-4xl">
         {children}
     </View>
 );
