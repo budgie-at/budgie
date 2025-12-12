609b0e48adf83bb2ba0eb8fd28c80a810de460d4
liaugust <ivan.liaugust@gmail.com>
2025-12-12 18:25:56 +0200

    fix: resolve review comments

--- packages/app/src/account/query/use-account-balance.query.ts
+++ packages/app/src/account/query/use-account-balance.query.ts
@@ -3,7 +3,7 @@ import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 import { accountBalanceRepository } from '../../@generic/drizzle/db/db';
 
 export const useAccountBalanceQuery = (accountId: number) => {
-    const { data } = useLiveQuery(accountBalanceRepository.getAccountBalance(accountId), [accountId]);
+    const { data } = useLiveQuery(accountBalanceRepository.getByAccountId(accountId), [accountId]);
     const { balance } = data.at(0) ?? { balance: 0 };
 
     return { balance };
