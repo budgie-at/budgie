f480993c163c35476d5bd105b822046b14f5e381
liaugust <ivan.liaugust@gmail.com>
2025-12-12 03:06:12 +0200

    wip

--- packages/app/src/@generic/components/date-picker/date-picker.tsx
+++ packages/app/src/@generic/components/date-picker/date-picker.tsx
@@ -1,50 +1,42 @@
-import DateTimePicker, { CalendarComponents, DateType, useDefaultClassNames } from 'react-native-ui-datepicker';
-
-import { isDefined } from '@rnw-community/shared';
+import { ComponentProps } from 'react';
+import DateTimePicker, { CalendarComponents, useDefaultClassNames } from 'react-native-ui-datepicker';
 
 import { useSettingsContext } from '../../../settings/context/settings.context';
 import { ICONS } from '../../constant/icons.constant';
 import { Icon } from '../icon/icon';
-import { DatePickerRangeProps, DatePickerSingleProps } from 'react-native-ui-datepicker/lib/typescript/datetime-picker';
 
 const components: CalendarComponents = {
     IconNext: <Icon icon={ICONS.ChevronRight} className="text-primary" size={24} />,
     IconPrev: <Icon icon={ICONS.ChevronLeft} className="text-primary" size={24} />
 };
 
-export const DatePicker = ({ date, onChange, mode, startDate, endDate }: DatePickerSingleProps | DatePickerRangeProps) => {
+export const DatePicker = ({ classNames, ...props }: ComponentProps<typeof DateTimePicker>) => {
     const { settings } = useSettingsContext();
     const defaultClassNames = useDefaultClassNames();
 
-    const handleDateChange = ({ date }: { date: DateType }) => {
-        if (isDefined(date)) {
-            void onChange(new Date(date.toString()));
-        }
-    };
-
     /* eslint-disable lingui/no-unlocalized-strings */
-    const classNames = {
+    const className = {
         ...defaultClassNames,
-        today: 'bg-primary/10',
+        ...classNames,
+        today: 'bg-primary/20',
         today_label: 'text-primary',
         header: 'py-xl px-xl',
         month: 'bg-primary-reverse',
         month_label: 'text-primary text-sm font-medium',
         selected_month: 'bg-primary rounded-full',
         selected_month_label: 'text-primary-reverse',
-        day: 'text-white',
+        day: 'text-white rounded-full',
+        day_cell: 'bg-primary-reverse overflow-hidden',
+        day_label: 'text-primary',
         year: 'bg-primary-reverse',
         year_label: 'text-primary text-sm font-medium',
         selected_year: 'bg-primary rounded-full',
         selected_year_label: 'text-primary-reverse',
         year_selector_label: 'text-secondary-foreground text-sm font-medium',
         month_selector_label: 'text-primary text-lg font-semibold',
-        day_label: 'text-primary',
-        // day_cell: 'bg-primary-reverse rounded-full overflow-hidden',
-        day_cell: 'bg-primary-reverse overflow-hidden',
         selected: 'bg-primary',
         selected_label: 'text-primary-reverse',
-        range_middle: 'bg-secondary-corner',
+        range_fill: 'bg-secondary-corner',
         range_middle_label: 'text-primary',
         range_start: 'rounded-l-full',
         range_end: 'rounded-r-full',
@@ -53,16 +45,5 @@ export const DatePicker = ({ date, onChange, mode, startDate, endDate }: DatePic
     };
     /* eslint-enable lingui/no-unlocalized-strings */
 
-    return (
-        <DateTimePicker
-            classNames={classNames}
-            locale={settings.locale}
-            date={date}
-            startDate={startDate}
-            endDate={endDate}
-            components={components}
-            onChange={onChange}
-            mode={mode}
-        />
-    );
+    return <DateTimePicker {...props} classNames={className} locale={settings.locale} components={components} />;
 };
--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -94,6 +94,8 @@ export { CategoryRepository } from './category/repository/category.repository';
 export { TransactionTypeEnum } from './transaction/enum/transaction-type.enum';
 export { TransactionAssociationEnum } from './transaction/enum/transaction-association.enum';
 
+export { DEFAULT_TRANSACTION_FILTER } from './transaction/constant/default-transaction-filter.constant';
+
 export { TransactionEntityTable } from './transaction/table/transaction-entity.table';
 export { TransactionEntityRelations } from './transaction/relations/transaction-entity.relations';
 
