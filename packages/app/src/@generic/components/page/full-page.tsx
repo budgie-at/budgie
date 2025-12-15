e067790c6f274621cc5fec14ceccec280130aef4
liaugust <ivan.liaugust@gmail.com>
2025-12-15 13:37:56 +0200

    fix: replace SafeAreaView with useSafeAreaInsets

--- packages/app/src/@generic/components/page/full-page.tsx
+++ packages/app/src/@generic/components/page/full-page.tsx
@@ -1,8 +1,8 @@
 import { ComponentProps } from 'react';
-import { Edges } from 'react-native-safe-area-context';
+import { Edge } from 'react-native-safe-area-context';
 
 import { Page } from './page';
 
-const safeEdges: Edges = ['bottom', 'top']
+const safeEdges: Edge[] = ['bottom', 'top'];
 
-export const FullPage = (props: ComponentProps<typeof Page>) => <Page {...props} safeEdges={safeEdges} />
+export const FullPage = (props: ComponentProps<typeof Page>) => <Page {...props} safeEdges={safeEdges} />;
