72018b900ff19d3b454ec3d1af798e3863c859af
liaugust <ivan.liaugust@gmail.com>
2025-12-15 22:50:46 +0200

    fix: remove hidden tabs and tab trigger for ai

--- packages/app/src/account/component/create-account-balance-field/account-balance-field.tsx
+++ packages/app/src/account/component/create-account-balance-field/account-balance-field.tsx
@@ -1,11 +1,8 @@
 import { AccountCreateEntityInterface } from '@budgie/contracts';
-import { cva } from 'class-variance-authority';
 import { Control, Controller, UseControllerReturn } from 'react-hook-form';
-import { Text, View } from 'react-native';
 
-import { FOREGROUND_COLOR_PALETTE } from '../../../@generic/constant/foreground-color-palette.constant';
+import { FormAmountInput } from '../../../@generic/components/form-amount-input/form-amount-input';
 import { ColorPaletteVariant } from '../../../@generic/type/color-palette-variant.type';
-import { AccountBalanceInput } from '../account-balance-input/account-balance-input';
 
 interface Props {
     readonly control: Control<AccountCreateEntityInterface>;
@@ -13,19 +10,9 @@ interface Props {
     readonly variant: ColorPaletteVariant;
 }
 
-const textVariants = cva('text-[72px]', {
-    variants: {
-        variant: FOREGROUND_COLOR_PALETTE
-    }
-});
-
 export const AccountBalanceField = ({ control, instrumentSymbol, variant }: Props) => {
     const renderInput = ({ field: { value, onChange } }: UseControllerReturn<AccountCreateEntityInterface, 'currentBalance'>) => (
-        <View className="flex-row items-center justify-center pt-[40px] pb-7xl">
-            <Text className={textVariants({ variant })}>{instrumentSymbol} </Text>
-
-            <AccountBalanceInput value={value} onChange={onChange} />
-        </View>
+        <FormAmountInput value={value} instrumentSymbol={instrumentSymbol} variant={variant} onChange={onChange} />
     );
 
     return <Controller control={control} name="currentBalance" render={renderInput} />;
