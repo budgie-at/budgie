971316d5ea0db4936cc589d9ebbc8863e630c21b
liaugust <ivan.liaugust@gmail.com>
2025-12-16 20:19:38 +0200

    fix: resolve new findings

---
+++ packages/app/src/@generic/utils/dismiss-all-or-replace.util.ts
@@ -0,0 +1,10 @@
+import { Href, router } from 'expo-router';
+import { NavigationOptions } from 'expo-router/build/global-state/routing';
+
+export const dismissAllOrReplace = (fallbackRoute: Href, fallbackOptions?: NavigationOptions): void => {
+    if (router.canDismiss()) {
+        router.dismissAll();
+    } else {
+        router.replace(fallbackRoute, fallbackOptions);
+    }
+};
