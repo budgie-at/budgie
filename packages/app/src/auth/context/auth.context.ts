2a23cb96f8c06151e5f56624e66424a4959f3c22
liaugust <ivan.liaugust@gmail.com>
2025-12-17 20:59:47 +0200

    fix: resolve CI

--- packages/app/src/auth/context/auth.context.ts
+++ packages/app/src/auth/context/auth.context.ts
@@ -3,13 +3,19 @@ import { createContext, use } from 'react';
 import { emptyFn } from '@rnw-community/shared';
 
 interface AuthContextInterface {
-    isUnlocked: boolean;
+    isSomeAvailable: boolean;
+    isUnlocked: boolean | null;
+    isFaceIdAvailable: boolean;
+    isTouchIdAvailable: boolean;
     setIsUnlocked: (unlocked: boolean) => void;
 }
 
 export const AuthContext = createContext<AuthContextInterface>({
     isUnlocked: false,
-    setIsUnlocked: emptyFn
+    isSomeAvailable: false,
+    setIsUnlocked: emptyFn,
+    isFaceIdAvailable: false,
+    isTouchIdAvailable: false,
 });
 
 export const useAuthContext = () => use(AuthContext);
