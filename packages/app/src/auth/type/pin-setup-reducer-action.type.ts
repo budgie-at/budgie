d9ef258d20f1123f32b165af2808fca0dea4d0e7
liaugust <ivan.liaugust@gmail.com>
2025-12-17 20:04:28 +0200

    wip

--- packages/app/src/auth/type/pin-setup-reducer-action.type.ts
+++ packages/app/src/auth/type/pin-setup-reducer-action.type.ts
@@ -1,10 +1,12 @@
+import { MessageDescriptor } from '@lingui/core';
+
 import { PinSetupReducerActionEnum } from '../enum/pin-setup-reducer-action.enum';
 
 export type PinSetupReducerAction =
     | { type: PinSetupReducerActionEnum.ADD_DIGIT; digit: string }
     | { type: PinSetupReducerActionEnum.DELETE_DIGIT }
     | { type: PinSetupReducerActionEnum.RESET_INPUT }
-    | { type: PinSetupReducerActionEnum.SET_ERROR; error: string }
+    | { type: PinSetupReducerActionEnum.SET_ERROR; error: MessageDescriptor }
     | { type: PinSetupReducerActionEnum.SET_LOADING; loading: boolean }
     | { type: PinSetupReducerActionEnum.VERIFY_OLD_SUCCESS }
     | { type: PinSetupReducerActionEnum.STORE_NEW_PIN; pin: string }
