9f11bbe8623b541f08c377985bf857d178f72620
liaugust <ivan.liaugust@gmail.com>
2025-12-18 00:52:54 +0200

    feat: add transaction details screen

--- packages/app/src/app/(main)/transactions/[id].tsx
+++ packages/app/src/app/(main)/transactions/[id].tsx
@@ -1,18 +1,39 @@
-import { Trans } from '@lingui/react/macro';
-import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import {
+    isExpenseTransaction,
+    isIncomeTransaction,
+    isNegativeAdjustmentTransaction,
+    isPositiveAdjustmentTransaction
+} from '@budgie/contracts';
+import { Redirect, useLocalSearchParams } from 'expo-router';
+
+import { isDefined } from '@rnw-community/shared';
+
+import { EmptyScreen } from '../../../@generic/components/empty-screen/empty-screen';
+import { IdParamInterface } from '../../../@generic/interface/id-param.interface';
+import { UpdateExpenseTransaction } from '../../../transaction/components/update-expense-transaction/update-expense-transaction';
+import { UpdateIncomeTransaction } from '../../../transaction/components/update-income-transaction/update-income-transaction';
+import { useGetTransactionByIdQuery } from '../../../transaction/query/use-get-transaction-by-id.query';
 
 export default function TransactionDetailsScreen() {
-    const { id } = useLocalSearchParams();
+    const { id } = useLocalSearchParams<IdParamInterface>();
 
-    return (
-        <View className="p-[100px]">
-            <Text className="text-primary text-3xl font-semibold">
-                <Trans>Transaction Details</Trans>
-            </Text>
-            <Text className="text-primary text-3xl font-semibold">
-                <Trans>Transaction ID: {id}</Trans>
-            </Text>
-        </View>
-    );
+    const { transaction, isLoading } = useGetTransactionByIdQuery(Number(id));
+
+    if (isLoading) {
+        return <EmptyScreen />;
+    }
+
+    if (!isDefined(transaction)) {
+        return <Redirect href="/" />;
+    }
+
+    if (isIncomeTransaction(transaction) || isPositiveAdjustmentTransaction(transaction)) {
+        return <UpdateIncomeTransaction transaction={transaction} />;
+    }
+
+    if (isExpenseTransaction(transaction) || isNegativeAdjustmentTransaction(transaction)) {
+        return <UpdateExpenseTransaction transaction={transaction} />;
+    }
+
+    return <Redirect href="/" />;
 }
