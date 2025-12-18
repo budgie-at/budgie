2e8ee4fddb4f8a63ebfa4763c47964b676878a80
liaugust <ivan.liaugust@gmail.com>
2025-12-18 09:25:05 +0200

    fix: resolve lint

--- packages/app/src/@generic/components/form-bottom-sheet/form-bottom-sheet.tsx
+++ packages/app/src/@generic/components/form-bottom-sheet/form-bottom-sheet.tsx
@@ -27,7 +27,7 @@ export const FormBottomSheet = (props: Props) => {
 
     return (
         <BottomSheet enableDynamicSizing onDismiss={onDismiss} ref={ref}>
-            <BottomSheetScrollView>
+            <BottomSheetScrollView keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
                 <View className="px-7xl py-5xl">
                     <View className="bg-secondary-background p-xl rounded-3xl mx-auto mb-3xl border border-secondary-corner">
                         <Icon icon={ICONS[icon]} className="text-primary" size={28} />
