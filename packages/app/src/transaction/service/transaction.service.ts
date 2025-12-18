0c7265820ea6d960bc44a04a5debfb55e18b1a51
liaugust <ivan.liaugust@gmail.com>
2025-12-18 21:34:09 +0200

    wip

--- packages/app/src/transaction/service/transaction.service.ts
+++ packages/app/src/transaction/service/transaction.service.ts
@@ -1,8 +1,18 @@
-import { TransactionCreateEntityInterface, TransactionEntityInterface } from '@budgie/contracts';
+/* eslint-disable lingui/no-unlocalized-strings */
+import {
+    TransactionCreateEntityInterface,
+    TransactionEntityInterface,
+    TransactionEntryTypeEnum
+} from '@budgie/contracts';
 
-import { isNotEmptyArray } from '@rnw-community/shared';
+import { isDefined, isNotEmptyArray } from '@rnw-community/shared';
 
-import { db, transactionEntryRepository, transactionRepository, transactionTagsRepository } from '../../@generic/drizzle/db/db';
+import {
+    db,
+    transactionEntryRepository,
+    transactionRepository,
+    transactionTagsRepository
+} from '../../@generic/drizzle/db/db';
 import { convertToMicroUnits } from '../../@generic/utils/convert-to-micro-units.util';
 
 class TransactionService {
@@ -33,6 +43,70 @@ class TransactionService {
                         tx
                     )
                 )
+            );
+
+            if (isNotEmptyArray(input.tagIds)) {
+                await Promise.all(
+                    input.tagIds.map(async id => transactionTagsRepository.create({ transactionId: transaction.id, tagId: id }, tx))
+                );
+            }
+
+            return transaction;
+        });
+    }
+
+    async createInternalTransfer(input: TransactionCreateEntityInterface): Promise<TransactionEntityInterface> {
+        return await db.transaction(async tx => {
+            const fromEntry = input.entries.find(({ accountId }) => accountId === input.fromAccountId);
+            const toEntry = input.entries.find(({ accountId }) => accountId === input.toAccountId);
+
+            if (!isDefined(fromEntry) || !isDefined(toEntry)) {
+                throw new Error('Transfer must have exactly two entries');
+            }
+
+            const fromAmount = convertToMicroUnits(fromEntry.amount);
+            const toAmount = convertToMicroUnits(toEntry.amount);
+
+            const transaction = await transactionRepository.create(
+                {
+                    tagIds: [],
+                    entries: [],
+                    externalId: null,
+                    type: input.type,
+                    amount: fromAmount,
+                    title: input.title,
+                    externalSource: null,
+                    comment: input.comment,
+                    operatedAt: input.operatedAt,
+                    toAccountId: input.toAccountId,
+                    exchangeRate: fromEntry.instrumentId === toEntry.instrumentId ? 1 : toAmount / fromAmount,
+                    fromAccountId: input.fromAccountId,
+                },
+                tx
+            );
+
+            await transactionEntryRepository.create(
+                {
+                    ...fromEntry,
+                    amount: fromAmount,
+                    accountId: toEntry.accountId,
+                    transactionId: transaction.id,
+                    type: TransactionEntryTypeEnum.CREDIT,
+                    instrumentId: fromEntry.instrumentId
+                },
+                tx
+            );
+
+            await transactionEntryRepository.create(
+                {
+                    ...toEntry,
+                    amount: toAmount,
+                    accountId: toEntry.accountId,
+                    transactionId: transaction.id,
+                    instrumentId: toEntry.instrumentId,
+                    type: TransactionEntryTypeEnum.DEBIT
+                },
+                tx
             );
 
             if (isNotEmptyArray(input.tagIds)) {
