4f8544f34123b7fc805da88353aac14fa57fa204
liaugust <ivan.liaugust@gmail.com>
2025-12-18 09:23:47 +0200

    fix: resolve lint

--- packages/app/src/transaction/components/transaction-split/transaction-split.tsx
+++ packages/app/src/transaction/components/transaction-split/transaction-split.tsx
@@ -4,19 +4,17 @@ import { cva } from 'class-variance-authority';
 import { Control, useFieldArray, useWatch } from 'react-hook-form';
 import { Switch, Text, View } from 'react-native';
 
-import { isDefined, isPositiveNumber } from '@rnw-community/shared';
+import { isDefined } from '@rnw-community/shared';
 
 import { Button } from '../../../@generic/components/button/button';
 import { Icon } from '../../../@generic/components/icon/icon';
 import { FOREGROUND_COLOR_PALETTE } from '../../../@generic/constant/foreground-color-palette.constant';
 import { ICONS } from '../../../@generic/constant/icons.constant';
 import { ColorPaletteVariant } from '../../../@generic/type/color-palette-variant.type';
-import { cn } from '../../../@generic/utils/cn.util';
-import { convertToMicroUnits } from '../../../@generic/utils/convert-to-micro-units.util';
-import { useFormatMoney } from '../../../i18n/hook/use-format-money.hook';
-import { useSettingsContext } from '../../../settings/context/settings.context';
 import { sumEntriesAmount } from '../../../transaction-entry/utils/sum-entries-amount.util';
 import { TransactionEntry } from '../transaction-entry/transaction-entry';
+
+import { TransactionSplitAllocation } from './transaction-split-allocation';
 
 interface Props {
     readonly control: Control<TransactionCreateEntityInterface>;
@@ -35,28 +33,7 @@ const categoryVariants = cva('text-sm font-medium flex-1', {
     }
 });
 
-const summaryVariants = cva('px-xl py-lg rounded-3xl flex-row items-center gap-x-md', {
-    variants: {
-        valid: {
-            true: 'bg-positive-background',
-            false: 'bg-destructive-background'
-        }
-    }
-});
-
-const summaryTextVariants = cva('text-xs', {
-    variants: {
-        valid: {
-            true: 'text-positive-foreground',
-            false: 'text-destructive-foreground'
-        }
-    }
-});
-
 export const TransactionSplit = ({ control, variant }: Props) => {
-    const { defaultCurrency, decimalPlaces } = useSettingsContext();
-    const formatMoney = useFormatMoney(decimalPlaces, defaultCurrency);
-
     const { fields, append, remove } = useFieldArray({
         control,
         name: TransactionAssociationEnum.ENTRIES
@@ -68,11 +45,6 @@ export const TransactionSplit = ({ control, variant }: Props) => {
     const { t } = useLingui();
 
     const entriesAmount = sumEntriesAmount(entries);
-    const isAllAllocated = entriesAmount === amount;
-    const isOverAllocated = entriesAmount > amount;
-    const isUnderAllocated = entriesAmount < amount;
-
-    const amountToAllocate = convertToMicroUnits(Math.abs(amount - entriesAmount));
 
     const showSummary = amount !== 0 || entriesAmount !== 0;
 
@@ -141,21 +113,7 @@ export const TransactionSplit = ({ control, variant }: Props) => {
                         />
                     </View>
 
-                    {showSummary ? (
-                        <View className={summaryVariants({ valid: isAllAllocated })}>
-                            {isAllAllocated ? <Icon icon={ICONS.Check} size={14} className="text-positive-foreground" /> : null}
-
-                            <Text className={cn(summaryTextVariants({ valid: isAllAllocated }), 'flex-1')}>
-                                {isAllAllocated ? <Trans>Allocated</Trans> : null}
-                                {isOverAllocated ? <Trans>Over:</Trans> : null}
-                                {isUnderAllocated ? <Trans>Remaining:</Trans> : null}
-                            </Text>
-
-                            {isPositiveNumber(amountToAllocate) ? (
-                                <Text className={summaryTextVariants({ valid: isAllAllocated })}>{formatMoney(amountToAllocate)}</Text>
-                            ) : null}
-                        </View>
-                    ) : null}
+                    {showSummary ? <TransactionSplitAllocation entriesAmount={entriesAmount} totalAmount={amount} /> : null}
                 </>
             ) : null}
         </View>
