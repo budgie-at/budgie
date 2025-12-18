cd0168bc155b205ab17766a30f36e0d2fdc1b97a
liaugust <ivan.liaugust@gmail.com>
2025-12-18 09:29:16 +0200

    fix: resolve review comments

--- packages/app/src/@generic/components/go-back-button/go-back-button.tsx
+++ packages/app/src/@generic/components/go-back-button/go-back-button.tsx
@@ -5,7 +5,7 @@ import { cn } from '../../utils/cn.util';
 import { HapticPressable } from '../haptic-pressable/haptic-pressable';
 import { Icon } from '../icon/icon';
 
-export const GoBackBtn = ({ className, ...rest }: ComponentProps<typeof HapticPressable>) => (
+export const GoBackButton = ({ className, ...rest }: ComponentProps<typeof HapticPressable>) => (
     <HapticPressable className={cn('p-md', className)} {...rest}>
         <Icon icon={ICONS.ChevronLeft} className="text-primary" size={24} />
     </HapticPressable>
