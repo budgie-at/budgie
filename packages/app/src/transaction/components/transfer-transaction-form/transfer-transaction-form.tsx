f7b55fdab9c8e1b29e9de639f320cfcfe0af0fb7
liaugust <ivan.liaugust@gmail.com>
2025-12-18 03:05:11 +0200

    wip

--- packages/app/src/transaction/components/transfer-transaction-form/transfer-transaction-form.tsx
+++ packages/app/src/transaction/components/transfer-transaction-form/transfer-transaction-form.tsx
@@ -1,73 +1,75 @@
-import { TransactionCreateEntityInterface, TransactionTypeEnum, TransferTransactionCreateEntitySchema } from '@budgie/contracts';
+import { TransactionCreateEntityInterface } from '@budgie/contracts';
 import { useLingui } from '@lingui/react/macro';
-import { Controller, UseControllerReturn, useWatch } from 'react-hook-form';
+import { Control, Controller, UseControllerReturn, UseFormSetValue, useWatch } from 'react-hook-form';
 import { View } from 'react-native';
+
+import { EmptyFn } from '@rnw-community/shared';
 
 import { CircleIcon } from '../../../@generic/components/circle-icon/circle-icon';
 import { FormItem } from '../../../@generic/components/form-item/form-item';
 import { FormLayoutGroup } from '../../../@generic/components/form-layout-group/form-layout-group';
 import { HapticPressable } from '../../../@generic/components/haptic-pressable/haptic-pressable';
-import { ICONS } from '../../../@generic/constant/icons.constant';
+import { IconName, ICONS } from '../../../@generic/constant/icons.constant';
+import { ColorPaletteVariant } from '../../../@generic/type/color-palette-variant.type';
 import { AccountSelectorSquare } from '../../../account/component/account-selector-square/account-selector-square';
 import { useSettingsContext } from '../../../settings/context/settings.context';
 import { TagsSelector } from '../../../tag/components/tags-selector/tags-selector';
-import { useCreateTransactionForm } from '../../hook/use-create-transaction-form.hook';
 import { TransactionTransferFormAmount } from '../transaction-form-amount/transaction-transfer-form-amount';
+import { TransactionTransferFormCategory } from '../transaction-form-category/transaction-transfer-form-category';
 import { TransactionFormDatePicker } from '../transaction-form-date-picker/transaction-form-date-picker';
 import { TransactionFormLayout } from '../transaction-form-layout/transaction-form-layout';
-import { TransactionTransferFormCategory } from '../transaction-form-category/transaction-transfer-form-category';
 
-export const CreateTransferTransaction = () => {
+interface Props {
+    readonly icon: IconName;
+    readonly onSubmit: EmptyFn;
+    readonly control: Control<TransactionCreateEntityInterface>;
+    readonly setValue: UseFormSetValue<TransactionCreateEntityInterface>;
+    readonly title: string;
+    readonly buttonText: string;
+    readonly variant: ColorPaletteVariant;
+}
+
+export const TransferTransactionForm = ({ onSubmit, icon, control, setValue, title, buttonText, variant }: Props) => {
     const { defaultInstrument } = useSettingsContext();
     const { t } = useLingui();
 
-    const { form, handleSubmit } = useCreateTransactionForm({
-        schema: TransferTransactionCreateEntitySchema,
-        type: TransactionTypeEnum.TRANSFER,
-        fromAccountId: null,
-        toAccountId: null
-    });
-
     const [fromAccountId, toAccountId] = useWatch({
-        control: form.control,
+        control,
         name: ['fromAccountId', 'toAccountId']
     });
-
-    const errors = form.formState.errors;
-    console.log(JSON.stringify({ errors }, null, 4));
 
     const handleSwitchAccounts = () => {
         const temp = fromAccountId;
 
-        form.setValue('fromAccountId', toAccountId);
-        form.setValue('toAccountId', temp);
+        setValue('fromAccountId', toAccountId);
+        setValue('toAccountId', temp);
 
-        form.setValue('entries.0.accountId', fromAccountId);
-        form.setValue('entries.1.accountId', toAccountId);
+        setValue('entries.0.accountId', fromAccountId);
+        setValue('entries.1.accountId', toAccountId);
     };
 
     const renderDateInput = ({ field: { value, onChange } }: UseControllerReturn<TransactionCreateEntityInterface, 'operatedAt'>) => (
         <FormItem className="w-auto flex-1" label={t`Date`}>
-            <TransactionFormDatePicker variant="default" date={value} onChange={onChange} />
+            <TransactionFormDatePicker variant={variant} date={value} onChange={onChange} />
         </FormItem>
     );
 
     const renderTagsSelector = ({ field: { value, onChange } }: UseControllerReturn<TransactionCreateEntityInterface, 'tagIds'>) => (
         <FormItem className="w-auto flex-1" label={t`Tags`}>
-            <TagsSelector tagIds={value} onChange={onChange} variant="default" />
+            <TagsSelector tagIds={value} onChange={onChange} variant={variant} />
         </FormItem>
     );
 
     const renderFromAccount = ({ field: { value, onChange } }: UseControllerReturn<TransactionCreateEntityInterface, 'fromAccountId'>) => {
         const handleChange = (accountId: number) => {
-            form.setValue('entries.0.accountId', accountId);
+            setValue('entries.0.accountId', accountId);
             onChange(accountId);
         };
 
         return (
             <AccountSelectorSquare
                 className="flex-1"
-                variant="default"
+                variant={variant}
                 excludeAccountId={toAccountId}
                 accountId={value}
                 onSelect={handleChange}
@@ -79,14 +81,14 @@ export const CreateTransferTransaction = () => {
 
     const renderToAccount = ({ field: { value, onChange } }: UseControllerReturn<TransactionCreateEntityInterface, 'toAccountId'>) => {
         const handleChange = (accountId: number) => {
-            form.setValue('entries.1.accountId', accountId);
+            setValue('entries.1.accountId', accountId);
             onChange(accountId);
         };
 
         return (
             <AccountSelectorSquare
                 className="flex-1"
-                variant="default"
+                variant={variant}
                 title={t`TO`}
                 excludeAccountId={fromAccountId}
                 accountId={value}
@@ -98,37 +100,37 @@ export const CreateTransferTransaction = () => {
 
     return (
         <TransactionFormLayout
-            title={t`New Transfer`}
-            variant="default"
-            icon="ArrowRightLeft"
-            onSubmit={handleSubmit}
-            buttonText={t`Add Transfer`}
+            icon={icon}
+            title={title}
+            variant={variant}
+            onSubmit={onSubmit}
+            buttonText={buttonText}
             description={t`Move Money`}
         >
             <View className="flex-row items-center justify-between pt-[40px] gap-x-lg">
-                <Controller render={renderFromAccount} control={form.control} name="fromAccountId" />
+                <Controller render={renderFromAccount} control={control} name="fromAccountId" />
 
                 <HapticPressable onPress={handleSwitchAccounts}>
                     <CircleIcon size="xxs" variant="ghost" icon={ICONS.ArrowRightIcon} />
                 </HapticPressable>
 
-                <Controller render={renderToAccount} name="toAccountId" control={form.control} />
+                <Controller render={renderToAccount} name="toAccountId" control={control} />
             </View>
 
             <TransactionTransferFormAmount
-                control={form.control}
-                setValue={form.setValue}
-                variant="default"
+                control={control}
+                setValue={setValue}
+                variant={variant}
                 instrumentSymbol={defaultInstrument.symbol}
             />
 
             <FormLayoutGroup>
-                <TransactionTransferFormCategory setValue={form.setValue} control={form.control} variant="default" />
+                <TransactionTransferFormCategory setValue={setValue} control={control} variant={variant} />
 
                 <FormLayoutGroup variant="horizontal">
-                    <Controller render={renderDateInput} control={form.control} name="operatedAt" />
+                    <Controller render={renderDateInput} control={control} name="operatedAt" />
 
-                    <Controller render={renderTagsSelector} name="tagIds" control={form.control} />
+                    <Controller render={renderTagsSelector} name="tagIds" control={control} />
                 </FormLayoutGroup>
             </FormLayoutGroup>
         </TransactionFormLayout>
---
+++ packages/contracts/src/transaction/entity/transaction-transfer-with-relations-entity.interface.ts
@@ -0,0 +1,15 @@
+import { AccountEntityInterface } from '../../account/entity/account-entity.interface';
+import { TransactionAssociationEnum } from '../enum/transaction-association.enum';
+import { TransactionTypeEnum } from '../enum/transaction-type.enum';
+
+import { TransactionWithRelationsEntityInterface } from './transaction-with-relations-entity.interface';
+
+export interface TransactionTransferWithRelationsEntityInterface
+    extends Omit<
+        TransactionWithRelationsEntityInterface,
+        TransactionAssociationEnum.TO_ACCOUNT | TransactionAssociationEnum.FROM_ACCOUNT | 'type'
+    > {
+    [TransactionAssociationEnum.FROM_ACCOUNT]: AccountEntityInterface;
+    [TransactionAssociationEnum.TO_ACCOUNT]: AccountEntityInterface;
+    type: TransactionTypeEnum.TRANSFER;
+}
