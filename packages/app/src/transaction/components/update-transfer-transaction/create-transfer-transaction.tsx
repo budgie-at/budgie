c52bf01649a5722e9e174e49bf56a89ee36035dd
liaugust <ivan.liaugust@gmail.com>
2025-12-18 03:05:11 +0200

    wip

---
+++ packages/app/src/transaction/components/update-transfer-transaction/create-transfer-transaction.tsx
@@ -0,0 +1,29 @@
+import { TransactionTypeEnum, TransferTransactionCreateEntitySchema } from '@budgie/contracts';
+import { useLingui } from '@lingui/react/macro';
+
+import { useCreateTransactionForm } from '../../hook/use-create-transaction-form.hook';
+
+import { TransferTransactionForm } from '../transfer-transaction-form/transfer-transaction-form';
+
+export const CreateTransferTransaction = () => {
+    const { t } = useLingui();
+
+    const { form, handleSubmit } = useCreateTransactionForm({
+        schema: TransferTransactionCreateEntitySchema,
+        type: TransactionTypeEnum.TRANSFER,
+        fromAccountId: null,
+        toAccountId: null
+    });
+
+    return (
+        <TransferTransactionForm
+            control={form.control}
+            setValue={form.setValue}
+            title={t`New Transfer`}
+            variant="default"
+            icon="ArrowRightLeft"
+            onSubmit={handleSubmit}
+            buttonText={t`Add Transfer`}
+        />
+    );
+};
