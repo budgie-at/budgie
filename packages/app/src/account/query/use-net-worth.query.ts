be5a80158ca0d9ebb7e0b721a7a9e7b07206fd13
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-18 13:36:02 +0100

    feat(app): fix network liveness

--- packages/app/src/account/query/use-net-worth.query.ts
+++ packages/app/src/account/query/use-net-worth.query.ts
@@ -1,4 +1,6 @@
 import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
 
 import { accountBalanceRepository } from '../../@generic/drizzle/db/db';
 import { useSettingsContext } from '../../settings/context/settings.context';
@@ -6,7 +8,22 @@ import { useSettingsContext } from '../../settings/context/settings.context';
 export const useNetWorthQuery = () => {
     const { defaultInstrument } = useSettingsContext();
     const { data } = useLiveQuery(accountBalanceRepository.getNetWorth(defaultInstrument.id), [defaultInstrument.id]);
-    const { netWorth } = data.at(0) ?? { netWorth: 0 };
 
-    return netWorth;
+    const [netWorthState, setNetWorthState] = useState<number>(data.at(0)?.netWorth ?? 0);
+
+    // HINT: temporary fix to force refresh on screen focus
+    useFocusEffect(
+        // eslint-disable-next-line react-hooks/preserve-manual-memoization
+        useCallback(() => {
+            accountBalanceRepository
+                .getNetWorth(defaultInstrument.id)
+                .execute()
+                .then(([{ netWorth }]) => void setNetWorthState(netWorth))
+                .catch(() => {
+                    setNetWorthState(0);
+                });
+        }, [data])
+    );
+
+    return netWorthState;
 };
