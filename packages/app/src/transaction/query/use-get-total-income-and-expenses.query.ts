3c8585010cb9c3a8a5714da676e0cdff9cb4edd8
liaugust <ivan.liaugust@gmail.com>
2025-12-20 00:38:20 +0200

    fix: resolve CI

--- packages/app/src/transaction/query/use-get-total-income-and-expenses.query.ts
+++ packages/app/src/transaction/query/use-get-total-income-and-expenses.query.ts
@@ -1,10 +1,10 @@
-import { DateRangeInterface } from '@budgie/contracts';
+import { TransactionFilterInterface } from '@budgie/contracts';
 import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 
 import { transactionRepository } from '../../@generic/drizzle/db/db';
 
-export const useGetTotalIncomeAndExpensesQuery = (range: DateRangeInterface) => {
-    const { data } = useLiveQuery(transactionRepository.getTotalIncomeAndExpenseQuery(range), [range]);
+export const useGetTotalIncomeAndExpensesQuery = (filters: TransactionFilterInterface) => {
+    const { data } = useLiveQuery(transactionRepository.getTotalIncomeAndExpenseQuery(filters), [filters]);
 
     return data.at(0) ?? { income: 0, expense: 0 };
 };
