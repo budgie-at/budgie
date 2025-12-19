effde2724206453e31d70f396bc831506c681b8f
liaugust <ivan.liaugust@gmail.com>
2025-12-19 23:12:26 +0200

    fix: resolve ts issues

--- packages/app/src/transaction/query/use-get-expense-by-category.query.ts
+++ packages/app/src/transaction/query/use-get-expense-by-category.query.ts
@@ -1,6 +1,7 @@
-import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
-import { transactionRepository } from '../../@generic/drizzle/db/db';
 import { DateRangeInterface } from '@budgie/contracts';
+import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
+
+import { transactionRepository } from '../../@generic/drizzle/db/db';
 
 export const useGetExpenseByCategoryQuery = (range: DateRangeInterface) => {
     const { data } = useLiveQuery(transactionRepository.getExpenseByCategoryQuery(range), [range]);
