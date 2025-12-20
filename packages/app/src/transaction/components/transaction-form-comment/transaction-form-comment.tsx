1ac2587262f0f0facd9c536ad9966937c8659375
liaugust <ivan.liaugust@gmail.com>
2025-12-20 22:30:06 +0200

    feat: add transaction comment field

---
+++ packages/app/src/transaction/components/transaction-form-comment/transaction-form-comment.tsx
@@ -0,0 +1,22 @@
+import { TransactionCreateEntityInterface } from '@budgie/contracts';
+import { useLingui } from '@lingui/react/macro';
+import { Control, Controller, UseControllerReturn } from 'react-hook-form';
+
+import { FormItem } from '../../../@generic/components/form-item/form-item';
+import { Input } from '../../../@generic/components/input/input';
+
+interface Props {
+    readonly control: Control<TransactionCreateEntityInterface>;
+}
+
+export const TransactionFormComment = ({ control }: Props) => {
+    const { t } = useLingui();
+
+    const renderNote = ({ field: { value, onChange } }: UseControllerReturn<TransactionCreateEntityInterface, 'comment'>) => (
+        <FormItem label={t`Note (optional)`}>
+            <Input value={value} onChangeText={onChange} className="p-xl h-[100px]" placeholder={t`Add a note...`} multiline />
+        </FormItem>
+    );
+
+    return <Controller control={control} render={renderNote} name="comment" />;
+};
