4debdc23841ff9eddbbd83c42f0f29935da0975f
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-22 15:53:49 +0100

    feat(app): ux for column mapper

---
+++ packages/app/src/import/schema/import-column-map.schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const ImportColumnMapSchema = z.object({
+    toAccount: z.string().min(1),
+    category: z.string().min(1),
+    operatedAt: z.string().min(1),
+    amount: z.string().min(1),
+    toCurrency: z.string().min(1),
+    externalId: z.string(),
+    fromAccount: z.string(),
+    fromCurrency: z.string(),
+    comment: z.string()
+});
+
+export type ImportColumnMapFormValues = z.infer<typeof ImportColumnMapSchema>;
