78d740a4198249c8c5b0cd6f492825d11ec790eb
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-22 16:32:23 +0100

    feat(app): ux for column mapper

--- packages/app/src/@generic/drizzle/db/db.ts
+++ packages/app/src/@generic/drizzle/db/db.ts
@@ -34,6 +34,13 @@ const dbInit = () => {
     return global.__expoSqliteDb__;
 };
 
+/** @deprecated TODO: DELETE ME WHEN DB IS STABLE */
+export const __REMOVE_ME_RESET_DB = async () => {
+    await global.__expoSqliteDb__?.closeAsync();
+    await SQLite.deleteDatabaseAsync(DB_NAME);
+    global.__expoSqliteDb__ ?? (global.__expoSqliteDb__ = SQLite.openDatabaseSync(DB_NAME, { enableChangeListener: true }));
+};
+
 export const expoDb = dbInit();
 
 export const db = global.__drizzleDb__ ?? (global.__drizzleDb__ = drizzle(expoDb, { schema }));
