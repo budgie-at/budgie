e899a480409d7be473aa0f310819147f14c3058a
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-22 14:32:40 +0100

    refactor(app): refactor importing logic

---
+++ packages/app/src/import/interface/importer-column-map.interface.ts
@@ -0,0 +1,11 @@
+export interface ImporterColumnMapInterface {
+    externalId: string;
+    fromAccount: string;
+    toAccount: string;
+    category: string;
+    operatedAt: string;
+    comment: string;
+    amount: string;
+    fromCurrency: string;
+    toCurrency: string;
+}
