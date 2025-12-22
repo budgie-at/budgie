efbcf201f47f00442c60c0ec8b6dc558de1ad926
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-22 21:24:54 +0100

    feat(app): use legend list for transactions

---
+++ packages/app/src/transaction/utils/get-transaction-category-label.util.ts
@@ -0,0 +1,23 @@
+import {
+    TransactionWithRelationsEntityInterface,
+    isNegativeAdjustmentTransaction,
+    isPositiveAdjustmentTransaction
+} from '@budgie/contracts';
+
+export const getTransactionCategoryLabel = (
+    transaction: TransactionWithRelationsEntityInterface,
+    balanceAdjustmentLabel: string,
+    categoriesLabel: string
+): string => {
+    if (isPositiveAdjustmentTransaction(transaction) || isNegativeAdjustmentTransaction(transaction)) {
+        return balanceAdjustmentLabel;
+    }
+
+    if (transaction.entries.length > 1) {
+        return categoriesLabel;
+    }
+
+    const [entry] = transaction.entries;
+
+    return entry.category?.title ?? transaction.type;
+};
