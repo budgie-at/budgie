db1488b6ca0a5dcf06663bc0ca564a37e1e2bef5
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-24 12:55:18 +0100

    fix(contracts): respecting setting for screenshot protection

--- packages/app/src/@generic/hooks/use-screenshot-protection.hook.ts
+++ packages/app/src/@generic/hooks/use-screenshot-protection.hook.ts
@@ -1,26 +1,28 @@
-import { PermissionStatus, allowScreenCaptureAsync, preventScreenCaptureAsync, requestPermissionsAsync } from 'expo-screen-capture';
+import { allowScreenCaptureAsync, preventScreenCaptureAsync, usePermissions } from 'expo-screen-capture';
 import { useEffect } from 'react';
 
 import { useSetting } from '../../settings/hook/use-setting.hook';
 
 export const useScreenshotProtection = (): boolean => {
     const isScreenshotProtectionEnabled = useSetting('isScreenshotProtectionEnabled');
+    const [status, requestPermission] = usePermissions();
 
     useEffect(() => {
-        const setupScreenshotProtection = async () => {
-            const { status } = await requestPermissionsAsync();
+        // HINT: Without allowing this in dev mode black screen after fast refresh
+        void allowScreenCaptureAsync('setting');
 
-            if (status === PermissionStatus.GRANTED) {
-                if (isScreenshotProtectionEnabled) {
-                    await preventScreenCaptureAsync('setting');
-                } else {
-                    await allowScreenCaptureAsync('setting');
-                }
+        const setupScreenshotProtection = async () => {
+            if (!status?.granted) {
+                await requestPermission();
+            } else if (isScreenshotProtectionEnabled) {
+                await preventScreenCaptureAsync('setting');
+            } else {
+                await allowScreenCaptureAsync('setting');
             }
         };
 
         void setupScreenshotProtection();
-    }, [isScreenshotProtectionEnabled]);
+    }, [isScreenshotProtectionEnabled, status]);
 
     return isScreenshotProtectionEnabled;
 };
