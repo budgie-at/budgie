cf294205c13d0bd69c0b04367eca64c82eeb9443
liaugust <ivan.liaugust@gmail.com>
2025-12-25 23:32:17 +0200

    feat: update transaction card

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -1,14 +1,12 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_fluffy_golden_guardian.sql';
-import m0001 from './0001_violet_iron_fist.sql';
+import m0000 from './0000_nasty_warhawk.sql';
 
   export default {
     journal,
     migrations: {
-      m0000,
-m0001
+      m0000
     }
   }
   
