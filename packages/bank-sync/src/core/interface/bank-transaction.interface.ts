b63345bd5befeb48f6bed6424c8b93633ca11306
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-25 18:15:26 +0100

    refactor(app): monoback sync background

--- packages/bank-sync/src/core/interface/bank-transaction.interface.ts
+++ packages/bank-sync/src/core/interface/bank-transaction.interface.ts
@@ -1,26 +1,9 @@
+import type { BaseTransactionFieldsInterface } from './base-transaction-fields.interface';
 import type { BankProviderEnum } from '../enum/bank-provider.enum';
 import type { BankTransactionTypeEnum } from '../enum/bank-transaction-type.enum';
 
-export interface BankTransactionInterface {
-    readonly id: string;
+export interface BankTransactionInterface extends BaseTransactionFieldsInterface {
     readonly provider: BankProviderEnum;
     readonly accountId: string;
-    readonly time: number;
-    readonly description: string;
-    readonly mcc: number;
-    readonly originalMcc: number;
-    readonly amount: number;
-    readonly operationAmount: number;
-    readonly currencyCode: number;
-    readonly commissionRate: number;
-    readonly cashbackAmount: number;
-    readonly balance: number;
-    readonly hold: boolean;
     readonly type: BankTransactionTypeEnum;
-    readonly receiptId?: string;
-    readonly invoiceId?: string;
-    readonly counterEdrpou?: string;
-    readonly counterIban?: string;
-    readonly counterName?: string;
-    readonly comment?: string;
 }
