20d514b7d1726981b8d20c1810460e4034f427ef
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-26 13:21:43 +0100

    refactor: banc sync state remove duplicate error field

--- packages/app/src/sync/component/get-token-card/get-token-card.tsx
+++ packages/app/src/sync/component/get-token-card/get-token-card.tsx
@@ -6,24 +6,25 @@ import { BankLogo } from '../../../@generic/components/bank-logo/bank-logo';
 import { Card } from '../../../@generic/components/card/card';
 import { Icon } from '../../../@generic/components/icon/icon';
 import { ICONS } from '../../../@generic/constant/icons.constant';
+import { monobankSyncService } from '../../service/monobank-sync.service';
 
-interface Props {
-    readonly onPress: () => void;
-}
+export const GetTokenCard = () => {
+    const handleOpenMonobank = () => void monobankSyncService.openAuthPage();
 
-export const GetTokenCard = ({ onPress }: Props) => (
-    <Card className="p-5xl" onPress={onPress}>
-        <View className="flex-row items-center gap-x-3xl">
-            <BankLogo bankProvider={BankProviderEnum.MONOBANK} />
-            <View className="flex-1">
-                <Text className="text-primary text-foreground text-md font-medium mb-xs">
-                    <Trans>Get API Token</Trans>
-                </Text>
-                <Text className="text-primary text-muted-foreground text-sm">
-                    <Trans>Open Monobank to get your token</Trans>
-                </Text>
+    return (
+        <Card className="p-5xl" onPress={handleOpenMonobank}>
+            <View className="flex-row items-center gap-x-3xl">
+                <BankLogo bankProvider={BankProviderEnum.MONOBANK} />
+                <View className="flex-1">
+                    <Text className="text-primary text-foreground text-md font-medium mb-xs">
+                        <Trans>Get API Token</Trans>
+                    </Text>
+                    <Text className="text-primary text-muted-foreground text-sm">
+                        <Trans>Open Monobank to get your token</Trans>
+                    </Text>
+                </View>
+                <Icon icon={ICONS.ChevronRight} className="text-muted-foreground" />
             </View>
-            <Icon icon={ICONS.ChevronRight} className="text-muted-foreground" />
-        </View>
-    </Card>
-);
+        </Card>
+    );
+};
