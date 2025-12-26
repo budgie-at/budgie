a212274227f52b7ac91854bf2782fe6095278791
liaugust <ivan.liaugust@gmail.com>
2025-12-26 04:24:10 +0200

    feat: add "truncate data" setting

---
+++ packages/app/src/@generic/service/app.service.ts
@@ -0,0 +1,24 @@
+import {
+    accountRepository,
+    categoryRepository,
+    db,
+    settingsRepository,
+    tagRepository,
+    transactionEntryRepository,
+    transactionRepository
+} from '../drizzle/db/db';
+
+class AppService {
+    async truncateData() {
+        await db.transaction(async tx => {
+            await tagRepository.truncate(tx);
+            await categoryRepository.truncate(false, tx);
+            await transactionEntryRepository.truncate(tx);
+            await transactionRepository.truncate(tx);
+            await settingsRepository.update({ defaultAccountId: null }, tx);
+            await accountRepository.truncate(tx);
+        });
+    }
+}
+
+export const appService = new AppService();
