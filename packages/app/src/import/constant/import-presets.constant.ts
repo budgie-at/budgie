9ddcaacf656d86bfc4e35e350bac5c991da8998f
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-26 14:25:19 +0100

    refactor(app): open monobank page inside the app

--- packages/app/src/import/constant/import-presets.constant.ts
+++ packages/app/src/import/constant/import-presets.constant.ts
@@ -32,4 +32,3 @@ export const IMPORT_PRESETS: Record<ImportPresetEnum, ImportColumnMapFormValues>
         toCurrency: 'outcomeCurrency'
     }
 };
-
--- packages/contracts/src/transaction-entry/input/transaction-entry-create-input.interface.ts
+++ packages/contracts/src/transaction-entry/input/transaction-entry-create-input.interface.ts
@@ -2,5 +2,4 @@ import { infer } from 'zod';
 
 import { TransactionEntryCreateInputSchema } from '../schema/transaction-entry-create-input.schema';
 
-export interface TransactionEntryCreateInputInterface extends infer<typeof TransactionEntryCreateInputSchema> {
-}
+export interface TransactionEntryCreateInputInterface extends infer<typeof TransactionEntryCreateInputSchema> {}
