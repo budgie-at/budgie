50357668adea4214daaac6ebfa5aee3d904510c2
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-26 10:37:08 +0100

    feat(app): reimplement sync through bg task and secure storage

--- packages/app/src/sync/constant/bank-sync-storage-key.constant.ts
+++ packages/app/src/sync/constant/bank-sync-storage-key.constant.ts
@@ -1,6 +1,6 @@
 import { BankProviderEnum } from '@budgie/bank-sync';
 
-const BANK_SYNC_STORAGE_KEY = 'bankSyncState';
+const BANK_SYNC_STORAGE_KEY = 'bank-sync-state';
 
-export const getBankSyncStorageKey = (provider: BankProviderEnum): string => `${BANK_SYNC_STORAGE_KEY}:${provider}`;
-export const getBankSyncEnabledKey = (provider: BankProviderEnum): string => `${BANK_SYNC_STORAGE_KEY}:${provider}:enabled`;
+export const getBankSyncStorageKey = (provider: BankProviderEnum): string => `${BANK_SYNC_STORAGE_KEY}.${provider}`;
+export const getBankSyncEnabledKey = (provider: BankProviderEnum): string => `${BANK_SYNC_STORAGE_KEY}.${provider}.enabled`;
