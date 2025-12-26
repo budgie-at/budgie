8da201caa257d3c0519e0f62cffb69c66dd1bfae
liaugust <ivan.liaugust@gmail.com>
2025-12-26 18:24:37 +0200

    fix: add cross-exchanges for currencies

--- packages/app/src/exchange-rate/task/exchange-rate-sync.task.ts
+++ packages/app/src/exchange-rate/task/exchange-rate-sync.task.ts
@@ -2,11 +2,11 @@ import * as BackgroundTask from 'expo-background-task';
 import * as TaskManager from 'expo-task-manager';
 
 import { EXCHANGE_RATE_SYNC_TASK } from '../constant/exchange-rate-sync-task.constant';
-import { exchangeRatesService } from '../service/exchange-rates-sync.service';
+import { exchangeRatesSyncService } from '../service/exchange-rates-sync.service';
 
 TaskManager.defineTask(EXCHANGE_RATE_SYNC_TASK, async () => {
     try {
-        await exchangeRatesService.sync();
+        await exchangeRatesSyncService.sync();
     } catch {
         return BackgroundTask.BackgroundTaskResult.Failed;
     }
