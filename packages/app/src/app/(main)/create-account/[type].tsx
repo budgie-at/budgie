9e8950e74786060c3d471fc0716e9858e5659fc8
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-26 10:39:12 +0100

    feat(app): reimplement sync through bg task and secure storage

--- packages/app/src/app/(main)/create-account/[type].tsx
+++ packages/app/src/app/(main)/create-account/[type].tsx
@@ -4,7 +4,7 @@ import { Redirect, useLocalSearchParams } from 'expo-router';
 
 import { CreateBankAccount } from '../../../account/component/create-bank-account/create-bank-account';
 import { CreateCashAccount } from '../../../account/component/create-cash-account/create-cash-account';
-import { CreateMonobankAccount } from '../../../account/component/create-monobank-account/create-monobank-account';
+import { CreateMonobankAccount } from '../../../sync/component/create-monobank-account/create-monobank-account';
 
 type AccountRouteType = AccountTypeEnum | 'monobank';
 
