7edc22548c08df3d31051345b4ce18b361cc9d69
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-27 11:05:42 +0100

    fix(app): fix searching latest tx date

--- packages/app/src/sync/hook/use-bank-sync-state.hook.ts
+++ packages/app/src/sync/hook/use-bank-sync-state.hook.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { BankSyncStateInterface } from '../interface/bank-sync-state.interface';
 import { bankSyncStorageService } from '../service/bank-sync-storage.service';
 
-const POLL_INTERVAL_MS = 5000;
+const POLL_INTERVAL_MS = 1000;
 
 export const useBankSyncState = (provider: BankProviderEnum) => {
     const [state, setState] = useState<BankSyncStateInterface>(bankSyncStorageService.getState(provider));
