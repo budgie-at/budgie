877e9b9070fb914a96e35a801f1592a8c76d3684
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-28 21:53:32 +0100

    fix(app): account calculation

--- packages/app/src/app/_layout.tsx
+++ packages/app/src/app/_layout.tsx
@@ -58,7 +58,6 @@ export default function RootLayout() {
             void exchangeRatesSyncService.sync();
             void exchangeRatesSyncService.registerBackgroundTask();
 
-            void accountBalanceIncrementalService.updateAllBalances();
             void accountBalanceIncrementalService.registerBackgroundTask();
 
             if (monobankSyncService.isEnabled()) {
