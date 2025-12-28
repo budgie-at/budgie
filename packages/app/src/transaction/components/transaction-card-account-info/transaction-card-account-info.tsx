508d1620188f7febed486bd06c73658f5306f72f
liaugust <ivan.liaugust@gmail.com>
2025-12-29 01:49:33 +0200

    wip

--- packages/app/src/transaction/components/transaction-card-account-info/transaction-card-account-info.tsx
+++ packages/app/src/transaction/components/transaction-card-account-info/transaction-card-account-info.tsx
@@ -14,34 +14,38 @@ export const TransactionCardAccountInfo = ({ transaction }: Props) => {
 
     if (isDefined(fromAccount) && isDefined(toAccount)) {
         return (
-            <View className="flex-row items-center gap-x-sm">
+            <View className="flex-row items-center gap-x-sm flex-1">
                 <Icon icon={fromAccount.icon} className="text-secondary-foreground" size={12} />
-                <Text className="text-xs font-medium text-secondary-foreground">{fromAccount.title}</Text>
+                <Text className="text-xs font-medium text-secondary-foreground flex-1 max-w-1/2" numberOfLines={1}>
+                    {fromAccount.title}
+                </Text>
 
                 <Icon icon="ArrowRightIcon" className="text-secondary-foreground" size={12} />
 
                 <Icon icon={toAccount.icon} className="text-secondary-foreground" size={12} />
-                <Text className="text-xs font-medium text-secondary-foreground">{toAccount.title}</Text>
+                <Text className="text-xs font-medium text-secondary-foreground flex-1 max-w-1/2" numberOfLines={1}>
+                    {toAccount.title}
+                </Text>
             </View>
         );
     }
 
     if (isDefined(fromAccount)) {
         return (
-            <View className="flex-row items-center gap-x-sm">
+            <View className="flex-row items-center gap-x-sm flex-1">
                 <Text className="text-xs text-secondary-foreground">from</Text>
                 <Icon icon={fromAccount.icon} className="text-secondary-foreground" size={12} />
-                <Text className="text-xs font-medium text-secondary-foreground">{fromAccount.title}</Text>
+                <Text className="text-xs font-medium text-secondary-foreground flex-1" numberOfLines={1}>{fromAccount.title}</Text>
             </View>
         );
     }
 
     if (isDefined(toAccount)) {
         return (
-            <View className="flex-row items-center gap-x-sm">
+            <View className="flex-row items-center gap-x-sm flex-1">
                 <Text className="text-xs text-secondary-foreground">to</Text>
                 <Icon icon={toAccount.icon} className="text-secondary-foreground" size={12} />
-                <Text className="text-xs font-medium text-secondary-foreground">{toAccount.title}</Text>
+                <Text className="text-xs font-medium text-secondary-foreground flex-1" numberOfLines={1}>{toAccount.title}</Text>
             </View>
         );
     }
