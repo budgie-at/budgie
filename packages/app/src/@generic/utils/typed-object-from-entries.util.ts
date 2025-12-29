1ba030f421e0441346eeb631563c229504f9ee5e
liaugust <ivan.liaugust@gmail.com>
2025-12-30 00:45:13 +0200

    fix: add padding

---
+++ packages/app/src/@generic/utils/typed-object-from-entries.util.ts
@@ -0,0 +1,9 @@
+type Entry<K extends PropertyKey, V> = [K, V];
+
+export const typedObjectFromEntries = <const T extends readonly Entry<PropertyKey, unknown>[]>(entries: T): ObjectFromEntries<T> => {
+    return Object.fromEntries(entries) as ObjectFromEntries<T>;
+};
+
+type ObjectFromEntries<T extends readonly Entry<PropertyKey, unknown>[]> = {
+    [E in keyof T as T[E] extends Entry<infer K, infer _> ? K : never]: T[E] extends Entry<infer _, infer V> ? V : never;
+};
