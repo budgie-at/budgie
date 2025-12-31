2aae5e573bb524124716034b8f374b9e2cf028f8
Vitaly Iegorov <vitalyiegorov@gmail.com>
2025-12-31 11:11:59 +0100

    fix(app): fix exporting archived accounts and transfer transactions (#146)
    * fix(app): fix import service
    
    * fix(app): fix import service
    
    * fix(app): archieving account,transactions, entries
    
    * fix(app): archieving account,transactions, entries
    
    * fix(app): export map deleted account transfer to income
    
    * fix(app): export map deleted account transfer to income
    
    * chore: format
    
    * chore: format

--- packages/app/src/account/hooks/use-debt-account-form.hook.ts
+++ packages/app/src/account/hooks/use-debt-account-form.hook.ts
@@ -7,11 +7,11 @@ import {
     UserIconNameEnum
 } from '@budgie/contracts';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { router } from 'expo-router';
 import { useForm, useWatch } from 'react-hook-form';
 
 import { useShowError } from '../../@generic/hook/use-show-error.hook';
 import { useGetInstrumentByIdQuery } from '../../instrument/query/use-get-instrument-by-id.query';
-import { router } from 'expo-router';
 
 export const useDebtAccountForm = (
     defaultValues: DebtAccountCreateInputInterface,
