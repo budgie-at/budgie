0d7e8286e40fe9f31130b9c2cb46ba30c59b73c7
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-02 09:26:53 +0100

    refactor: more icons

--- packages/app/src/import/components/import-database/import-database.tsx
+++ packages/app/src/import/components/import-database/import-database.tsx
@@ -1,3 +1,4 @@
+import { UserIconNameEnum } from '@budgie/contracts';
 import { useLingui } from '@lingui/react/macro';
 import * as DocumentPicker from 'expo-document-picker';
 import { useState } from 'react';
