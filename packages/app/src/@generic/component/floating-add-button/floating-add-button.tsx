c0dae2bc0ef3889e3418be1b32432c4686d34f04
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-02 08:03:02 +0100

    refactor: remove duplicated add button

--- packages/app/src/@generic/component/floating-add-button/floating-add-button.tsx
+++ packages/app/src/@generic/component/floating-add-button/floating-add-button.tsx
@@ -22,7 +22,7 @@ export const FloatingAddButton = <T extends IdInterface>({ renderBottomSheet }: 
                 <Animated.View entering={ZoomIn.duration(300).delay(350)}>
                     <HapticPressable
                         onPress={handleOpen}
-                        className="bg-primary rounded-full w-16 h-16 items-center justify-center active:scale-[0.95] "
+                        className="bg-primary rounded-full w-16 h-16 items-center justify-center active:scale-[0.95]"
                     >
                         <Icon icon="Plus" className="text-primary-reverse" size={32} />
                     </HapticPressable>
