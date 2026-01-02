d2ed52c104b51ffe387223e3f72eefbcaf13b541
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-02 17:30:54 +0100

    fix: review

--- packages/contracts/src/mcc-category/repository/mcc-category.repository.ts
+++ packages/contracts/src/mcc-category/repository/mcc-category.repository.ts
@@ -1,5 +1,3 @@
-import { eq, inArray } from 'drizzle-orm';
-
 import { TX } from '../../@generic/type/db.type';
 import * as schema from '../../schema';
 import { MccCategoryCreateEntityInterface } from '../entity/mcc-category-create-entity.interface';
@@ -15,24 +13,6 @@ export class MccCategoryRepository {
         return this.db.query.MccCategoryEntityTable.findMany();
     }
 
-    findByMcc(mcc: string) {
-        return this.db.query.MccCategoryEntityTable.findFirst({
-            where: eq(MccCategoryEntityTable.mcc, mcc)
-        });
-    }
-
-    findByMccs(mccs: string[]) {
-        return this.db.query.MccCategoryEntityTable.findMany({
-            where: inArray(MccCategoryEntityTable.mcc, mccs)
-        });
-    }
-
-    findByMccGroupId(mccGroupId: number) {
-        return this.db.query.MccCategoryEntityTable.findMany({
-            where: eq(MccCategoryEntityTable.mccGroupId, mccGroupId)
-        });
-    }
-
     async create(input: MccCategoryCreateEntityInterface, tx?: TX): Promise<MccCategoryEntityInterface> {
         const [mccCategory] = await this.bulkCreate([input], tx);
 
@@ -41,34 +21,6 @@ export class MccCategoryRepository {
 
     async bulkCreate(inputs: MccCategoryCreateEntityInterface[], tx?: TX): Promise<MccCategoryEntityInterface[]> {
         return await (tx ?? this.db).insert(MccCategoryEntityTable).values(inputs).returning();
-    }
-
-    async upsert(input: MccCategoryCreateEntityInterface, tx?: TX): Promise<MccCategoryEntityInterface> {
-        const [mccCategory] = await (tx ?? this.db)
-            .insert(MccCategoryEntityTable)
-            .values(input)
-            .onConflictDoUpdate({
-                target: MccCategoryEntityTable.mcc,
-                set: {
-                    mccGroupId: input.mccGroupId,
-                    shortDescription: input.shortDescription,
-                    fullDescription: input.fullDescription
-                }
-            })
-            .returning();
-
-        return mccCategory;
-    }
-
-    async bulkUpsert(inputs: MccCategoryCreateEntityInterface[], tx?: TX): Promise<void> {
-        for (const input of inputs) {
-            // eslint-disable-next-line no-await-in-loop
-            await this.upsert(input, tx);
-        }
-    }
-
-    async deleteByMcc(mcc: string): Promise<void> {
-        await this.db.delete(MccCategoryEntityTable).where(eq(MccCategoryEntityTable.mcc, mcc));
     }
 
     async truncate(tx?: TX): Promise<void> {
