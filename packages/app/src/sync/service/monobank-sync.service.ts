009124faac09d74c0b85d709286345b5e628516f
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-02 14:46:24 +0100

    feat: add MCC categories support

--- packages/app/src/sync/service/monobank-sync.service.ts
+++ packages/app/src/sync/service/monobank-sync.service.ts
@@ -258,7 +258,6 @@ class AppMonobankSyncService {
         const existing = await transactionService.findByExternalSource(this.provider);
         const existingIds = new Set(existing.map(tx => tx.externalId));
         const newTxs = result.transactions.filter(tx => !existingIds.has(tx.id));
-        console.log(this.mccCategoryIdMap);
 
         if (isNotEmptyArray(newTxs)) {
             await transactionService.bulkCreate(
