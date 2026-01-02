a5b331ed7a3132edcabb72935ac046d38f917391
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-02 09:26:53 +0100

    refactor: more icons

--- packages/app/src/@generic/component/selector-card/selector-card.tsx
+++ packages/app/src/@generic/component/selector-card/selector-card.tsx
@@ -1,3 +1,4 @@
+import { UserIconNameEnum } from '@budgie/contracts';
 import { cva } from 'class-variance-authority';
 import { ReactNode } from 'react';
 import { Text, View } from 'react-native';
