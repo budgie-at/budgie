ba0e2780afd103c11be4053412bd833119781bdb
liaugust <ivan.liaugust@gmail.com>
2026-01-04 19:13:20 +0200

    fix: update migrations

--- packages/app/drizzle/0008_modern_sheva_callister.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `rules` DROP COLUMN `title`;--> statement-breakpoint
-ALTER TABLE `rules` DROP COLUMN `title_search`;--> statement-breakpoint
-ALTER TABLE `rules` DROP COLUMN `priority`;
