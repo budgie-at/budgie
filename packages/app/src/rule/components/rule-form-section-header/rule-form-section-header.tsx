e715c6263b42fc561c9b5aa3d899ac21dab34ab7
liaugust <ivan.liaugust@gmail.com>
2026-01-04 07:48:26 +0200

    feat: implement rules

--- packages/app/src/rule/components/rule-form-section-header/rule-form-section-header.tsx
+++ packages/app/src/rule/components/rule-form-section-header/rule-form-section-header.tsx
@@ -4,18 +4,23 @@ import { Text, View } from 'react-native';
 
 import { HapticPressable } from '../../../@generic/component/haptic-pressable/haptic-pressable';
 import { Icon } from '../../../@generic/component/icon/icon';
+import { EmptyFn } from '@rnw-community/shared';
 
 interface Props {
     readonly title: string;
-    readonly onAdd: () => void;
+    readonly onAdd: EmptyFn;
 }
 
 export const RuleFormSectionHeader = ({ title, onAdd }: Props) => (
     <View className="flex-row items-center justify-between">
         <Text className="text-primary text-lg font-semibold">{title}</Text>
+
         <HapticPressable onPress={onAdd} className="bg-primary rounded-full px-lg py-xs flex-row items-center gap-x-xs">
             <Icon icon={UserIconNameEnum.Plus} className="text-primary-reverse" size={16} />
-            <Text className="text-primary-reverse text-sm font-medium"><Trans>Add</Trans></Text>
+
+            <Text className="text-primary-reverse text-sm font-medium">
+                <Trans>Add</Trans>
+            </Text>
         </HapticPressable>
     </View>
 );
