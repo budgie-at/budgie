c839d62b4506939c744a7d79473f39df742e0233
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-04 17:27:42 +0100

    style: center MCC chip and add vertical margin

--- packages/app/src/transaction/components/transaction-mcc-info-field/transaction-mcc-info-field.tsx
+++ packages/app/src/transaction/components/transaction-mcc-info-field/transaction-mcc-info-field.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export const TransactionMccInfoField = ({ mccCategory }: Props) => (
-    <View className="px-sm py-xs rounded-full bg-primary border border-primary self-start">
+    <View className="px-sm py-xs rounded-full bg-primary border border-primary self-center my-md">
         <Text className="text-xxs font-medium text-primary-foreground">{mccCategory.shortDescription}</Text>
     </View>
 );
