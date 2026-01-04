1fedac26ec10ed15152c4795200aae7e1a20d479
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-04 17:27:23 +0100

    feat: enhance MCC chip with inverse colors and cleaner design

--- packages/app/src/transaction/components/transaction-mcc-info-field/transaction-mcc-info-field.tsx
+++ packages/app/src/transaction/components/transaction-mcc-info-field/transaction-mcc-info-field.tsx
@@ -1,18 +1,12 @@
 import { MccCategoryEntityInterface } from '@budgie/contracts';
-import { useLingui } from '@lingui/react/macro';
 import { Text, View } from 'react-native';
 
 interface Props {
     readonly mccCategory: MccCategoryEntityInterface;
 }
 
-export const TransactionMccInfoField = ({ mccCategory }: Props) => {
-    const { t } = useLingui();
-
-    return (
-        <View className="flex-row items-center gap-x-xs px-sm py-xs rounded-full bg-secondary-background self-start">
-            <Text className="text-xxs text-secondary-foreground/70">{t`MCC`}:</Text>
-            <Text className="text-xxs font-medium text-primary/70">{mccCategory.shortDescription}</Text>
-        </View>
-    );
-};
+export const TransactionMccInfoField = ({ mccCategory }: Props) => (
+    <View className="px-sm py-xs rounded-full bg-primary border border-primary self-start">
+        <Text className="text-xxs font-medium text-primary-foreground">{mccCategory.shortDescription}</Text>
+    </View>
+);
