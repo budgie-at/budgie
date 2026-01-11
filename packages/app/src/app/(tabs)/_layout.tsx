59ccb099a249ea93686777f4f9818bfa4c1faa75
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-11 14:17:31 +0100

    feat(app): add smooth close animation to transaction menu

--- packages/app/src/app/(tabs)/_layout.tsx
+++ packages/app/src/app/(tabs)/_layout.tsx
@@ -1,11 +1,10 @@
-import { UserIconNameEnum } from '@budgie/contracts';
 import { TabList, TabSlot, TabTrigger, Tabs } from 'expo-router/ui';
 import { useState } from 'react';
 import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { BlurGradient } from '../../@generic/component/blur-gradient/blur-gradient';
-import { TabButton } from '../../@generic/component/tab-button/tab-button';
+import { TabButtons } from '../../@generic/component/tab-buttons/tab-buttons';
 import { CreateTransactionMenu } from '../../transaction/components/create-transaction-menu/create-transaction-menu';
 import { CreateTransactionTrigger } from '../../transaction/components/create-transaction-trigger/create-transaction-trigger';
 
@@ -34,23 +33,7 @@ export default function TabsLayout() {
                 <BlurGradient position="bottom">
                     <View className="absolute inset-x-0 bottom-0" pointerEvents="box-none">
                         <View className="flex-row items-center justify-between px-lg pb-lg pt-md" style={containerStyle}>
-                            <View className="flex-row items-center gap-sm">
-                                <TabTrigger name="home" asChild reset="always">
-                                    <TabButton icon={UserIconNameEnum.Home} />
-                                </TabTrigger>
-
-                                <TabTrigger name="transactions" asChild reset="always">
-                                    <TabButton icon={UserIconNameEnum.Receipt} />
-                                </TabTrigger>
-
-                                <TabTrigger name="analytics" asChild reset="always">
-                                    <TabButton icon={UserIconNameEnum.ChartNoAxesColumn} />
-                                </TabTrigger>
-
-                                <TabTrigger name="settings" asChild reset="always">
-                                    <TabButton icon={UserIconNameEnum.Settings} />
-                                </TabTrigger>
-                            </View>
+                            <TabButtons />
 
                             <CreateTransactionTrigger isOpen={isMenuOpen} onPress={handleOpenMenu} />
                         </View>
