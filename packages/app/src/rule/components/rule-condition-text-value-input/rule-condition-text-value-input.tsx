f2bdd3c4c4c0b286b20961a083ee5eeb353f7003
liaugust <ivan.liaugust@gmail.com>
2026-01-11 03:00:38 +0200

    fix: split rule condition value into separate inputs for amount and text values
    Signed-off-by: liaugust <ivan.liaugust@gmail.com>

---
+++ packages/app/src/rule/components/rule-condition-text-value-input/rule-condition-text-value-input.tsx
@@ -0,0 +1,27 @@
+import { RuleCreateInputInterface } from '@budgie/contracts';
+import { useLingui } from '@lingui/react/macro';
+import { Controller, UseControllerReturn, useFormContext } from 'react-hook-form';
+
+import { FormItem } from '../../../@generic/component/form-item/form-item';
+import { Input } from '../../../@generic/component/input/input';
+
+interface Props {
+    readonly index: number;
+}
+
+export const RuleConditionTextValueInput = ({ index }: Props) => {
+    const { t } = useLingui();
+    const { control } = useFormContext<RuleCreateInputInterface>();
+
+    const renderValueInput = ({
+        field: { value, onChange }
+    }: UseControllerReturn<RuleCreateInputInterface, `conditions.${number}.value`>) => (
+        <Input value={value} onChangeText={onChange} placeholder={t`Enter value...`} />
+    );
+
+    return (
+        <FormItem label={t`Value`}>
+            <Controller control={control} name={`conditions.${index}.value`} render={renderValueInput} />
+        </FormItem>
+    );
+};
