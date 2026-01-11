7ddc5f3fd62bc1056e2904b2370197808b096158
liaugust <ivan.liaugust@gmail.com>
2026-01-11 03:00:38 +0200

    fix: split rule condition value into separate inputs for amount and text values
    Signed-off-by: liaugust <ivan.liaugust@gmail.com>

--- packages/app/src/rule/components/rule-condition-value-input/rule-condition-value-input.tsx
+++ packages/app/src/rule/components/rule-condition-value-input/rule-condition-value-input.tsx
@@ -1,12 +1,11 @@
-import { RuleConditionFieldEnum, RuleConditionOperatorEnum, RuleCreateInputInterface } from '@budgie/contracts';
-import { useLingui } from '@lingui/react/macro';
-import { Controller, UseControllerReturn, useFormContext, useWatch } from 'react-hook-form';
+import { RuleConditionFieldEnum, RuleCreateInputInterface } from '@budgie/contracts';
+import { useFormContext, useWatch } from 'react-hook-form';
 
-import { FormItem } from '../../../@generic/component/form-item/form-item';
-import { Input } from '../../../@generic/component/input/input';
 import { RuleConditionAccountSelector } from '../rule-condition-account-selector/rule-condition-account-selector';
+import { RuleConditionAmountValueInput } from '../rule-condition-amount-value-input/rule-condition-amount-value-input';
 import { RuleConditionExternalSourceSelector } from '../rule-condition-external-source-selector/rule-condition-external-source-selector';
 import { RuleConditionMccSelector } from '../rule-condition-mcc-selector/rule-condition-mcc-selector';
+import { RuleConditionTextValueInput } from '../rule-condition-text-value-input/rule-condition-text-value-input';
 import { RuleConditionTransactionTypeSelector } from '../rule-condition-transaction-type-selector/rule-condition-transaction-type-selector';
 
 interface Props {
@@ -14,51 +13,21 @@ interface Props {
 }
 
 export const RuleConditionValueInput = ({ index }: Props) => {
-    const { t } = useLingui();
     const { control } = useFormContext<RuleCreateInputInterface>();
-    const [field, operator] = useWatch({ control, name: [`conditions.${index}.field`, `conditions.${index}.operator`] });
-    const showSecondaryValue = operator === RuleConditionOperatorEnum.BETWEEN;
+    const field = useWatch({ control, name: `conditions.${index}.field` });
 
-    const renderValueInput = ({
-        field: { value, onChange }
-    }: UseControllerReturn<RuleCreateInputInterface, `conditions.${number}.value`>) => (
-        <Input value={value} onChangeText={onChange} placeholder={t`Enter value...`} />
-    );
-
-    const renderSecondaryValueInput = ({
-        field: { value, onChange }
-    }: UseControllerReturn<RuleCreateInputInterface, `conditions.${number}.secondaryValue`>) => (
-        <Input value={value ?? ''} onChangeText={onChange} placeholder={t`Enter secondary value...`} />
-    );
-
-    const renderPrimaryInput = () => {
-        switch (field) {
-            case RuleConditionFieldEnum.ACCOUNT_ID:
-                return <RuleConditionAccountSelector index={index} />;
-            case RuleConditionFieldEnum.TRANSACTION_TYPE:
-                return <RuleConditionTransactionTypeSelector index={index} />;
-            case RuleConditionFieldEnum.EXTERNAL_SOURCE:
-                return <RuleConditionExternalSourceSelector index={index} />;
-            case RuleConditionFieldEnum.MCC_CODE:
-                return <RuleConditionMccSelector index={index} />;
-            default:
-                return (
-                    <FormItem label={t`Value`}>
-                        <Controller control={control} name={`conditions.${index}.value`} render={renderValueInput} />
-                    </FormItem>
-                );
-        }
-    };
-
-    return (
-        <>
-            {renderPrimaryInput()}
-
-            {showSecondaryValue && (
-                <FormItem label={t`Secondary Value`}>
-                    <Controller control={control} name={`conditions.${index}.secondaryValue`} render={renderSecondaryValueInput} />
-                </FormItem>
-            )}
-        </>
-    );
+    switch (field) {
+        case RuleConditionFieldEnum.ACCOUNT_ID:
+            return <RuleConditionAccountSelector index={index} />;
+        case RuleConditionFieldEnum.TRANSACTION_TYPE:
+            return <RuleConditionTransactionTypeSelector index={index} />;
+        case RuleConditionFieldEnum.EXTERNAL_SOURCE:
+            return <RuleConditionExternalSourceSelector index={index} />;
+        case RuleConditionFieldEnum.MCC_CODE:
+            return <RuleConditionMccSelector index={index} />;
+        case RuleConditionFieldEnum.AMOUNT:
+            return <RuleConditionAmountValueInput index={index} />;
+        default:
+            return <RuleConditionTextValueInput index={index} />;
+    }
 };
