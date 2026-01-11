7d8c25dc2b84ffa1f39f1bc8ec556849caf5c69c
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-11 10:20:29 +0100

    Revert "fix(landing): fix hydration mismatch for theme-dependent components"
    This reverts commit 1da6011c305b886f7b9c47fbdeefb1f0ca7383ad.

--- packages/landing/src/generic/hook/use-mounted.hook.ts
+++
@@ -1,7 +0,0 @@
-import { useSyncExternalStore } from 'react';
-
-const noop = (): (() => void) => noop;
-const getSnapshot = (): boolean => true;
-const getServerSnapshot = (): boolean => false;
-
-export const useMounted = (): boolean => useSyncExternalStore(noop, getSnapshot, getServerSnapshot);
