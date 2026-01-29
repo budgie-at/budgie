aaae9f484228385bf308bb82332f39e922372753
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-29 10:10:50 +0100

    chore: translations

--- packages/app/src/transaction/constant/convert-to-transfer-schema.constant.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const ConvertToTransferSchema = z.object({
-    accountId: z.number().positive()
-});
-
-export type ConvertToTransferFormValues = z.infer<typeof ConvertToTransferSchema>;
