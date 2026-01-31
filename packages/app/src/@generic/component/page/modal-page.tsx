4fb7cce88a56db0258daf322f403d9b3933df1e8
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-01-31 20:44:11 +0100

    Revert "fix(app): dismiss keyboard on tap outside input in modal forms"
    This reverts commit 678ed8033c78b07aac7ae2cb404999cf91d7c067.

--- packages/app/src/@generic/component/page/modal-page.tsx
+++ packages/app/src/@generic/component/page/modal-page.tsx
@@ -1,5 +1,4 @@
 import { ComponentProps } from 'react';
-import { Keyboard, Pressable } from 'react-native';
 import { Edge } from 'react-native-safe-area-context';
 
 import { cn } from '../../utils/cn.util';
@@ -8,14 +7,6 @@ import { Page } from './page';
 
 const safeEdges: Edge[] = ['bottom'];
 
-const dismissKeyboard = (): void => {
-    Keyboard.dismiss();
-};
-
-export const ModalPage = ({ className, children, ...rest }: ComponentProps<typeof Page>) => (
-    <Page {...rest} className={cn('pt-3xl', className)} safeEdges={safeEdges}>
-        <Pressable onPress={dismissKeyboard} className="flex-1">
-            {children}
-        </Pressable>
-    </Page>
+export const ModalPage = ({ className, ...rest }: ComponentProps<typeof Page>) => (
+    <Page {...rest} className={cn('pt-3xl', className)} safeEdges={safeEdges} />
 );
