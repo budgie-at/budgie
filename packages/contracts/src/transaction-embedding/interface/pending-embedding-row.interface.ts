f3897ba9ca12a6c6253646d89131fc2a17163ced
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-04-17 18:10:44 +0100

    feat(contracts): add PendingEmbeddingRowInterface

---
+++ packages/contracts/src/transaction-embedding/interface/pending-embedding-row.interface.ts
@@ -0,0 +1,11 @@
+import { AccountEntityInterface } from '../../account/entity/account-entity.interface';
+import { TransactionEntryWithRelationsEntityInterface } from '../../transaction-entry/entity/transaction-entry-with-relations-entity.interface';
+import { TransactionTagsWithTagEntityInterface } from '../../transaction-tags/entity/transaction-tags-with-tag-entity.interface';
+import { TransactionEntityInterface } from '../../transaction/entity/transaction-entity.interface';
+
+export interface PendingEmbeddingRowInterface extends TransactionEntityInterface {
+    readonly entries: TransactionEntryWithRelationsEntityInterface[];
+    readonly transactionTags: TransactionTagsWithTagEntityInterface[];
+    readonly fromAccount: AccountEntityInterface | null;
+    readonly toAccount: AccountEntityInterface | null;
+}
