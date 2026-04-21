edacec36b18756d2ff52c42a348209dad2600b79
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-04-21 13:43:30 +0100

    fix(app): drop unused fts tables

--- packages/app/drizzle/migrations.js
+++ packages/app/drizzle/migrations.js
@@ -18,11 +18,12 @@ import m0013 from './0013_fat_wild_child.sql';
 import m0014 from './0014_light_calypso.sql';
 import m0015 from './0015_add_entry_exchange_rate_and_to_iban.sql';
 import m0016 from './0016_add_needs_embedding.sql';
+import m0017 from './0017_drop_unused_fts_tables.sql';
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
+export default {
+  journal,
+  migrations: {
+    m0000,
 m0001,
 m0002,
 m0003,
@@ -38,7 +39,7 @@ m0012,
 m0013,
 m0014,
 m0015,
-m0016
-    }
+m0016,
+m0017
   }
-  
+};
