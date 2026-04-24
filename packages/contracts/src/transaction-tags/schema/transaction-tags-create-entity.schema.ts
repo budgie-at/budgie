27c92f5b9c5b675ed4f37d36e842800ce6a0918b
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-04-24 17:32:32 +0200

    feat(contracts): include isPrimary in transaction tags create schema

--- packages/contracts/src/transaction-tags/schema/transaction-tags-create-entity.schema.ts
+++ packages/contracts/src/transaction-tags/schema/transaction-tags-create-entity.schema.ts
@@ -2,5 +2,6 @@ import { TransactionTagsEntitySchema } from './transaction-tags-entity.schema';
 
 export const TransactionTagsCreateEntitySchema = TransactionTagsEntitySchema.pick({
     transactionId: true,
-    tagId: true
+    tagId: true,
+    isPrimary: true
 });
