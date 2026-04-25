500d0bbce4c08758f06bc87f3cfa023f2d9b298c
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-04-25 13:35:54 +0200

    fix(contracts): wait for sqlite transaction commit

--- packages/contracts/src/@generic/util/transaction-async.util.ts
+++ packages/contracts/src/@generic/util/transaction-async.util.ts
@@ -1,14 +1,35 @@
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 
+import { isDefined } from '@rnw-community/shared';
+
 import * as schema from '../../schema';
 
 import type { DB } from '../type/db.type';
 
-export const transactionAsync = <T>(database: DB, callback: (txDb: DB) => Promise<T>): Promise<T> =>
-    new Promise<T>((resolve, reject) => {
-        void database.$client
-            .withExclusiveTransactionAsync(async expoTransaction => {
-                resolve(await callback(drizzle(expoTransaction, { schema }) as DB));
-            })
-            .catch(reject);
+class TransactionAsyncResult<T> {
+    private result: { readonly value: T } | null = null;
+
+    set(value: T): void {
+        this.result = { value };
+    }
+
+    get(): T {
+        const {result} = this;
+
+        if (!isDefined(result)) {
+            throw new Error('Transaction did not produce a result');
+        }
+
+        return result.value;
+    }
+}
+
+export const transactionAsync = async <T>(database: DB, callback: (txDb: DB) => Promise<T>): Promise<T> => {
+    const result = new TransactionAsyncResult<T>();
+
+    await database.$client.withExclusiveTransactionAsync(async expoTransaction => {
+        result.set(await callback(drizzle(expoTransaction, { schema })));
     });
+
+    return result.get();
+};
