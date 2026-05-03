729b2da42f095d2e5f3f6d41dfb1ab518e9128c0
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-05-03 12:15:20 +0200

    feat(landing): add feature FAQ interface

---
+++ packages/landing/src/feature/interface/feature-faq.interface.ts
@@ -0,0 +1,6 @@
+import type { MessageDescriptor } from '@lingui/core';
+
+export interface FeatureFaqInterface {
+    readonly question: MessageDescriptor;
+    readonly answer: MessageDescriptor;
+}
