c886aa49baa0ac0d280f54437ad41d2cb3b290e2
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-05-03 12:22:21 +0200

    feat(landing): add feature page JSON-LD component

---
+++ packages/landing/src/feature/component/feature-page-json-ld/feature-page-json-ld.tsx
@@ -0,0 +1,13 @@
+import { JsonLd } from '../../../generic/component/json-ld/json-ld';
+
+interface Props {
+    readonly schemas: readonly Record<string, unknown>[];
+}
+
+export const FeaturePageJsonLd = ({ schemas }: Props) => (
+    <>
+        {schemas.map((schema, index) => (
+            <JsonLd key={`${schema['@type']}-${index}`} data={schema} />
+        ))}
+    </>
+);
