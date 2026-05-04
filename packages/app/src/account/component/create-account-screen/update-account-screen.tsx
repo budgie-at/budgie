5dbb07eac95f9276565632fa379e1e0b8761b759
Vitaly Iegorov <vitalyiegorov@gmail.com>
2026-05-04 07:46:19 +0200

    fix(app): disable account type changes (#410)

--- packages/app/src/account/component/create-account-screen/update-account-screen.tsx
+++ packages/app/src/account/component/create-account-screen/update-account-screen.tsx
@@ -5,23 +5,20 @@ import {
     LiabilityAccountCreateInputInterface
 } from '@budgie/contracts';
 import { useLingui } from '@lingui/react/macro';
-import { cva } from 'class-variance-authority';
 import { ReactNode } from 'react';
-import { Control, FieldValues, useWatch } from 'react-hook-form';
+import { Control, FieldValues } from 'react-hook-form';
 import { View } from 'react-native';
 
-import { EmptyFn, isDefined } from '@rnw-community/shared';
+import { EmptyFn } from '@rnw-community/shared';
 
 import { AccountDetailsField } from '../../../@generic/component/account-details-field/account-details-field';
 import { Button } from '../../../@generic/component/button/button';
 import { FormLayoutGroup } from '../../../@generic/component/form-layout-group/form-layout-group';
 import { FormPage } from '../../../@generic/component/form-page/form-page';
 import { PageHeader } from '../../../@generic/component/page-header/page-header';
-import { FOREGROUND_COLOR_PALETTE } from '../../../@generic/constant/foreground-color-palette.constant';
 import { goBackOrReplace } from '../../../@generic/utils/go-back-or-replace.util';
 import { ACCOUNT_COLOR } from '../../constant/account-color.constant';
 import { ACCOUNT_DEBT_TYPE_COLOR } from '../../constant/account-debt-type-color.constant';
-import { ACCOUNT_TYPE } from '../../constant/account-type.constant';
 import { AccountActiveToggleField } from '../account-active-toggle-field/account-active-toggle-field';
 import { AccountBalanceField } from '../account-balance-field/account-balance-field';
 import { ArchiveAccount } from '../archive-account/archive-account';
@@ -37,34 +34,18 @@ interface Props<T extends FieldValues> {
     readonly onSubmit: EmptyFn;
 }
 
-const descriptionVariants = cva('uppercase', {
-    variants: { variant: FOREGROUND_COLOR_PALETTE }
-});
-
 export const UpdateAccountScreen = <T extends LiabilityAccountCreateInputInterface | DebtAccountCreateInputInterface>(props: Props<T>) => {
     const { children, account, onSubmit, control, instrumentSymbol, allowNegativeBalance } = props;
     const { t } = useLingui();
 
-    const formValues = useWatch({ control });
-    const currentType = isDefined(formValues.type) ? (formValues.type as AccountTypeEnum) : account.type;
-
     const handleGoBack = () => void goBackOrReplace('/');
 
-    const description = t(ACCOUNT_TYPE[currentType]);
-    const variant = currentType === AccountTypeEnum.DEBT ? ACCOUNT_DEBT_TYPE_COLOR[account.debtType] : ACCOUNT_COLOR[currentType];
+    const variant = account.type === AccountTypeEnum.DEBT ? ACCOUNT_DEBT_TYPE_COLOR[account.debtType] : ACCOUNT_COLOR[account.type];
 
     return (
         <FormPage
             scrollViewTestID={CreateAccountScreenSelector.ScrollView}
-            header={
-                <PageHeader
-                    iconVariant={variant}
-                    onGoBack={handleGoBack}
-                    description={description}
-                    title={t`Account Settings`}
-                    descriptionClassName={descriptionVariants({ variant })}
-                />
-            }
+            header={<PageHeader iconVariant={variant} onGoBack={handleGoBack} title={t`Account Settings`} />}
             footer={
                 <View className="flex-row gap-2">
                     <ArchiveAccount accountId={account.id} />
