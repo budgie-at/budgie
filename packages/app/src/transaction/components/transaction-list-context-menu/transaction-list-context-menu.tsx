e6ab589db44f6b2fc753ded66fe95a62915dbf09
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-05-24 16:40:15 +0200

    fix(app): polish refund actions

--- packages/app/src/transaction/components/transaction-list-context-menu/transaction-list-context-menu.tsx
+++ packages/app/src/transaction/components/transaction-list-context-menu/transaction-list-context-menu.tsx
@@ -9,6 +9,7 @@ import { emptyFn, isDefined } from '@rnw-community/shared';
 import { PopoverMenu } from '../../../@generic/component/popover-menu/popover-menu';
 import { PopoverMenuItem } from '../../../@generic/component/popover-menu-item/popover-menu-item';
 import { convertFromMicroUnits } from '../../../@generic/utils/convert-from-micro-units.util';
+import { useConvertToRefundModal } from '../../context/convert-to-refund-modal.context';
 import { useConvertToTransferModal } from '../../context/convert-to-transfer-modal.context';
 import { useDeleteTransaction } from '../../hook/use-delete-transaction.hook';
 import { useRevertConsolidation } from '../../hook/use-revert-consolidation.hook';
@@ -29,6 +30,24 @@ const getConvertTransactionType = (
 ): TransactionTypeEnum.EXPENSE | TransactionTypeEnum.INCOME =>
     isExpenseTransaction(transaction) ? TransactionTypeEnum.EXPENSE : TransactionTypeEnum.INCOME;
 
+const openTransactionListTransferConversion = (
+    transaction: TransactionWithRelationsEntityInterface,
+    openConvertToTransfer: ReturnType<typeof useConvertToTransferModal>[0]
+) => {
+    const [sourceEntry] = transaction.entries;
+    const sourceAccount = sourceEntry.account;
+
+    openConvertToTransfer({
+        transactionId: transaction.id,
+        transactionType: getConvertTransactionType(transaction),
+        excludeAccountId: sourceEntry.accountId,
+        sourceAmount: convertFromMicroUnits(sourceEntry.amount),
+        sourceInstrumentId: sourceAccount.instrumentId,
+        sourceCode: sourceAccount.instrument.code,
+        skipPostConvertNavigation: true
+    }).catch(emptyFn);
+};
+
 // eslint-disable-next-line max-statements -- Context menu component with deferred action pattern and multiple handlers
 export const TransactionListContextMenu = ({
     transaction,
@@ -40,6 +59,7 @@ export const TransactionListContextMenu = ({
     const { t } = useLingui();
     const router = useRouter();
     const deleteTransaction = useDeleteTransaction();
+    const [openConvertToRefund] = useConvertToRefundModal();
     const [openConvertToTransfer] = useConvertToTransferModal();
     const pendingActionRef = useRef<EmptyFn | null>(null);
     const transactionId = transaction?.id ?? 0;
@@ -50,7 +70,9 @@ export const TransactionListContextMenu = ({
     }
 
     const isConsolidated = isDefined(transaction.consolidationType);
+    const isRefunded = isDefined(transaction.consolidationParentTransactionId);
     const canConvert = !isConsolidated && isConvertibleTransaction(transaction);
+    const canConvertToRefund = !isConsolidated && !isRefunded && isIncomeTransaction(transaction);
     const actionLabel = isConsolidated ? t`Revert` : t`Delete Transaction`;
     const actionIcon = isConsolidated ? UserIconNameEnum.Undo2 : UserIconNameEnum.Trash2;
     const actionTestID = isConsolidated ? TransactionListContextMenuSelector.RevertButton : TransactionListContextMenuSelector.DeleteButton;
@@ -91,19 +113,14 @@ export const TransactionListContextMenu = ({
     };
 
     const handleConvertPress = () => {
-        const [sourceEntry] = transaction.entries;
-        const sourceAccount = sourceEntry.account;
-
         closeMenu(() => {
-            openConvertToTransfer({
-                transactionId: transaction.id,
-                transactionType: getConvertTransactionType(transaction),
-                excludeAccountId: sourceEntry.accountId,
-                sourceAmount: convertFromMicroUnits(sourceEntry.amount),
-                sourceInstrumentId: sourceAccount.instrumentId,
-                sourceCode: sourceAccount.instrument.code,
-                skipPostConvertNavigation: true
-            }).catch(emptyFn);
+            openTransactionListTransferConversion(transaction, openConvertToTransfer);
+        });
+    };
+
+    const handleConvertToRefundPress = () => {
+        closeMenu(() => {
+            openConvertToRefund({ refundIncomeTransactionId: transaction.id }).catch(emptyFn);
         });
     };
 
@@ -116,6 +133,7 @@ export const TransactionListContextMenu = ({
                     onPress={handleEditPress}
                     testID={TransactionListContextMenuSelector.EditButton}
                 />
+                <TransactionListConvertMenuItem isVisible={canConvertToRefund} isRefund onConvert={handleConvertToRefundPress} />
                 <TransactionListConvertMenuItem isVisible={canConvert} onConvert={handleConvertPress} />
                 <PopoverMenuItem
                     icon={actionIcon}
