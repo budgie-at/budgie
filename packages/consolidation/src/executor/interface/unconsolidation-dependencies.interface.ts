3777a247781da3e1c295a42e068d05cb2aef2601
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-01 13:15:53 +0200

    refactor: move unconsolidation service

---
+++ packages/consolidation/src/executor/interface/unconsolidation-dependencies.interface.ts
@@ -0,0 +1,17 @@
+import type {
+    TransactionEntryRepository,
+    TransactionRepository,
+    TransactionTagsRepository
+} from '@budgie/contracts';
+
+export interface UnconsolidationDependenciesInterface {
+    readonly transactionEntryRepository: Pick<
+        TransactionEntryRepository,
+        'deleteLedgerByTransactionId' | 'moveBackToOriginalTransactions'
+    >;
+    readonly transactionRepository: Pick<
+        TransactionRepository,
+        'clearConsolidationParent' | 'deleteById' | 'getByIdRaw' | 'setConsolidationType'
+    >;
+    readonly transactionTagsRepository: Pick<TransactionTagsRepository, 'deleteByTransactionId'>;
+}
--- packages/consolidation/src/index.ts
+++ packages/consolidation/src/index.ts
@@ -1,6 +1,7 @@
 export { ConsolidationAutoCandidateService } from './auto/service/consolidation-auto-candidate.service';
 export { ConsolidationCandidateService } from './auto/service/consolidation-candidate.service';
 export { ConsolidationExecutorService } from './executor/service/consolidation-executor.service';
+export { UnconsolidationService } from './executor/service/unconsolidation.service';
 
 export type { ConsolidationCandidateDependenciesInterface } from './auto/interface/consolidation-candidate-dependencies.interface';
 export type { ConsolidationCandidateGroupsInterface } from './auto/interface/consolidation-candidate-groups.interface';
@@ -9,4 +10,5 @@ export type { ConsolidationProgressSnapshotInterface } from './auto/interface/co
 export type { ConsolidationResultInterface } from './auto/interface/consolidation-result.interface';
 export type { CanonicalTransferInputInterface } from './executor/interface/canonical-transfer-input.interface';
 export type { ConsolidationExecutorDependenciesInterface } from './executor/interface/consolidation-executor-dependencies.interface';
+export type { UnconsolidationDependenciesInterface } from './executor/interface/unconsolidation-dependencies.interface';
 export type { TransactionRunnerInterface } from './wiring/interface/transaction-runner.interface';
