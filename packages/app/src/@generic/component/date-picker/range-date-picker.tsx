238f1b2763f9cb3b1c5a604e05067019673e7edc
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-03 18:48:19 +0200

    fix(app): normalize single-day date ranges

--- packages/app/src/@generic/component/date-picker/range-date-picker.tsx
+++ packages/app/src/@generic/component/date-picker/range-date-picker.tsx
@@ -2,8 +2,11 @@ import { DateRangeInterface } from '@budgie/contracts';
 import { ImpactFeedbackStyle } from 'expo-haptics/src/Haptics.types';
 import { DateType } from 'react-native-ui-datepicker';
 
+import { isDefined } from '@rnw-community/shared';
+
 import { useVibration } from '../../hook/use-vibration.hook';
 import { dateTypeToDate } from '../../utils/date/date-type-to-date.util';
+import { getDateRangeForDay } from '../../utils/date/get-date-range-for-day.util';
 
 import { DatePicker } from './date-picker';
 
@@ -17,9 +20,18 @@ export const RangeDatePicker = ({ range, onChange }: Props) => {
 
     const handleChange = (value: { startDate?: DateType; endDate?: DateType }) => {
         hapticImpact(ImpactFeedbackStyle.Light);
+        const from = dateTypeToDate(value.startDate);
+        const to = dateTypeToDate(value.endDate);
+
+        if (isDefined(from) && !isDefined(to)) {
+            onChange(getDateRangeForDay(from));
+
+            return;
+        }
+
         onChange({
-            from: dateTypeToDate(value.startDate),
-            to: dateTypeToDate(value.endDate)
+            from,
+            to
         });
     };
 
