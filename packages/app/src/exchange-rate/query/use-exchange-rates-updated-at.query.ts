6d27ac808123fd73e996199951af46638d29233e
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-05 13:15:08 +0200

    fix(app): reduce crypto rate refresh jank

--- packages/app/src/exchange-rate/query/use-exchange-rates-updated-at.query.ts
+++ packages/app/src/exchange-rate/query/use-exchange-rates-updated-at.query.ts
@@ -3,7 +3,7 @@ import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 import { exchangeRateRepository } from '../../@generic/drizzle/db/db';
 
 export const useExchangeRatesUpdatedAtQuery = () => {
-    const { updatedAt } = useLiveQuery(exchangeRateRepository.getAll(), []);
+    const { updatedAt } = useLiveQuery(exchangeRateRepository.getLatestUpdatedAt(), []);
 
     return updatedAt;
 };
