6f5858242bc1b882d3c5be57f7fa44f1a01ea7ca
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-06 08:35:04 +0200

    refactor(app): simplify Home balance summary

--- packages/app/src/account/query/use-accounts-with-bank-sync.query.ts
+++ packages/app/src/account/query/use-accounts-with-bank-sync.query.ts
@@ -1,14 +1,12 @@
 import { AccountWithBankSyncEntityInterface } from '@budgie/contracts';
 import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 
-import { isDefined } from '@rnw-community/shared';
-
 import { accountRepository } from '../../@generic/drizzle/db/db';
 
 export const useAccountsWithBankSyncQuery = () => {
     const { data, updatedAt, error } = useLiveQuery(accountRepository.findAllWithBankSync(), []);
 
-    const accounts: AccountWithBankSyncEntityInterface[] = isDefined(data) ? data : [];
+    const accounts: AccountWithBankSyncEntityInterface[] = data;
 
     return { accounts, updatedAt, error };
 };
