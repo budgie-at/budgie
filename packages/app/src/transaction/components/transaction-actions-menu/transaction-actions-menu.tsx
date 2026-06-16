ed0e86745edd893bbf3cc14ab06a14cc57594211
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-16 11:58:44 +0200

    fix(app): keep transaction menu actions stable

--- packages/app/src/transaction/components/transaction-actions-menu/transaction-actions-menu.tsx
+++ packages/app/src/transaction/components/transaction-actions-menu/transaction-actions-menu.tsx
@@ -1,6 +1,6 @@
 import { UserIconNameEnum } from '@budgie/contracts';
 import { useLingui } from '@lingui/react/macro';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { GestureResponderEvent, View } from 'react-native';
 
 import { emptyFn, isDefined } from '@rnw-community/shared';
@@ -9,22 +9,39 @@ import { CircleIcon } from '../../../@generic/component/circle-icon/circle-icon'
 import { HapticPressable } from '../../../@generic/component/haptic-pressable/haptic-pressable';
 import { PopoverMenu, PopoverMenuAnchor } from '../../../@generic/component/popover-menu/popover-menu';
 import { PopoverMenuItem } from '../../../@generic/component/popover-menu-item/popover-menu-item';
-import { useDeferredMenuClose } from '../../../@generic/hook/use-deferred-menu-close.hook';
 import { TransactionActionsMenuContext } from '../../context/transaction-actions-menu.context';
 
 import { TransactionActionsMenuSelector } from './transaction-actions-menu.selector';
 
 import type { TransactionActionsMenuPropsInterface } from '../../interface/transaction-actions-menu-props.interface';
+import type { EmptyFn } from '@rnw-community/shared';
 
 const TRIGGER_SIZE = 40;
 
 export const TransactionActionsMenu = ({ onDelete, onRevert, isConsolidated = false, children }: TransactionActionsMenuPropsInterface) => {
     const { t } = useLingui();
-    const { isMenuOpen, closeMenu, handleCloseComplete, openMenu } = useDeferredMenuClose();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [anchor, setAnchor] = useState<PopoverMenuAnchor | undefined>();
+    const pendingActionRef = useRef<EmptyFn | null>(null);
     const actionLabel = isConsolidated ? t`Revert` : t`Delete Transaction`;
     const actionIcon = isConsolidated ? UserIconNameEnum.Undo2 : UserIconNameEnum.Trash2;
     const actionTestID = isConsolidated ? TransactionActionsMenuSelector.RevertButton : TransactionActionsMenuSelector.DeleteButton;
+
+    const openMenu = () => {
+        setIsMenuOpen(true);
+    };
+
+    const closeMenu = (afterClose?: EmptyFn) => {
+        pendingActionRef.current = afterClose ?? null;
+        setIsMenuOpen(false);
+    };
+
+    const handleCloseComplete = () => {
+        if (isDefined(pendingActionRef.current)) {
+            pendingActionRef.current();
+            pendingActionRef.current = null;
+        }
+    };
 
     const handleToggleMenu = (event: GestureResponderEvent) => {
         if (isMenuOpen) {
