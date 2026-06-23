ea8ef8db140bd9ef2df8e19ac3a861f04dfee223
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-24 01:17:19 +0200

    fix(app): expose transaction keypad submit target

--- packages/app/src/transaction/components/transaction-keypad/transaction-keypad.tsx
+++ packages/app/src/transaction/components/transaction-keypad/transaction-keypad.tsx
@@ -68,15 +68,14 @@ export const TransactionKeypad = (props: Props) => {
 
                 <View className="flex-row gap-md">
                     <TransactionKeypadButton icon={UserIconNameEnum.X} variant="cancel" onPress={onCancel} />
-                    <View className="flex-2" collapsable={false} testID={confirmTestID}>
-                        <TransactionKeypadButton
-                            icon={UserIconNameEnum.CircleCheck}
-                            variant="confirm"
-                            colorVariant={variant}
-                            onPress={onConfirm}
-                            disabled={isConfirmDisabled}
-                        />
-                    </View>
+                    <TransactionKeypadButton
+                        icon={UserIconNameEnum.CircleCheck}
+                        variant="confirm"
+                        colorVariant={variant}
+                        onPress={onConfirm}
+                        disabled={isConfirmDisabled}
+                        testID={confirmTestID}
+                    />
                 </View>
             </View>
         </Animated.View>
