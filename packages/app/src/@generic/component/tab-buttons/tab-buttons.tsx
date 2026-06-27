c1ff5219b47697d239d51b9f42902bf645b29b3f
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-27 06:44:14 +0200

    fix(app): keep tab selectors stable

--- packages/app/src/@generic/component/tab-buttons/tab-buttons.tsx
+++ packages/app/src/@generic/component/tab-buttons/tab-buttons.tsx
@@ -8,20 +8,28 @@ import { TabButtonsSelector } from './tab-buttons.selector';
 
 export const TabButtons = () => (
     <View className="flex-row items-center gap-sm">
-        <TabTrigger name="home" asChild resetOnFocus>
-            <TabButton buttonTestID={TabButtonsSelector.Home} icon={UserIconNameEnum.Home} />
-        </TabTrigger>
+        <View testID={TabButtonsSelector.Home} nativeID={TabButtonsSelector.Home} collapsable={false}>
+            <TabTrigger name="home" asChild resetOnFocus>
+                <TabButton buttonTestID={TabButtonsSelector.Home} icon={UserIconNameEnum.Home} />
+            </TabTrigger>
+        </View>
 
-        <TabTrigger name="transactions" asChild resetOnFocus>
-            <TabButton buttonTestID={TabButtonsSelector.Transactions} icon={UserIconNameEnum.Receipt} />
-        </TabTrigger>
+        <View testID={TabButtonsSelector.Transactions} nativeID={TabButtonsSelector.Transactions} collapsable={false}>
+            <TabTrigger name="transactions" asChild resetOnFocus>
+                <TabButton buttonTestID={TabButtonsSelector.Transactions} icon={UserIconNameEnum.Receipt} />
+            </TabTrigger>
+        </View>
 
-        <TabTrigger name="analytics" asChild resetOnFocus>
-            <TabButton buttonTestID={TabButtonsSelector.Analytics} icon={UserIconNameEnum.ChartNoAxesColumn} />
-        </TabTrigger>
+        <View testID={TabButtonsSelector.Analytics} nativeID={TabButtonsSelector.Analytics} collapsable={false}>
+            <TabTrigger name="analytics" asChild resetOnFocus>
+                <TabButton buttonTestID={TabButtonsSelector.Analytics} icon={UserIconNameEnum.ChartNoAxesColumn} />
+            </TabTrigger>
+        </View>
 
-        <TabTrigger name="settings" asChild resetOnFocus>
-            <TabButton buttonTestID={TabButtonsSelector.Settings} icon={UserIconNameEnum.Settings} />
-        </TabTrigger>
+        <View testID={TabButtonsSelector.Settings} nativeID={TabButtonsSelector.Settings} collapsable={false}>
+            <TabTrigger name="settings" asChild resetOnFocus>
+                <TabButton buttonTestID={TabButtonsSelector.Settings} icon={UserIconNameEnum.Settings} />
+            </TabTrigger>
+        </View>
     </View>
 );
