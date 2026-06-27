235a13fd35541b4d459bbb236b526e9f55b23966
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-27 09:49:06 +0200

    fix(app): pass tab wrapper touches through

--- packages/app/src/@generic/component/tab-buttons/tab-buttons.tsx
+++ packages/app/src/@generic/component/tab-buttons/tab-buttons.tsx
@@ -8,25 +8,30 @@ import { TabButtonsSelector } from './tab-buttons.selector';
 
 export const TabButtons = () => (
     <View className="flex-row items-center gap-sm">
-        <View testID={TabButtonsSelector.Home} nativeID={TabButtonsSelector.Home} collapsable={false}>
+        <View testID={TabButtonsSelector.Home} nativeID={TabButtonsSelector.Home} collapsable={false} pointerEvents="box-none">
             <TabTrigger name="home" asChild resetOnFocus>
                 <TabButton buttonTestID={TabButtonsSelector.Home} icon={UserIconNameEnum.Home} />
             </TabTrigger>
         </View>
 
-        <View testID={TabButtonsSelector.Transactions} nativeID={TabButtonsSelector.Transactions} collapsable={false}>
+        <View
+            testID={TabButtonsSelector.Transactions}
+            nativeID={TabButtonsSelector.Transactions}
+            collapsable={false}
+            pointerEvents="box-none"
+        >
             <TabTrigger name="transactions" asChild resetOnFocus>
                 <TabButton buttonTestID={TabButtonsSelector.Transactions} icon={UserIconNameEnum.Receipt} />
             </TabTrigger>
         </View>
 
-        <View testID={TabButtonsSelector.Analytics} nativeID={TabButtonsSelector.Analytics} collapsable={false}>
+        <View testID={TabButtonsSelector.Analytics} nativeID={TabButtonsSelector.Analytics} collapsable={false} pointerEvents="box-none">
             <TabTrigger name="analytics" asChild resetOnFocus>
                 <TabButton buttonTestID={TabButtonsSelector.Analytics} icon={UserIconNameEnum.ChartNoAxesColumn} />
             </TabTrigger>
         </View>
 
-        <View testID={TabButtonsSelector.Settings} nativeID={TabButtonsSelector.Settings} collapsable={false}>
+        <View testID={TabButtonsSelector.Settings} nativeID={TabButtonsSelector.Settings} collapsable={false} pointerEvents="box-none">
             <TabTrigger name="settings" asChild resetOnFocus>
                 <TabButton buttonTestID={TabButtonsSelector.Settings} icon={UserIconNameEnum.Settings} />
             </TabTrigger>
