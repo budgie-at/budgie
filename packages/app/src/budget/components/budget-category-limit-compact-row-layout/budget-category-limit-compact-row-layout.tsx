6bf3422cd56ee130686de633fe9bd783aea94ed2
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-06-28 21:44:53 +0200

    fix(app): expose budget category limit row target

--- packages/app/src/budget/components/budget-category-limit-compact-row-layout/budget-category-limit-compact-row-layout.tsx
+++ packages/app/src/budget/components/budget-category-limit-compact-row-layout/budget-category-limit-compact-row-layout.tsx
@@ -10,9 +10,9 @@ interface Props {
 }
 
 export const BudgetCategoryLimitCompactRowLayout = ({ amountInput, icon, testID, title }: Props) => (
-    <View testID={testID} collapsable={false} className="flex-row items-center gap-x-md bg-primary-reverse px-md py-sm rounded-2xl">
+    <View collapsable={false} className="flex-row items-center gap-x-md bg-primary-reverse px-md py-sm rounded-2xl">
         {icon}
-        <Text className="text-primary text-md flex-1" numberOfLines={1}>
+        <Text className="text-primary text-md flex-1" numberOfLines={1} testID={testID}>
             {title}
         </Text>
         {amountInput}
