47eaebc8f7bfa4fbda7c4d0e904e0dff34990c03
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-07-16 20:30:57 +0200

    fix(app): prevent create action press cancellation

--- packages/app/src/transaction/components/create-transaction-trigger/create-transaction-trigger.tsx
+++ packages/app/src/transaction/components/create-transaction-trigger/create-transaction-trigger.tsx
@@ -27,6 +27,7 @@ export const CreateTransactionTrigger = ({ isOpen, onPress }: Props) => {
             accessibilityLabel={CreateTransactionMenuSelector.Trigger}
             accessibilityRole="button"
             accessible
+            cancelable={false}
             collapsable={false}
             disabled={isOpen}
             className="bg-primary rounded-full items-center justify-center w-18 h-18"
